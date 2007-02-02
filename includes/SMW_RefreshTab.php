9aee299b606aa19f4c125ead621632374561720e
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2007-02-02 20:56:47 +0000

    Fixed problem with CologneBlue et al.

--- includes/SMW_RefreshTab.php
+++ includes/SMW_RefreshTab.php
@@ -7,16 +7,12 @@ $wgHooks[ 'SkinTemplateTabs' ][] = 'smwfAddRefreshTab';
  * the cache and thus refreshes the inline queries.
  */
 function smwfAddRefreshTab($obj, $content_actions) {
-	global $wgUser;
-	$title = $obj->mTitle;
-	if ($title === NULL) { // TODO: quick fix for some MediaWiki skins
-		return true;
-	}
+	global $wgUser, $wgTitle;
 	if($wgUser->isAllowed('delete')){
 		$content_actions['purge'] = array(
 			'class' => false,
 			'text' => wfMsg('purge'),
-			'href' => $title->getLocalUrl( 'action=purge' )
+			'href' => $wgTitle->getLocalUrl( 'action=purge' )
 		);
 	}
 	return true; // always return true, in order not to stop MW's hook processing!
