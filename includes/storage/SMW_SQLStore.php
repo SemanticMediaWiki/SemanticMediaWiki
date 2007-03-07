63085d54235b77047efa9d6799416ed39d383717
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2007-03-07 10:07:27 +0000

    New code for storage abstraction. This will soon make SMW_Storage obsolete and allow semantic storage backends other (and faster) than MySQL.

---
+++ includes/storage/SMW_SQLStore.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * SQL implementation of SMW's storage abstraction layer.
+ *
+ * @author Markus Krötzsch
+ */
+
+global $smwgIP;
+require_once( "$smwgIP/includes/storage/SMW_Store.php" );
+require_once( "$smwgIP/includes/SMW_Datatype.php" );
+require_once( "$smwgIP/includes/SMW_DataValue.php" );
+
+/**
+ * Storage access class for using the standard MediaWiki SQL database
+ * for keeping semantic data.
+ */
+class SMWSQLStore extends SMWStore {
+
+
+	function getSpecialValues(Title $subject, $specialprop, $limit = -1, $offset = 0) {
+		$db =& wfGetDB( DB_MASTER ); // TODO: can we use SLAVE here? Is '=&' needed in PHP5?
+		$sql = 'subject_id=' . $db->addQuotes($subject->getArticleID()) .
+		       'AND property_id=' . $db->addQuotes($specialprop);
+		$res = $db->select( $db->tableName('smw_specialprops'), 
+		                    'value_string',
+		                    $sql, 'SMW::getSpecialValues', $this->getSQLOptions($limit, $offset) );
+		// rewrite result as array
+		$result = array();
+		if($db->numRows( $res ) > 0) {
+			while($row = $db->fetchObject($res)) {
+				$result[] = $row->value_string;
+			}
+		}
+		$db->freeResult($res);
+		return $result;
+	}
+
+
+	function getAttributeValues(Title $subject, Title $attribute, $limit = -1, $offset = 0) {
+		$db =& wfGetDB( DB_MASTER ); // TODO: can we use SLAVE here? Is '=&' needed in PHP5?
+		$sql = 'subject_id=' . $db->addQuotes($subject->getArticleID()) .
+		       ' AND attribute_title=' . $db->addQuotes($attribute->getDBkey());
+
+		$res = $db->select( $db->tableName('smw_attributes'), 
+		                    'value_unit, value_datatype, value_xsd',
+		                    $sql, 'SMW::getAttributeValues', $this->getSQLOptions($limit, $offset) );
+		// rewrite result as array
+		$result = array();
+		if($db->numRows( $res ) > 0) {
+			while($row = $db->fetchObject($res)) {
+				$dv = SMWDataValue::newTypedValue(SMWTypeHandlerFactory::getTypeHandlerByID($row->value_datatype));
+				$dv->setXSDValue($row->value_xsd, $row->value_unit);
+				$result[] = $dv;
+			}
+		}
+		$db->freeResult($res);
+
+		return $result;
+	}
+
+
+	function getAttributes(Title $subject, $limit = -1, $offset = 0) {
+		$db =& wfGetDB( DB_MASTER ); // TODO: can we use SLAVE here? Is '=&' needed in PHP5?
+		$sql = 'subject_id=' . $db->addQuotes($subject->getArticleID());
+
+		$res = $db->select( $db->tableName('smw_attributes'), 
+		                    'DISTINCT attribute_title',
+		                    $sql, 'SMW::getAttributes', $this->getSQLOptions($limit, $offset) );
+		// rewrite result as array
+		$result = array();
+		if($db->numRows( $res ) > 0) {
+			while($row = $db->fetchObject($res)) {
+				$result[] = Title::newFromText($row->attribute_title, SMW_NS_ATTRIBUTE);
+			}
+		}
+		$db->freeResult($res);
+
+		return $result;
+	}
+
+	function getRelationObjects(Title $subject, Title $relation, $limit = -1, $offset = 0) {
+		$db =& wfGetDB( DB_MASTER ); // TODO: can we use SLAVE here? Is '=&' needed in PHP5?
+		$sql = 'subject_id=' . $db->addQuotes($subject->getArticleID()) .
+		       ' AND relation_title=' . $db->addQuotes($relation->getDBKey());
+
+		$res = $db->select( $db->tableName('smw_relations'), 
+		                    'object_title, object_namespace',
+		                    $sql, 'SMW::getRelationObjects', $this->getSQLOptions($limit, $offset) );
+		// rewrite result as array
+		$result = array();
+		if($db->numRows( $res ) > 0) {
+			while($row = $db->fetchObject($res)) {
+				$result[] = Title::newFromText($row->object_title, $row->object_namespace);
+			}
+		}
+		$db->freeResult($res);
+
+		return $result;
+	}
+
+	function getRelationSubjects(Title $object, Title $relation, $limit = -1, $offset = 0) {
+		$db =& wfGetDB( DB_MASTER ); // TODO: can we use SLAVE here? Is '=&' needed in PHP5?
+		$sql = 'object_namespace=' . $db->addQuotes($object->getNamespace()) . 
+		       ' AND object_title=' . $db->addQuotes($object->getDBKey()) .
+		       ' AND relation_title=' . $db->addQuotes($relation->getDBKey());
+
+		$res = $db->select( $db->tableName('smw_relations'), 
+		                    'DISTINCT subject_id',
+		                    $sql, 'SMW::getRelationSubjects', $this->getSQLOptions($limit, $offset) );
+		// rewrite result as array
+		$result = array();
+		if($db->numRows( $res ) > 0) {
+			while($row = $db->fetchObject($res)) {
+				$result[] = Title::newFromID($row->subject_id);
+			}
+		}
+		$db->freeResult($res);
+
+		return $result;
+	}
+
+	function getOutRelations(Title $subject, $limit = -1, $offset = 0) {
+		$db =& wfGetDB( DB_MASTER ); // TODO: can we use SLAVE here? Is '=&' needed in PHP5?
+		$sql = 'subject_id=' . $db->addQuotes($subject->getArticleID());
+
+		$res = $db->select( $db->tableName('smw_relations'), 
+		                    'DISTINCT relation_title',
+		                    $sql, 'SMW::getOutRelations', $this->getSQLOptions($limit, $offset) );
+		// rewrite result as array
+		$result = array();
+		if($db->numRows( $res ) > 0) {
+			while($row = $db->fetchObject($res)) {
+				$result[] = Title::newFromText($row->relation_title, SMW_NS_RELATION);
+			}
+		}
+		$db->freeResult($res);
+
+		return $result;
+	}
+
+	function getInRelations(Title $object, $limit = -1, $offset = 0) {
+		$db =& wfGetDB( DB_MASTER ); // TODO: can we use SLAVE here? Is '=&' needed in PHP5?
+		$sql = 'object_namespace=' . $db->addQuotes($object->getNamespace()) . 
+		       ' AND object_title=' . $db->addQuotes($object->getDBKey());
+
+		$res = $db->select( $db->tableName('smw_relations'), 
+		                    'DISTINCT relation_title',
+		                    $sql, 'SMW::getInRelations', $this->getSQLOptions($limit, $offset) );
+		// rewrite result as array
+		$result = array();
+		if($db->numRows( $res ) > 0) {
+			while($row = $db->fetchObject($res)) {
+				$result[] = Title::newFromText($row->relation_title, SMW_NS_RELATION);
+			}
+		}
+		$db->freeResult($res);
+
+		return $result;
+	}
+
+	/**
+	 * Transform input parameters into a suitable array of SQL options.
+	 */
+	protected function getSQLOptions($limit, $offset) {
+		$sql_options = array();
+		if ($limit >= 0) {
+			$sql_options['LIMIT'] = $limit;
+		}
+		if ($offset > 0) {
+			$sql_options['OFFSET'] = $offset;
+		}
+		return $sql_options;
+	}
+
+}
+
+ 
+?>
---
+++ includes/storage/SMW_Store.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Basic abstract classes for SMW's storage abstraction layer.
+ *
+ * @author Markus Krötzsch
+ */
+
+/**
+ * The abstract base class for all classes that implement access to some
+ * semantic store. Besides the relevant interface, this class provides default
+ * implementations for some optional methods, which inform the caller that 
+ * these methods are not implemented.
+ */
+abstract class SMWStore {
+
+///// Reading methods /////
+
+	/**
+	 * Get an array of all special values stored for the given subject and special property
+	 * (identified as usual by an integer constant). The result is an array which may contain
+	 * different kinds of contents depending on the special property that was requested.
+	 */
+	abstract function getSpecialValues(Title $subject, $specialprop, $limit = -1, $offset = 0);
+
+	/**
+	 * Get an array of all attribute values stored for the given subject and atttribute. The result 
+	 * is an array of SMWDataValue objects.
+	 */
+	abstract function getAttributeValues(Title $subject, Title $attribute, $limit = -1, $offset = 0);
+	
+	/**
+	 * Get an array of all attributes for which the given subject has some value. The result is an
+	 * array of Title objects.
+	 */
+	abstract function getAttributes(Title $subject, $limit = -1, $offset = 0);
+
+	/**
+	 * Get an array of all objects that a given subject relates to via the given relation. The
+	 * result is an array of Title objects.
+	 */
+	abstract function getRelationObjects(Title $subject, Title $relation, $limit = -1, $offset = 0);
+
+	/**
+	 * Get an array of all subjects that are related to a given object via the given relation. The
+	 * result is an array of Title objects.
+	 */
+	abstract function getRelationSubjects(Title $object, Title $relation, $limit = -1, $offset = 0);
+
+	/**
+	 * Get an array of all relations via which the given subject relates to some object. The result is an
+	 * array of Title objects.
+	 */
+	abstract function getOutRelations(Title $subject, $limit = -1, $offset = 0);
+
+	/**
+	 * Get an array of all relations for which there is some subject that relates to the given object. 
+	 * The result is an array of Title objects.
+	 */
+	abstract function getInRelations(Title $object, $limit = -1, $offset = 0);
+
+}
+
+ 
+?>
