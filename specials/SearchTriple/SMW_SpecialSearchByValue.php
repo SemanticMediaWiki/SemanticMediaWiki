0f0b83b16e0bb4654ece99079a0fb4726f6a17b7
Denny Vrandecic <vrandezo@users.mediawiki.org>
2007-03-21 21:36:41 +0000

    Expanded heavily on the -what links here- page

---
+++ specials/SearchTriple/SMW_SpecialSearchByValue.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * @author Denny Vrandecic
+ *
+ * This special page for Semantic MediaWiki implements a
+ * view on a relation-object pair, i.e. a typed baclink.
+ * For example, it shows me all persons born in Croatia,
+ * or all winners of the Academy Award for best actress.
+ */
+
+if (!defined('MEDIAWIKI')) die();
+require_once( "$IP/includes/SpecialPage.php" );
+require_once( "$smwgIP/includes/storage/SMW_Store.php" );
+global $wgExtensionFunctions;
+$wgExtensionFunctions[] = "wfSearchByValueExtension";
+
+function wfSearchByValueExtension()
+{
+	global $IP, $smwgIP, $wgMessageCache, $wgOut;
+	smwfInitMessages(); // initialize messages, always called before anything else on this page
+
+	function doSpecialSearchByValue($query = '') {
+		SMW_SearchByValue::execute($query);
+	}
+
+	SpecialPage::addPage( new SpecialPage('SearchByValue','',true,'doSpecialSearchByValue',false) );
+}
+
+class SMW_SearchByValue {
+
+	static function execute($query = '') {
+		global $wgRequest, $wgOut, $wgUser, $smwgIQMaxLimit;
+		$skin = $wgUser->getSkin();
+
+		// get the GET parameters
+		$attribute = $wgRequest->getVal( 'attribute' );
+		$valuestring = $wgRequest->getVal( 'value' );
+		// no GET parameters? Then try the URL
+		if (('' == $attribute) && ('' == $valuestring)) {
+			$queryparts = explode(':=', $query);
+			$attribute = $query;
+			if (count($queryparts) == 2) {
+				$attribute = $queryparts[0];
+				$valuestring = str_replace("_", " ", $queryparts[1]);
+			}
+		}
+		$attributetitle = Title::newFromText( $attribute, SMW_NS_ATTRIBUTE );
+
+		$limit = $wgRequest->getVal( 'limit' );
+		if ('' == $limit) $limit =  20;
+		$offset = $wgRequest->getVal( 'offset' );
+		if ('' == $offset) $offset = 0;
+		$html = '';
+		$spectitle = Title::makeTitle( NS_SPECIAL, 'SearchByValue' );
+
+		// display query form
+		$html .= '<form name="searchbyvalue" action="' . $spectitle->escapeLocalURL() . '" method="get">' . "\n" .
+		         '<input type="hidden" name="title" value="' . $spectitle->getPrefixedText() . '"/>' ;
+		$html .= wfMsg('smw_sbv_attribute') . ' <input type="text" name="attribute" value="' . htmlspecialchars($attribute) . '" />' . "\n";
+		$html .= wfMsg('smw_sbv_value') . ' <input type="text" name="value" value="' . htmlspecialchars($valuestring) . '" />' . "\n";
+		$html .= '<input type="submit" value="' . wfMsg('smw_sbv_submit') . "\"/>\n</form>\n";
+
+		if ('' == $attribute) { // empty page. If no attribute given the value does not matter
+			$html .= wfMsg('smw_sbv_docu') . "\n";
+		} elseif ('' == $valuestring) { // no value given
+			$html .= wfMSG('smw_sbv_novalue', $skin->makeLinkObj($attributetitle, $attributetitle->mTextform));
+		} else { // everything is given
+			$unit = NULL;
+			$type = NULL;
+			$value = NULL;
+			// set unit and (XSD) value
+			$datavalue = SMWDataValue::newAttributeValue($attribute, $skin);
+			if ( $datavalue->getTypeID() != 'error') {
+				// TODO: Performance (medium): setUserValue() calls the data type's processValue() which does a lot of conversion and tooltip work that's unused here.
+				$datavalue->setUserValue($valuestring);
+				if ( $datavalue->isValid() === false ) {
+					// try to use unparsable values as units
+					$unit=$value;
+					$value=NULL;
+				} else {
+					$unit = $datavalue->getUnit();
+					$value  = $datavalue->getXSDValue();
+				}
+			}
+
+			$res = smwfGetAttributes(NULL, $attributetitle, $unit, NULL, $value, false); // do not care about the typeid here
+			// TODO change to new SMW_Store asap
+			//$results = &smwfGetStore()->getRelationSubjects($relation, $object, $limit+1, $offset);
+			$count = count($res);
+
+
+			$html .= "<p>&nbsp;</p>\n" . wfMsg('smw_sbv_displayresult', $skin->makeLinkObj($attributetitle, $attributetitle->mTextform), $valuestring) . "<br />\n";
+
+			// prepare navigation bar
+			if ($offset > 0)
+				$navigation = '<a href="' . htmlspecialchars($skin->makeSpecialUrl('SearchByValue','offset=' . max(0,$offset-$limit) . '&limit=' . $limit . '&attribute=' . urlencode($attribute) .'&value=' . urlencode($valuestring))) . '">' . wfMsg('smw_sbv_prev') . '</a>';
+			else
+				$navigation = wfMsg('smw_sbv_prev');
+
+			$navigation .= '&nbsp;&nbsp;&nbsp;&nbsp; <b>' . wfMsg('smw_sbv_results') . ' ' . ($offset+1) . '&ndash; ' . ($offset + min($count, 20)) . '</b>&nbsp;&nbsp;&nbsp;&nbsp;';
+
+			if ($count>$limit)
+				$navigation .= ' <a href="' . htmlspecialchars($skin->makeSpecialUrl('SearchByValue', 'offset=' . ($offset+$limit) . '&limit=' . $limit . '&attribute=' . urlencode($attribute) . '&value=' . urlencode($valuestring)))  . '">' . wfMsg('smw_sbv_next') . '</a>';
+			else
+				$navigation .= wfMsg('smw_sbv_next');
+
+			$max = false; $first=true;
+			foreach (array(20,50,100,250,500) as $l) {
+				if ($max) continue;
+				if ($first) {
+					$navigation .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(';
+					$first = false;
+				} else
+					$navigation .= ' | ';
+				if ($l > $smwgIQMaxLimit) {
+					$l = $smwgIQMaxLimit;
+					$max = true;
+				}
+				if ( $limit != $l ) {
+					$navigation .= '<a href="' . htmlspecialchars($skin->makeSpecialUrl('SearchByValue','offset=' . $offset . '&limit=' . $l . '&attribute=' . urlencode($attribute) . '&value=' . urlencode($valuestring))) . '">' . $l . '</a>';
+				} else {
+					$navigation .= '<b>' . $l . '</b>';
+				}
+			}
+			$navigation .= ')';
+
+			// The navigation display is currently disabled, as long as the new
+			// storage abstraction layer is not available.
+
+			// no need to show the navigation bars when there is not enough to navigate
+			///if (($offset>0) || ($count>$limit))
+				///$html .= '<br />' . $navigation;
+			if ($count == 0)
+				$html .= wfMsg( 'smw_sbv_noresults' );
+			else {
+				$html .= "<ul>\n";
+				foreach ($res as $line) {
+					$t = Title::newFromID($line[0]);
+					$html .= "<li>" . $skin->makeKnownLink($t->getPrefixedText()) . "</li>\n";
+				}
+				$html .= "</ul>\n";
+			}
+			///if (($offset>0) || ($count>$limit))
+				///$html .= $navigation;
+		}
+
+		$wgOut->addHTML($html);
+	}
+
+}
+?>
---
+++ specials/SearchTriple/SMW_SpecialTypedBacklinks.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * @author Denny Vrandecic
+ *
+ * This special page for Semantic MediaWiki implements a
+ * view on a relation-object pair, i.e. a typed baclink.
+ * For example, it shows me all persons born in Croatia,
+ * or all winners of the Academy Award for best actress.
+ */
+
+if (!defined('MEDIAWIKI')) die();
+require_once( "$IP/includes/SpecialPage.php" );
+require_once( "$smwgIP/includes/storage/SMW_Store.php" );
+global $wgExtensionFunctions;
+$wgExtensionFunctions[] = "wfTypedBacklinksExtension";
+
+function wfTypedBacklinksExtension()
+{
+	global $IP, $smwgIP, $wgMessageCache, $wgOut;
+	smwfInitMessages(); // initialize messages, always called before anything else on this page
+
+	function doSpecialTypedBacklinks($query = '') {
+		SMW_TypedBacklinks::execute($query);
+	}
+
+	SpecialPage::addPage( new SpecialPage('TypedBacklinks','',true,'doSpecialTypedBacklinks',false) );
+}
+
+class SMW_TypedBacklinks {
+
+	static function execute($query = '') {
+		global $wgRequest, $wgOut, $wgUser, $smwgIQMaxLimit;
+		$skin = $wgUser->getSkin();
+
+		// get the GET parameters
+		$type = $wgRequest->getVal( 'type' );
+		$target = $wgRequest->getVal( 'target' );
+		// no GET parameters? Then try the URL
+		if (('' == $type) && ('' == $target)) {
+			$queryparts = explode('::', $query);
+			$type = $query;
+			if (count($queryparts) > 1) {
+				$type = $queryparts[0];
+				$target = implode('::', array_slice($queryparts, 1));
+			}
+		}
+		$relation = Title::newFromText( $type, SMW_NS_RELATION );
+		$object = Title::newFromText( $target );
+		$limit = $wgRequest->getVal( 'limit' );
+		if ('' == $limit) $limit =  20;
+		$offset = $wgRequest->getVal( 'offset' );
+		if ('' == $offset) $offset = 0;
+		$html = '';
+		$spectitle = Title::makeTitle( NS_SPECIAL, 'TypedBacklinks' );
+
+		// display query form
+		$html .= '<form name="typedbacklinks" action="' . $spectitle->escapeLocalURL() . '" method="get">' . "\n" .
+		         '<input type="hidden" name="title" value="' . $spectitle->getPrefixedText() . '"/>' ;
+		$html .= wfMsg('smw_tb_linktype') . ' <input type="text" name="type" value="' . htmlspecialchars($type) . '" />' . "\n";
+		$html .= wfMsg('smw_tb_linktarget') . ' <input type="text" name="target" value="' . htmlspecialchars($target) . '" />' . "\n";
+		$html .= '<input type="submit" value="' . wfMsg('smw_tb_submit') . "\"/>\n</form>\n";
+
+		if (('' == $type) && ('' == $target)) { // empty page. No relation and no object given.
+			$html .= wfMsg('smw_tb_docu') . "\n";
+		} elseif ('' == $type) { // no relation given
+			$wlhtitle = Title::makeTitle( NS_SPECIAL, 'Whatlinkshere' );
+			$wlhlink = htmlspecialchars($skin->makeSpecialUrl('Whatlinkshere/' . $object->getPrefixedURL()));
+			$html .= wfMsg('smw_tb_notype', $object->getPrefixedText(), $wlhlink);
+		} elseif ('' == $target) { // no object given
+			$html .= wfMSG('smw_tb_notarget', $skin->makeLinkObj($relation, $relation->mTextform));
+		} else { // everything is given
+			// get results (get one more, to see if we have to add a link to more)
+			$results = &smwfGetStore()->getRelationSubjects($relation, $object, $limit+1, $offset);
+
+			$html .= "<p>&nbsp;</p>\n" . wfMsg('smw_tb_displayresult', $skin->makeLinkObj($relation, $relation->mTextform), $skin->makeLinkObj($object)) . "<br />\n";
+
+			// prepare navigation bar
+			if ($offset > 0)
+				$navigation = '<a href="' . htmlspecialchars($skin->makeSpecialUrl('TypedBacklinks','offset=' . max(0,$offset-$limit) . '&limit=' . $limit . '&type=' . urlencode($type) .'&target=' . urlencode($target))) . '">' . wfMsg('smw_tb_prev') . '</a>';
+			else
+				$navigation = wfMsg('smw_tb_prev');
+
+			$navigation .= '&nbsp;&nbsp;&nbsp;&nbsp; <b>' . wfMsg('smw_tb_results') . ' ' . ($offset+1) . '&ndash; ' . ($offset + min(count($results), 20)) . '</b>&nbsp;&nbsp;&nbsp;&nbsp;';
+
+			if (count($results)==($limit+1))
+				$navigation .= ' <a href="' . htmlspecialchars($skin->makeSpecialUrl('TypedBacklinks', 'offset=' . ($offset+$limit) . '&limit=' . $limit . '&type=' . urlencode($type) . '&target=' . urlencode($target)))  . '">' . wfMsg('smw_tb_next') . '</a>';
+			else
+				$navigation .= wfMsg('smw_tb_next');
+
+			$max = false; $first=true;
+			foreach (array(20,50,100,250,500) as $l) {
+				if ($max) continue;
+				if ($first) {
+					$navigation .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(';
+					$first = false;
+				} else
+					$navigation .= ' | ';
+				if ($l > $smwgIQMaxLimit) {
+					$l = $smwgIQMaxLimit;
+					$max = true;
+				}
+				if ( $limit != $l ) {
+					$navigation .= '<a href="' . htmlspecialchars($skin->makeSpecialUrl('TypedBacklinks','offset=' . $offset . '&limit=' . $l . '&type=' . urlencode($type) . '&target=' . urlencode($target))) . '">' . $l . '</a>';
+				} else {
+					$navigation .= '<b>' . $l . '</b>';
+				}
+			}
+			$navigation .= ')';
+
+			// no need to show the navigation bars when there is not enough to navigate
+			if (($offset>0) || (count($results)>$limit))
+				$html .= '<br />' . $navigation;
+			if (count($results) == 0)
+				$html .= wfMsg( 'smw_tb_noresults' );
+			else {
+				$html .= "<ul>\n";
+				foreach ($results as $result) {
+					$html .= '<li>' . $skin->makeKnownLinkObj($result) . "</li>\n";
+				}
+				$html .= "</ul>\n";
+			}
+			if (($offset>0) || (count($results)>$limit))
+				$html .= $navigation;
+		}
+
+		$wgOut->addHTML($html);
+	}
+
+}
+?>
