570cd2d18f296a11d41a6ed637c3bf9aae1e0398
Denny Vrandecic <vrandezo@users.mediawiki.org>
2007-03-21 21:32:30 +0000

    Enabling linked data over URI content negotiation

---
+++ specials/URIResolver/SMW_SpecialURIResolver.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author Denny Vrandecic
+ *
+ * This special page solves with the URI crisis
+ * without the need of changing code deep in
+ * MediaWiki were no hook has ever seen light.
+ */
+
+if (!defined('MEDIAWIKI')) die();
+require_once( "$IP/includes/SpecialPage.php" );
+global $wgExtensionFunctions;
+$wgExtensionFunctions[] = "wfURIResolverExtension";
+
+function wfURIResolverExtension()
+{
+	global $IP, $smwgIP, $wgMessageCache, $wgOut;
+	smwfInitMessages(); // initialize messages, always called before anything else on this page
+
+	function doSpecialURIResolver($name = '') {
+		SMW_URIResolver::execute($name);
+	}
+
+	SpecialPage::addPage( new SpecialPage('URIResolver','',false,'doSpecialURIResolver',false) );
+}
+
+class SMW_URIResolver {
+
+	static function execute($query = '') {
+		global $wgOut, $smwgIP;
+		require_once( $smwgIP . '/specials/ExportRDF/SMW_SpecialExportRDF.php' );
+		if ('' == $query) {
+			$wgOut->addHTML(wfMsg('smw_uri_doc'));
+		} else {
+			$wgOut->disable();
+
+			$query = ExportRDF::makeURIfromXMLExportId($query);
+			$query = str_replace( "_", "%20", $query );
+			$query = urldecode($query);
+			$title = Title::newFromText($query);
+			$t = $title->getFullURL();
+
+			$rdftitle = Title::newFromText('ExportRDF', NS_SPECIAL);
+			$s = $rdftitle->getFullURL() . "/" . $title->getPrefixedURL();
+
+			header('HTTP/1.1 303 See Other');
+			if (stristr($_SERVER['HTTP_ACCEPT'], 'RDF'))
+				header('Location: ' . $s);
+			else
+				header('Location: ' . $t);
+		}
+	}
+}
+?>
