a76acd0ba101d5114f60f55c2ce13f3a47a0be15
Denny Vrandecic <vrandezo@users.mediawiki.org>
2007-03-21 21:50:16 +0000

    Added some extended statistics (broken, no prefix possible)

---
+++ specials/ExtendedStatistics/SMW_SpecialExtendedStatistics.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * @author Daniel M. Herzig
+ * @version 0.1
+ *
+ * This special page of the Semantic Media Wiki Extension shows some statistics about relations.
+ */
+if (!defined('MEDIAWIKI')) die();
+$wgExtensionFunctions[] = "wfExtendedStatistics";
+
+
+function wfExtendedStatistics() {
+
+	global $wgMessageCache;
+	smwfInitMessages();
+	global $IP, $smwgIP;
+
+	require_once( "$IP/includes/SpecialPage.php" );
+	require_once( "$IP/includes/Title.php" );
+
+	class ExtendedStatistics extends SpecialPage {
+
+		function ExtendedStatistics() {
+			SpecialPage::SpecialPage( 'ExtendedStatistics' );
+			$this->includable( true );
+		}
+
+		function getName() {
+			return "extendedstatistics";
+		}
+
+		function execute( $par = null ) {
+			global $wgOut, $wgLang;
+
+			$dbr =& wfGetDB( DB_SLAVE );
+
+			$views = SiteStats::views();
+			$edits = SiteStats::edits();
+			$good = SiteStats::articles();
+			$images = SiteStats::images();
+			$users = SiteStats::users();
+
+			$sql = "SELECT Count(DISTINCT relation_title) AS count FROM pim5_smw_relations";
+			$res = $dbr->query( $sql );
+			$row = $dbr->fetchObject( $res );
+			$relations = $wgLang->formatNum($row->count);
+			$dbr->freeResult( $res );
+
+			$sql = "SELECT Count(*) AS count FROM pim5_smw_relations";
+			$res = $dbr->query( $sql );
+			$row = $dbr->fetchObject( $res );
+			$relation_instance = $wgLang->formatNum($row->count);
+			$dbr->freeResult( $res );
+
+			$sql  = "SELECT Count(*) AS count ";
+			$sql .= "FROM pim5_page ";
+			$sql .= "where page_title IN ";
+			$sql .= "(SELECT DISTINCT pim5_smw_relations.relation_title FROM pim5_smw_relations);";
+			$res = $dbr->query( $sql );
+			$row = $dbr->fetchObject( $res );
+			$relation_pages = $wgLang->formatNum($row->count);
+			$dbr->freeResult( $res );
+
+			$sql = "SELECT Count(DISTINCT attribute_title) AS count FROM pim5_smw_attributes";
+			$res = $dbr->query( $sql );
+			$row = $dbr->fetchObject( $res );
+			$attributes = $wgLang->formatNum($row->count);
+			$dbr->freeResult( $res );
+
+			$sql = "SELECT Count(*) AS count FROM pim5_smw_attributes";
+			$res = $dbr->query( $sql );
+			$row = $dbr->fetchObject( $res );
+			$attribute_instance = $wgLang->formatNum($row->count);
+			$dbr->freeResult( $res );
+
+
+
+			$out = "<table >
+					<tr>
+						<td><h2>" . wfMsg('smw_extstats_general') ."</h2></td>
+						<td></td>
+					</tr>
+					<tr>
+						 <td>" . wfMsg('smw_extstats_totalp') ."</td>
+						 <td>".$good."</td>
+					</tr>
+					<tr>
+						 <td>" . wfMsg('smw_extstats_totalv') ."</td>
+						 <td>".$views."</td>
+					</tr>
+					<tr>
+						 <td>" . wfMsg('smw_extstats_totalpe') ."</td>
+						 <td>".$edits."</td>
+					</tr>
+					<tr>
+						 <td>" . wfMsg('smw_extstats_totali') ."</td>
+						 <td>".$images."</td>
+					</tr>
+					<tr>
+						 <td>" . wfMsg('smw_extstats_totalu') ."</td>
+						 <td>".$users." </td>
+					</tr>
+				   </table>";
+
+			$out .= "<table>
+						<tr>
+							 <td><h2>" . wfMsg('extendedstatistics') ."</h2> </td>
+							 <td> </td>
+						</tr>
+						<tr>
+							 <td>" . wfMsg('smw_extstats_totalr') ."</td>
+							 <td>".$relations." </td>
+						</tr>
+						<tr>
+							 <td>" . wfMsg('smw_extstats_totalri') ."</td>
+							 <td>".$relation_instance."</td>
+						</tr>
+						<tr>
+							 <td>" . wfMsg('smw_extstats_totalra') ."</td>
+							 <td>".$wgLang->formatNum( sprintf( '%0.2f', $relation_instance!=0 ? $relation_instance / $relations : 0 ) ) ."</td>
+						</td>
+
+						<tr>
+							 <td>" . wfMsg('smw_extstats_totalpr') ."</td>
+							 <td>".$relation_pages." </td>
+						</tr>
+						<tr>
+							 <td>" . wfMsg('smw_extstats_totala') ."</td>
+							 <td>".$attributes."</td>
+						</tr>
+						<tr>
+							 <td>" . wfMsg('smw_extstats_totalai') ."</td>
+							 <td>".$attribute_instance."</td>
+						</tr>
+						<tr>
+							 <td>" . wfMsg('smw_extstats_totalaa') ."</td>
+							 <td>".$wgLang->formatNum( sprintf( '%0.2f', $attribute_instance!=0 ? $attribute_instance / $attributes : 0 ) ) ." </td>
+						</td>
+					</table>";
+
+			$wgOut->addHTML( $out );
+
+		}
+	}
+
+	SpecialPage::addPage( new ExtendedStatistics );
+}
+?>
