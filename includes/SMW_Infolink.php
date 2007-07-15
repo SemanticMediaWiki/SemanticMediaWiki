47726805bc78ec3dc4ee4e14b429d22f314b2634
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2007-07-15 12:08:16 +0000

    Seperated infolink code

---
+++ includes/SMW_Infolink.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * This file contains the SMWInfolink class.
+ *
+ * @author Markus Krötzsch
+ */
+
+/**
+ * This class mainly is a container to store URLs for the factbox in a
+ * clean way. The class provides methods for creating source code for
+ * realising them in wiki or html contexts.
+ */
+class SMWInfolink {
+	private $target;        // the actual link target
+	private $caption;       // the label for the link
+	private $style;         // CSS class of a span to embedd the link into, or
+	                        // false if no extra style is required
+	private $internal;      // indicates whether $target is a page name (true) or URL (false)
+
+	/**
+	 * Create a new link to an internal page $target. All parameters are mere strings
+	 * as used by wiki users
+	 */
+	public static function newInternalLink($caption, $target, $style=false) {
+		return new SMWInfolink(true,$caption,$target,$style);
+	}
+
+	/**
+	 * Create a new link to an external location $url.
+	 */
+	public static function newExternalLink($caption, $url, $style=false) {
+		return new SMWInfolink(false,$caption,$url,$style);
+	}
+
+	/**
+	 * Static function to construct links to property searches.
+	 */
+	public static function newPropertySearchLink($caption,$propertyname,$value,$style = 'smwsearch') {
+		global $wgContLang;
+		return new SMWInfolink(true,$caption,$wgContLang->getNsText(NS_SPECIAL) . ':SearchByProperty/' .  $propertyname . ':=' . $value, $style);
+	}
+
+	/**
+	 * Static function to construct links to inverse property searches.
+	 */
+	public static function newInversePropertySearchLink($caption,$subject,$propertyname,$style = false) {
+		global $wgContLang;
+		return new SMWInfolink(true,$caption,$wgContLang->getNsText(NS_SPECIAL) . ':PageProperty/' .  $subject . '::' . $propertyname, $style);
+	}
+
+	/**
+	 * Static function to construct links to the browsing special.
+	 */
+	public static function newBrowsingLink($caption,$titletext,$style = 'smwbrowse') {
+		global $wgContLang;
+		return new SMWInfolink(true,$caption,$wgContLang->getNsText(NS_SPECIAL) . ':Browse/' .  $titletext, $style);
+	}
+
+	/**
+	 * Create a new link to some internal page or to some external URL.
+	 */
+	public function SMWInfolink($internal, $caption, $target, $style=false) {
+		$this->internal = $internal;
+		$this->caption = $caption;
+		$this->target = $target;
+		$this->style = $style;
+	}
+
+	/**
+	 * Return hyperlink for this infolink in HTML format.
+	 */
+	public function getHTML($skin) {
+		if ($this->style !== false) {
+			$start = "<span class=\"$this->style\">";
+			$end = '</span>';
+		} else {
+			$start = '';
+			$end = '';
+		}
+		if ($this->internal) {
+			$title = Title::newFromText($this->target);
+			if ($title !== NULL) {
+				return $start . $skin->makeKnownLinkObj(Title::newFromText($this->target), $this->caption) . $end;
+			} else {
+				return '';
+			}
+		} else {
+			return $start . "<a href=\"$this->target\">$this->caption</a>" . $end;
+		}
+	}
+
+	/**
+	 * Return hyperlink for this infolink in wiki format.
+	 */
+	public function getWikiText() {
+		if ($this->style !== false) {
+			$start = "<span class=\"$this->style\">";
+			$end = '</span>';
+		} else {
+			$start = '';
+			$end = '';
+		}
+		if ($this->internal) {
+			if (preg_match('/(.*)(\[|\]|<|>|&gt;|&lt;|{|})(.*)/', $this->target) != 0 ) {
+				return ''; // give up if illegal characters occur,
+				           // TODO: we would need a skin to provide an ext URL in this case
+			}
+			return $start . "[[$this->target|$this->caption]]" . $end;
+		} else {
+			return $start . "[$this->target $this->caption]" . $end;
+		}
+	}
+
+
+///// Deprecated transition functions
+
+	/**
+	 * Static function to construct links to attribute searches.
+	 * @DEPRECATED
+	 */
+	public static function newAttributeSearchLink($caption,$attribute,$value,$style = 'smwsearch') {
+		trigger_error("Function newAttributeSearchLink is deprecated. Use new property methods.", E_USER_NOTICE);
+		return SMWInfolink::newPropertySearchLink($caption,$attribute,$value,$style);
+	}
+
+	/**
+	 * Static function to construct links to relation searches.
+	 * @DEPRECATED
+	 */
+	public static function newRelationSearchLink($caption,$relation,$object,$style = 'smwsearch') {
+		trigger_error("Function newRelationSearchLink is deprecated. Use new property methods.", E_USER_NOTICE);
+		return SMWInfolink::newPropertySearchLink($caption,$relation,$value,$style);
+// 		global $wgContLang;
+// 		return new SMWInfolink(true,$caption,$wgContLang->getNsText(NS_SPECIAL) . ':SearchByRelation/' . $relation . '::' . $object, $style);
+	}
+
+	/**
+	 * Static function to construct links to inverse relation searches.
+	 * @DEPRECATED
+	 */
+	public static function newInverseRelationSearchLink($caption,$subject,$relation,$style = false) {
+		trigger_error("Function newInverseRelationSearchLink is deprecated. Use new property methods.", E_USER_NOTICE);
+		return SMWInfolink::newInversePropertySearchLink($caption,$subject,$relation,$style);
+// 		global $wgContLang;
+// 		return new SMWInfolink(true,$caption,$wgContLang->getNsText(NS_SPECIAL) . ':PageProperty/' .  $subject . '::' . $relation, $style);
+	}
+
+	/**
+	 * Static function to construct links to inverse attribute searches.
+	 * @DEPRECATED
+	 */
+	public static function newInverseAttributeSearchLink($caption,$subject,$attribute,$style = false) {
+		trigger_error("Function newInverseAttributeSearchLink is deprecated. Use new property methods.", E_USER_NOTICE);
+		return SMWInfolink::newInversePropertySearchLink($caption,$subject,$attribute,$style);
+	}
+
+}
