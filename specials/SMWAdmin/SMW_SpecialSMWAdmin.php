a196d72b3e67355056dd2afc8e1c664bbc42523d
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2007-07-16 06:25:36 +0000

    fixed return mime-type

--- specials/SMWAdmin/SMW_SpecialSMWAdmin.php
+++ specials/SMWAdmin/SMW_SpecialSMWAdmin.php
@@ -43,7 +43,7 @@ function doSpecialSMWAdmin($par = null) {
 			$wgOut->disable(); // raw output
 			ob_start();
 			print "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\" dir=\"ltr\">\n<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>Setting up Storage for Semantic MediaWiki</title></head><body><p><pre>";
-			header( "Content-type: application/rdf+xml; charset=UTF-8" );
+			header( "Content-type: text/html; charset=UTF-8" );
 			$result = smwfGetStore()->setup();
 			print '</pre></p>';
 			if ($result === true) {
