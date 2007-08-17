90f5d854fbda3abf14517ddcdd740bcf535d5d89
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2007-08-17 15:22:52 +0000

    New special pages for replacing the former attribute/relation pages.

---
+++ specials/QueryPages/SMW_SpecialUnusedProperties.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author Markus Krötzsch
+ *
+ * This page shows all used attributes.
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+global $smwgIP;
+include_once( "$smwgIP/specials/QueryPages/SMW_QueryPage.php" );
+
+class UnusedPropertiesPage extends SMWQueryPage {
+
+	function getName() {
+		/// TODO: should probably use SMW prefix
+		return "UnusedProperties";
+	}
+
+	function isExpensive() {
+		return false; /// disables caching for now
+	}
+
+	function isSyndicated() { 
+		return false; ///TODO: why not?
+	}
+
+	function getPageHeader() {
+		return '<p>' . wfMsg('smw_unusedproperties_docu') . "</p><br />\n";
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgLang, $wgExtraNamespaces;
+		$proplink = $skin->makeKnownLinkObj( $result, $result->getText() );
+		$types = smwfGetStore()->getSpecialValues($result, SMW_SP_HAS_TYPE);
+		if (count($types) >= 1) {
+			$typestring = $types[0]->getLongHTMLText($skin);
+		} else {
+			$type = SMWDataValueFactory::newSpecialValue(SMW_SP_HAS_TYPE);
+			$type->setXSDValue('_wpg');
+			$typestring = $type->getLongHTMLText($skin);
+			$errors[] = wfMsg('smw_propertylackstype', $type->getLongHTMLText($skin));
+		}
+		return wfMsg('smw_unusedproperty_template', $proplink, $typestring) . ' ' . smwfEncodeMessages($errors);
+	}
+	
+	function getResults($requestoptions) {
+		return smwfGetStore()->getUnusedPropertiesSpecial($requestoptions);
+	}
+
+}
+
---
+++ specials/QueryPages/SMW_SpecialWantedProperties.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Markus Krötzsch
+ *
+ * This page shows all used attributes.
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+global $smwgIP;
+include_once( "$smwgIP/specials/QueryPages/SMW_QueryPage.php" );
+
+class WantedPropertiesPage extends SMWQueryPage {
+
+	function getName() {
+		/// TODO: should probably use SMW prefix
+		return "WantedProperties";
+	}
+
+	function isExpensive() {
+		return false; /// disables caching for now
+	}
+
+	function isSyndicated() { 
+		return false; ///TODO: why not?
+	}
+
+	function getPageHeader() {
+		return '<p>' . wfMsg('smw_wantedproperties_docu') . "</p><br />\n";
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgLang, $wgExtraNamespaces;
+		$proplink = $skin->makeLinkObj( $result[0], $result[0]->getText() );
+		return wfMsg('smw_wantedproperty_template', $proplink, $result[1]);
+	}
+	
+	function getResults($requestoptions) {
+		return smwfGetStore()->getWantedPropertiesSpecial($requestoptions);
+	}
+
+}
+
