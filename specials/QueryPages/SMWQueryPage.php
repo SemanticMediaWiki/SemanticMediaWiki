34e5750527695faff5beb57215ba4e8633b168bf
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2007-08-17 14:22:38 +0000

    New query pages for properties (will replace the old pages for relations/attributes)

---
+++ specials/QueryPages/SMWQueryPage.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @author Markus Krötzsch
+ *
+ * An abstract query page base class that supports array-based
+ * data retrieval instead of the SQL-based access used by MW.
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+global $IP;
+require_once( "$IP/includes/SpecialPage.php" );
+require_once( "$IP/includes/Title.php" );
+require_once( "$IP/includes/QueryPage.php");
+
+/**
+ * Abstract base class for SMW's variant of the MW QueryPage.
+ * Subclasses must implement getResults() and formatResult(), as
+ * well as some other standard functions of QueryPage.
+ */
+abstract class SMWQueryPage extends QueryPage {
+
+	/**
+	 * Implemented by subclasses to provide concrete functions.
+	 */
+	abstract function getResults($requestoptions);
+
+	/**
+	 * Clear the cache and save new results
+	 */
+	function recache( $limit, $ignoreErrors = true ) {
+		///TODO
+	}
+
+	/**
+	 * This is the actual workhorse. It does everything needed to make a
+	 * real, honest-to-gosh query page.
+	 * Alas, we need to overwrite the whole beast since we do not assume
+	 * an SQL-based storage backend.
+	 *
+	 * @param $offset database query offset
+	 * @param $limit database query limit
+	 * @param $shownavigation show navigation like "next 200"?
+	 */
+	function doQuery( $offset, $limit, $shownavigation=true ) {
+		global $wgUser, $wgOut, $wgLang, $wgContLang;
+
+		$options = new SMWRequestOptions();
+		$options->limit = $limit;
+		$options->offset = $offset;
+		$options->sort = true;
+		$res = $this->getResults($options);
+		$num = count($res);
+
+		$sk = $wgUser->getSkin();
+		$sname = $this->getName();
+
+		if($shownavigation) {
+			$wgOut->addHTML( $this->getPageHeader() );
+
+			// if list is empty, show it
+			if( $num == 0 ) {
+				$wgOut->addHTML( '<p>' . wfMsgHTML('specialpage-empty') . '</p>' );
+				return;
+			}
+
+			$top = wfShowingResults( $offset, $num);
+			$wgOut->addHTML( "<p>{$top}\n" );
+
+			// often disable 'next' link when we reach the end
+			$atend = $num < $limit;
+
+			$sl = wfViewPrevNext( $offset, $limit ,
+				$wgContLang->specialPage( $sname ),
+				wfArrayToCGI( $this->linkParameters() ), $atend );
+			$wgOut->addHTML( "<br />{$sl}</p>\n" );
+		}
+		if ( $num > 0 ) {
+			$s = array();
+			if ( ! $this->listoutput )
+				$s[] = $this->openList( $offset );
+
+			foreach ($res as $r) {
+				$format = $this->formatResult( $sk, $r );
+				if ( $format ) {
+					$s[] = $this->listoutput ? $format : "<li>{$format}</li>\n";
+				}
+			}
+
+			if ( ! $this->listoutput )
+				$s[] = $this->closeList();
+			$str = $this->listoutput ? $wgContLang->listToText( $s ) : implode( '', $s );
+			$wgOut->addHTML( $str );
+		}
+		if($shownavigation) {
+			$wgOut->addHTML( "<p>{$sl}</p>\n" );
+		}
+		return $num;
+	}
+
+}
+
---
+++ specials/QueryPages/SMW_SpecialProperties.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @author Markus Krötzsch
+ *
+ * This page shows all used attributes.
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+global $smwgIP;
+include_once( "$smwgIP/specials/QueryPages/SMWQueryPage.php" );
+
+class PropertiesPage extends SMWQueryPage {
+
+	function getName() {
+		/// TODO: should probably use SMW prefix
+		return "Properties";
+	}
+
+	function isExpensive() {
+		return false; /// disables caching for now
+	}
+
+	function isSyndicated() { 
+		return false; ///TODO: why not?
+	}
+
+	function getPageHeader() {
+		return '<p>' . wfMsg('smw_properties_docu') . "</p><br />\n";
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgLang, $wgExtraNamespaces;
+		$proplink = $skin->makeLinkObj( $result[0], $result[0]->getText() );
+		$typestring = '';
+		$errors = array();
+		if ($result[1]<=5) {
+			$errors[] = wfMsg('smw_propertyhardlyused');
+		}
+		if ($result[0]->exists()) {
+			$types = smwfGetStore()->getSpecialValues($result[0], SMW_SP_HAS_TYPE);
+			if (count($types) >= 1) {
+				$typestring = $types[0]->getLongHTMLText($skin);
+			}
+		} else {
+			$errors[] = wfMsg('smw_propertylackspage');
+		}
+		if ($typestring == '') {
+			$type = SMWDataValueFactory::newSpecialValue(SMW_SP_HAS_TYPE);
+			$type->setXSDValue('_wpg');
+			$typestring = $type->getLongHTMLText($skin);
+			$errors[] = wfMsg('smw_propertylackstype', $type->getLongHTMLText($skin));
+		}
+		return wfMsg('smw_property_template', $proplink, $typestring, $result[1]) . ' ' . smwfEncodeMessages($errors);
+	}
+	
+	function getResults($requestoptions) {
+		return smwfGetStore()->getPropertiesSpecial($requestoptions);
+	}
+
+}
+
