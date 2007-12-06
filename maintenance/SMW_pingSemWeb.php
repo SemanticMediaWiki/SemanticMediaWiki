5e0eaf87e6f4dc6a15a57324a8d03aa3178ba034
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2007-12-06 16:37:17 +0000

    Extended ping script. Option to specify (external) server name, heuristics for guessing it anyway.

--- maintenance/SMW_pingSemWeb.php
+++ maintenance/SMW_pingSemWeb.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Announces selected OWL/RDF-files of the wiki to http://pingthesemanticweb.com.
+ * See http://pingthesemanticweb.com/about.php for the rational behind this.
+ * This script may be extended to other such services if desired.
  *
  * Note: this file must be placed in MediaWiki's "maintenance" directory!
  *
@@ -10,17 +12,20 @@
  * -d <delay>   Wait for this many milliseconds after processing an article, useful for limiting server load.
  * -s <startid> Start refreshing at given article ID, useful for partial refreshing
  * -e <endid>   Stop refreshing at given article ID, useful for partial refreshing 
- * -v           Be verbose about the progress.
+ * -q           Be quite (no output).
+ * -h <server>  Define the server (without sub-path, e.g. "http://example.org" even for "http://example.org/wiki"
+ *              Use "-" for reusing the server name given to "enableSemantics()" when loading SMW. If omitted,
+ *              the value of $wgServer is used, though this may be localhost in many cases.
  *
  * @author Markus Krötzsch
+ * @TODO it should be possible to ping based on pages' modification dates
  */
 
-$optionsWithArgs = array( 'd', 's', 'e' ); // -d <delay>, -s <startid>
+$optionsWithArgs = array( 'd', 's', 'e', 'h' ); // -d <delay>, -s <startid>, -e <endid>
 
 require_once( 'commandLine.inc' );
 
-global $smwgIP;
-// require_once($smwgIP . '/includes/SMW_Factbox.php');
+global $smwgIP, $wgServer;
 include_once($smwgIP . '/includes/SMW_Infolink.php');
 
 $dbr =& wfGetDB( DB_MASTER );
@@ -41,21 +46,35 @@ if ( array_key_exists( 'e', $options ) ) {
 	$end = min(intval($options['e']), $end);
 }
 
-if (  array_key_exists( 'v', $options ) ) {
-	$verbose = true;
-} else {
+if (  array_key_exists( 'q', $options ) ) {
 	$verbose = false;
+} else {
+	$verbose = true;
+}
+
+if ( array_key_exists( 'h', $options ) ) {
+	$server = $options['h'];
+} else {
+	$server = $wgServer;
+}
+
+if ( ($server == 'http://localhost') || ($server == '') || ($server == '-') ) {
+	global $smwgNamespace;
+	$server = $smwgNamespace;
+	if ($server[0] == '.') {
+		$resolver = Title::makeTitle( NS_SPECIAL, 'URIResolver');
+		$server = "http://" . mb_substr($server, 1);
+	}
+	if ($verbose) print "Trying to construct ping URL with use server parameter given to SMW:\n  $server\nAlternatively, you can specify a publicly reachable server via the parameter -h with this script.\n\n";
 }
 
 $linkCache =& LinkCache::singleton();
-global $wgUser, $wgServer;
-if ($wgServer == 'http://localhost' ) {
-	print "Sorry, you cannot publicly announce a local wiki. Aborting.\n";
-	die;
-}
+global $wgUser;
 
-print "Refreshing all semantic data in the database!\n";
-print "Processing pages from ID $start to ID $end ...\n";
+if ($verbose) {
+	print "Notifying pingthesemanticweb.com of all semantic data in this wiki!\n\n";
+	print "Processing pages from ID $start to ID $end ...\n";
+}
 
 $num_files = 0;
 
@@ -63,23 +82,18 @@ $skin = $wgUser->getSkin();
 for ($id = $start; $id <= $end; $id++) {
 	$title = Title::newFromID($id);
 	if ( ($title === NULL) ) continue;
-	$url = $wgServer . $skin->makeSpecialUrl( 'ExportRDF/' . $title->getPrefixedText() );
+	$url = $server . $skin->makeSpecialUrl( 'ExportRDF/' . $title->getPrefixedText() );
 	if ($verbose) {
 		print "($num_files) Processing page with ID " . $id . " ($url).\n";
+		print ' Pinging http://pingthesemanticweb.com/rest/?url=' . rawurlencode($url) . ' ...';
 	}
-	print " Pinging http://pingthesemanticweb.com/rest/?url=" . rawurlencode($url) . "...";
 	$fp = fopen('http://pingthesemanticweb.com/rest/?url=' . rawurlencode($url), 'r');
 	if ($fp === false) {
-		print " failed.\n";
+		if ($verbose) print " failed.\n";
 	} else {
 		fclose($fp);
-		print " done.\n";
+		if ($verbose) print " done.\n";
 	}
-
-// 		$revision = Revision::newFromTitle( $title );
-// 		if ( $revision === NULL ) continue;
-// 		$wgParser->parse($revision->getText(), $title, $options, true, true, $revision->getID());
-// 		SMWFactbox::storeData($title, true);
 
 	// sleep to be nice to the server
 	if ( ($delay !== false) && (($num_files+1) % 100 === 0) ) {
@@ -89,6 +103,6 @@ for ($id = $start; $id <= $end; $id++) {
 	$linkCache->clear(); // avoid memory leaks
 }
 
-print "$num_files pages pinged.\n";
+if ($verbose) print "$num_files pages pinged.\n";
 
 
