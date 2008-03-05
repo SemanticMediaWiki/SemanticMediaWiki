fb7b4b3207536260a6fb0006ba83f7abd1645a49
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2008-03-05 12:54:52 +0000

    additional urlencoding for property names in URIs (may contain symbols not allowed in XML)

--- includes/export/SMW_Exporter.php
+++ includes/export/SMW_Exporter.php
@@ -185,7 +185,7 @@ class SMWExporter {
 			if ($dv->getNamespace() == SMW_NS_PROPERTY) {
 				$namespace = '&property;';
 				$namespaceid = 'property';
-				$localname = SMWExporter::encodeURI($dv->getTitle()->getDBKey());
+				$localname = SMWExporter::encodeURI(rawurlencode($dv->getTitle()->getDBKey()));
 				if (in_array($localname[0], array('-','0','1','2','3','4','5','6','7','8','9'))) {
 					$namespace = '&wiki;';
 					$namespaceid = 'wiki';
