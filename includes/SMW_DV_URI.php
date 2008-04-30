98f8ea781a9b717fe33d835fccffdec291e22fa6
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2008-04-30 08:31:46 +0000

    quickfixed Bug 13884 as proposed, better generic regexps should be used eventually here

--- includes/SMW_DV_URI.php
+++ includes/SMW_DV_URI.php
@@ -103,7 +103,7 @@ class SMWURIValue extends SMWDataValue {
 					}
 					break;
 				case SMW_URI_MODE_EMAIL:
-					$check = "#^([_a-zA-Z0-9-]+)((\.[_a-zA-Z0-9-]+)*)@([_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*)\.([a-zA-Z]{2,3})$#u";
+					$check = "#^([_a-zA-Z0-9-]+)((\.[_a-zA-Z0-9-]+)*)@([_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*)\.([a-zA-Z]{2,6})$#u";
 					if (!preg_match($check, $value)) {
 						///TODO: introduce error-message for "bad" email
 						$this->addError(wfMsgForContent('smw_baduri', $value));
