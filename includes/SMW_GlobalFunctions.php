6d2b3507a4e773254301febb1dd308be00703167
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2008-07-06 18:45:06 +0000

    version increment

--- includes/SMW_GlobalFunctions.php
+++ includes/SMW_GlobalFunctions.php
@@ -3,7 +3,7 @@
  * Global functions and constants for Semantic MediaWiki.
  */
 
-define('SMW_VERSION','1.2h-SVN');
+define('SMW_VERSION','1.2i-SVN');
 
 // constants for special properties, used for datatype assignment and storage
 define('SMW_SP_HAS_TYPE',1);
