0686b6344618d6e04164ac1d0beaa22602079e65
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2008-08-28 14:58:59 +0000

    typo

--- languages/SMW_Language.php
+++ languages/SMW_Language.php
@@ -7,8 +7,8 @@
 /**
  * This group contains all parts of SMW that relate to localisation and
  * translation.
- * @defgroup SMWDataValues
- * @ingroup SMWLanguage
+ * @defgroup SMWLanguae
+ * @ingroup SMW
  */
 
 /**
