e572d10da0a6e216b40578b960f85105e91386d9
Denny Vrandecic <vrandezo@users.mediawiki.org>
2008-09-04 17:54:08 +0000

    Added Data and Declare keywords

---
+++ includes/SMW_Data.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Introducing a #set parser function as an alternative to
+ * annotationg with the [[p::o]] syntax.
+ * @file
+ * @ingroup SMW
+ * @author Denny Vrandecic 
+ */
+
+global $wgExtensionFunctions, $wgHooks;
+$wgExtensionFunctions[] = 'smwfDataEntry_Setup';
+$wgHooks['LanguageGetMagic'][] = 'smwfDataEntry_Magic';
+
+/**
+ * Hooks the data parser function.
+ */
+function smwfDataEntry_Setup() {
+	global $wgParser;
+	// Set a function hook associating "set" magic word with our function
+	$wgParser->setFunctionHook( 'set', 'smwfDataEntry_Render' );
+}
+
+/**
+ * Add the magic word for the dataparser function. 
+ */
+function smwfDataEntry_Magic( &$magicWords, $langCode ) {
+	// Add the magic word
+	// The first array element is case sensitive, in this case it is not case sensitive
+	// All remaining elements are synonyms for our parser function
+	$magicWords['set'] = array( 0, 'set' );
+	// unless we return true, other parser functions extensions won't get loaded.
+	return true;
+}
+
+/**
+ * Parser function for data, that enables to use a parser function for annotations.
+ * Using the following syntax:
+ * {{#set:
+ *   population = 13000
+ * | area = 396 km²
+ * | sea = Adria
+ * }}
+ * This creates annotations with the properties as stated on the left side, and the
+ * values on the right side.
+ * 
+ * @param[in] &$parser Parser  The current parser
+ * @return nothing
+ */
+function smwfDataEntry_Render( &$parser ) {
+	$params = func_get_args();
+	array_shift( $params ); // we already know the $parser ...
+	foreach ($params as $p)
+		if (trim($p) != "") {
+			$parts = explode("=", trim($p));
+			if (count($parts)==2) {
+				$property = $parts[0];
+				$subject = $parts[1];
+				// Adds the fact to the factbox, which may be problematic in case
+				// the parser gets called several times...
+				if (!SMWFactbox::$semdata) SMWFactBox::initStorage($parser->getTitle());
+				SMWFactbox::addProperty( $property, $subject, false, true );
+			}
+		}
+	return;
+}
---
+++ includes/SMW_TemplateDeclare.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Introducing a #declare parser function in order to declare the arguments
+ * of a template call, i.e. on a template page we say
+ * {{#declare:Author=Author#list|Publisher=editor}}
+ * and then, when the template is included in a page, the values set for the
+ * fields are taken as annotations.
+ * @file
+ * @ingroup SMW
+ * @author Denny Vrandecic
+ */
+
+global $wgExtensionFunctions, $wgHooks;
+$wgExtensionFunctions[] = 'smwfTemplateDeclare_Setup';
+$wgHooks['LanguageGetMagic'][] = 'smwfTemplateDeclare_Magic';
+
+/**
+ * Sets up the declare parser function.
+ */
+function smwfTemplateDeclare_Setup() {
+	global $wgParser;
+	// Set a function hook associating "declare" magic word with our function
+	$wgParser->setFunctionHook( 'declare', 'smwfTemplateDeclare_Render', SFH_OBJECT_ARGS );
+}
+
+/**
+ * Adds the declare magic word.
+ */
+function smwfTemplateDeclare_Magic( &$magicWords, $langCode ) {
+	// Add the magic word
+	// The first array element is case sensitive, in this case it is not case sensitive
+	// All remaining elements are synonyms for our parser function
+	$magicWords['declare'] = array( 0, 'declare' );
+	// unless we return true, other parser functions extensions won't get loaded.
+	return true;
+}
+
+function smwfTemplateDeclare_Render( Parser &$parser, PPFrame $frame, $args ) {
+	if ($frame->isTemplate()) {
+
+		foreach ($args as $arg)
+			if (trim($arg) != "") {
+				$expanded = trim( $frame->expand( $arg ));
+				$parts = explode("=", $expanded, 2);
+				if (count($parts)==1) {
+					$propertystring = $expanded;
+					$argumentname = $expanded;
+				} else {
+					$propertystring = $parts[0];
+					$argumentname = $parts[1];
+				}
+				$property = Title::newFromText( $propertystring, SMW_NS_PROPERTY );
+				//if ($property == null) continue;
+				$argument = $frame->getArgument($argumentname);
+				$valuestring = $frame->expand($argument);
+				if ($property != null) {
+					$type = SMWDataValueFactory::getPropertyObjectTypeID($property);
+					if ($type == "_wpg") {
+						$matches = array();
+						preg_match_all("/\[\[([^\[\]]*)\]\]/", $valuestring, $matches);
+						$objects = $matches[1];
+						if (count($objects) == 0) {
+							if (trim($valuestring) != '') {
+								if (!SMWFactbox::$semdata) SMWFactBox::initStorage($parser->getTitle());
+								SMWFactbox::addProperty( $propertystring, $valuestring, false, true );
+							}
+						} else {
+							foreach ($objects as $object) {
+								if (!SMWFactbox::$semdata) SMWFactBox::initStorage($parser->getTitle());
+								SMWFactbox::addProperty( $propertystring, $object, false, true );
+							}
+						}
+					} else {
+						if (trim($valuestring) != '') {
+							if (!SMWFactbox::$semdata) SMWFactBox::initStorage($parser->getTitle());
+							SMWFactbox::addProperty( $propertystring, $valuestring, false, true );
+						}
+					}
+					$value = SMWDataValueFactory::newPropertyObjectValue($property, $valuestring);
+					//if (!$value->isValid()) continue;
+				}
+			}
+
+	} else {
+	
+		// @todo Save as metadata
+
+	}
+	
+	return;
+}
