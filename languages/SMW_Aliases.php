735326b0288314d9bcbadd3e90f5ebfa4127c888
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-09-06 23:14:53 +0000

    Localisation updates for extension messages from Betawiki (2008-09-07 00:39 CEST)

--- languages/SMW_Aliases.php
+++ languages/SMW_Aliases.php
@@ -25,19 +25,36 @@ $aliases['en'] = array(
 	'WantedProperties' => array( 'WantedProperties' ),
 );
 
-/** Dutch (Nederlands) */
-$aliases['nl'] = array(
-	'Ask' => array( 'Vragen' ),
-	'Browse' => array( 'Bekijken' ),
-	'ExportRDF' => array( 'RDFExporteren' ),
-	'PageProperty' => array( 'Paginaeigenschap' ),
-	'Properties' => array( 'Eigenschappen' ),
-	'SMWAdmin' => array( 'SMWBeheer' ),
-	'SearchByProperty' => array( 'OpEigenschapZoeken' ),
-	'SemanticStatistics' => array( 'SemantischeStatistieken' ),
-	'Types' => array( 'Typen' ),
-	'UnusedProperties' => array( 'OngebruikteEigenschappen' ),
-	'WantedProperties' => array( 'GewensteEigenschappen' ),
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'Ask' => array( 'سؤال' ),
+	'Browse' => array( 'تصفح' ),
+	'ExportRDF' => array( 'تصدير_آر_دي_إف' ),
+	'PageProperty' => array( 'خاصية_الصفحة' ),
+	'Properties' => array( 'خصائص' ),
+	'SMWAdmin' => array( 'إداري_سمو' ),
+	'SearchByProperty' => array( 'بحث_بواسطة_الخاصية' ),
+	'SemanticStatistics' => array( 'إحصاءات_سيمانتيك' ),
+	'Types' => array( 'أنواع' ),
+	'URIResolver' => array( 'محلل_يو_آر_آي' ),
+	'UnusedProperties' => array( 'خصائص_غير_مستخدمة' ),
+	'WantedProperties' => array( 'خصائص_مطلوبة' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'Ask' => array( 'سؤال' ),
+	'Browse' => array( 'تصفح' ),
+	'ExportRDF' => array( 'تصدير_آر_دى_إف' ),
+	'PageProperty' => array( 'خاصية_الصفحة' ),
+	'Properties' => array( 'خصائص' ),
+	'SMWAdmin' => array( 'إدارى_سمو' ),
+	'SearchByProperty' => array( 'بحث_بواسطة_الخاصية' ),
+	'SemanticStatistics' => array( 'إحصاءات_سيمانتيك' ),
+	'Types' => array( 'أنواع' ),
+	'URIResolver' => array( 'محلل_يو_آر_آى' ),
+	'UnusedProperties' => array( 'خصائص_غير_مستخدمة' ),
+	'WantedProperties' => array( 'خصائص_مطلوبة' ),
 );
 
 /** German (Deutsch) */
@@ -54,3 +71,24 @@ $aliases['de'] = array(
 	'UnusedProperties' => array( 'Verwaiste Attribute' ),
 	'WantedProperties' => array( 'Gewünschte Attribute' ),
 );
+
+/** Luxembourgish (Lëtzebuergesch) */
+$aliases['lb'] = array(
+	'Ask' => array( 'Froen' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'Ask' => array( 'Vragen' ),
+	'Browse' => array( 'Bekijken' ),
+	'ExportRDF' => array( 'RDFExporteren' ),
+	'PageProperty' => array( 'Paginaeigenschap' ),
+	'Properties' => array( 'Eigenschappen' ),
+	'SMWAdmin' => array( 'SMWBeheer' ),
+	'SearchByProperty' => array( 'OpEigenschapZoeken' ),
+	'SemanticStatistics' => array( 'SemantischeStatistieken' ),
+	'Types' => array( 'Typen' ),
+	'UnusedProperties' => array( 'OngebruikteEigenschappen' ),
+	'WantedProperties' => array( 'GewensteEigenschappen' ),
+);
+
