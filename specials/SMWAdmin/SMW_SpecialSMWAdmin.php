7bd907ee1895a9e8427f598d348f6edce2280f13
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2008-11-19 14:32:13 +0000

    fixes in HTML

--- specials/SMWAdmin/SMW_SpecialSMWAdmin.php
+++ specials/SMWAdmin/SMW_SpecialSMWAdmin.php
@@ -108,21 +108,21 @@ class SMWAdmin extends SpecialPage {
 		// creating tables and converting contents from older versions
 		$html .= '<form name="buildtables" action="" method="POST">' . "\n" .
 				'<input type="hidden" name="action" value="updatetables" />' . "\n";
-		$html .= '<h2>Preparing database for Semantic MediaWiki</h2>' . "\n" .
+		$html .= '<br /><h2>Database Installation and Upgrade</h2>' . "\n" .
 				'<p>Semantic MediaWiki requires some extensions to the MediaWiki database in 
 				order to store the semantic data. The below function ensures that your database is
 				set up properly. The changes made in this step do not affect the rest of the 
 				MediaWiki database, and can easily be undone if desired. This setup function
 				can be executed multiple times without doing any harm, but it is needed only once on
-				installation or upgrade.<p/>' . "\n";
+				installation or upgrade.</p>' . "\n";
 		$html .= '<p>If the operation fails with SQL errors, the database user employed 
 				by your wiki (check your LocalSettings.php) probably does not have sufficient 
 				permissions. Either grant this user additional persmissions to create and delete 
-				tables, temporarily enter the login of your database root in LocalSettings.php, or use the maintenance script <tt>SMW_setup.php</tt> which can use the credentials of AdminSettings.php.<p/>' .
+				tables, temporarily enter the login of your database root in LocalSettings.php, or use the maintenance script <tt>SMW_setup.php</tt> which can use the credentials of AdminSettings.php.</p>' .
 				"\n" . '<input type="hidden" name="udsure" value="yes"/>' .
 				'<input type="submit" value="Initialise or upgrade tables"/></form>' . "\n";
 
-		$html .= '<h2>Announce your wiki</h2>' . "\n" . 
+		$html .= '<br /><h2>Announce Your Wiki</h2>' . "\n" .
 				'<p>SMW has a web service for announcing new semantic wiki sites. This is used to maintain a list of public sites that use SMW, mainly to help the <a href="http://semantic-mediawiki.org/wiki/SMW_Project">SMW project</a> to get an overview of typical uses of SMW. See the SMW homepage for <a href="http://semantic-mediawiki.org/wiki/Registry">further information about this service.</a></p>' .
 				'<p>Press the following button to submit your wiki URL to that service. The service will not register wikis that are not publicly accessible, and it will only store publicly accessible information.</p>
 				 <form name="announcewiki" action="http://semantic-mediawiki.org/wiki/Special:SMWRegistry" method="GET">' .
@@ -130,11 +130,11 @@ class SMWAdmin extends SpecialPage {
 				 '<input type="hidden" name="return" value="Special:SMWAdmin" />' .
 				 '<input type="submit" value="Announce wiki"/></form>' . "\n";
 
-		$html .= '<h2>Repair and Upgrade</h2>' . "\n" .
+		$html .= '<br /><h2>Data Repair and Upgrade</h2>' . "\n" .
 				'<p>It is possible to restore all SMW data based on the current contents of the wiki. This can be useful to repair broken data or to refresh the data if the internal format has changed due to some software upgrade. The update is executed page by page and will not be completed immediately. The following shows if an update is in progress and allows you to start or stop upates (unless this feature was disabled by the site administrator).</p>';
 		if ($refreshjob !== NULL) {
 			$prog = $refreshjob->getProgress();
-			$html .= "<p><b>An update is already in progress.</b> It is normal that the update progresses only slowly since it only refreshes data in small chunks each time a user accesses the wiki. To finish this update more quickly, you can invoke the MediaWiki maintenance script <tt>runJobs.php</tt> (use the option <tt>--maxjobs 2000</tt> to restrict the number of updates done at once). Estimated progress of current update:</p> " .
+			$html .= "<p><b>An update is already in progress.</b> It is normal that the update progresses only slowly since it only refreshes data in small chunks each time a user accesses the wiki. To finish this update more quickly, you can invoke the MediaWiki maintenance script <tt>runJobs.php</tt> (use the option <tt>--maxjobs 1000</tt> to restrict the number of updates done in one batch). Estimated progress of current update:</p> " .
 			'<p><div style="float: left; background: #DDDDDD; border: 1px solid grey; width: 300px; "><div style="background: #AAF; width: ' .
 				round($prog*300) . 'px; height: 20px; "> </div></div> &nbsp;' . round($prog*100,4) . '%</p><br /><br />';
 			if ($smwgAdminRefreshStore) {
@@ -155,14 +155,14 @@ class SMWAdmin extends SpecialPage {
 		}
 
 
-		$html .= '<h2>Getting support</h2>' . "\n" . 
+		$html .= '<br /><h2>Getting Support</h2>' . "\n" . 
 				'<p>Various resources might help you in case of problems:</p>
 				<ul>
 				<li> If you experience problems with your installation, start by checking the guidelines in the <a href="http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticMediaWiki/INSTALL">INSTALL file</a>.</li>
 				<li>The complete user documentation to Semantic MediaWiki is at <b><a href="http://semantic-mediawiki.org">semantic-mediawiki.org</a></b>.</li>
 				<li>Bugs can be reported to <a href="http://bugzilla.wikimedia.org/">MediaZilla</a>.</li>
 				<li>If you have further questions or suggestions, join the discussion on <a href="mailto:semediawiki-user@lists.sourceforge.net">semediawiki-user@lists.sourceforge.net</a>.</li>
-				<ul/>' . "\n";
+				</ul>' . "\n";
 
 		$wgOut->addHTML($html);
 	}
