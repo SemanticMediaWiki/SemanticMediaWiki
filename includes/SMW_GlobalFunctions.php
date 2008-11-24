d4ce2748ee1d6744190b25a2184d41cc681e05b4
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2008-11-24 09:23:08 +0000

    version increment

--- includes/SMW_GlobalFunctions.php
+++ includes/SMW_GlobalFunctions.php
@@ -14,7 +14,7 @@
  * @defgroup SMW Semantic MediaWiki
  */
 
-define('SMW_VERSION','1.4.0');
+define('SMW_VERSION','1.5a-SVN');
 
 // constants for displaying the factbox
 define('SMW_FACTBOX_HIDDEN', 1);
