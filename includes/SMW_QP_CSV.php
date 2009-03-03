fc13d01712358f9cb476ca22e4cd4319bfa310f9
Yaron Koren <yaron@users.mediawiki.org>
2009-03-03 20:31:26 +0000

    Added handling for 'headers' and 'mainlabel' parameters; this format will now return, by default, a header row at the top

--- includes/SMW_QP_CSV.php
+++ includes/SMW_QP_CSV.php
@@ -35,6 +35,13 @@ class SMWCsvResultPrinter extends SMWResultPrinter {
 		$result = '';
 		if ($outputmode == SMW_OUTPUT_FILE) { // make CSV file
 			$csv = fopen('php://memory', 'r+');
+			if ( $this->mShowHeaders == true ) {
+				$header_items = array();
+				foreach ($res->getPrintRequests() as $pr) {
+					$header_items[] = $pr->getLabel();
+				}
+				fputcsv($csv, $header_items, $this->m_sep);
+			}
 			while ( $row = $res->getNext() ) {
 				$row_items = array();
 				foreach ($row as $field) {
@@ -62,6 +69,12 @@ class SMWCsvResultPrinter extends SMWResultPrinter {
 			$link = $res->getQueryLink($label);
 			$link->setParameter('csv','format');
 			$link->setParameter($this->m_sep,'sep');
+			if (array_key_exists('mainlabel', $this->m_params))
+				$link->setParameter($this->m_params['mainlabel'],'mainlabel');
+			if ($this->mShowHeaders)
+				$link->setParameter('show', 'headers');
+			else
+				$link->setParameter('hide', 'headers');
 			if (array_key_exists('limit', $this->m_params)) {
 				$link->setParameter($this->m_params['limit'],'limit');
 			} else { // use a reasonable default limit
