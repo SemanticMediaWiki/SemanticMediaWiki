114c0e90e183df9e3a28d93c897e10c45144cb99
Chad Horohoe <demon@users.mediawiki.org>
2009-04-29 06:22:04 +0000

    Kill some more $wgTitles

--- includes/SMW_RefreshTab.php
+++ includes/SMW_RefreshTab.php
@@ -16,14 +16,14 @@ $wgHooks[ 'SkinTemplateTabs' ][] = 'smwfAddRefreshTab';
  * Adds an action that refreshes the article, i.e. it purges the article from
  * the cache and thus refreshes the inline queries.
  */
-function smwfAddRefreshTab($obj, $content_actions) {
-	global $wgUser, $wgTitle;
+function smwfAddRefreshTab($skin, $content_actions) {
+	global $wgUser;
 	if($wgUser->isAllowed('delete')){
 		wfLoadExtensionMessages('SemanticMediaWiki');
 		$content_actions['purge'] = array(
 			'class' => false,
 			'text' => wfMsg('smw_purge'),
-			'href' => $wgTitle->getLocalUrl( 'action=purge' )
+			'href' => $skin->mTitle->getLocalUrl( 'action=purge' )
 		);
 	}
 	return true; // always return true, in order not to stop MW's hook processing!
