d7e67e5b63eece942efbd93d2cf484b372e8d23b
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2009-08-04 18:33:40 +0000

    this is SMW 1.4.3

--- includes/SMW_GlobalFunctions.php
+++ includes/SMW_GlobalFunctions.php
@@ -14,7 +14,7 @@
  * @defgroup SMW Semantic MediaWiki
  */
 
-define('SMW_VERSION','1.5e-SVN');
+define('SMW_VERSION','1.4.3');
 
 // constants for displaying the factbox
 define('SMW_FACTBOX_HIDDEN', 1);
