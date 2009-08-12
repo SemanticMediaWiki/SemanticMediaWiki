9a01eac4abbdea170ca8eb79e4cfe138ed4b410b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-08-12 14:18:40 +0000

    Fix syntax error from r54850

--- languages/SMW_Magic.php
+++ languages/SMW_Magic.php
@@ -3,13 +3,13 @@
 $magicWords = array();
 
 $magicWords['en'] = array(
-	'ask'                 => array( 0, 'ask' );
-	'show'                => array( 0, 'show' );
-	'info'                => array( 0, 'info' );
-	'concept'             => array( 0, 'concept' );
-	'set'                 => array( 0, 'set' );
-	'set_recurring_event' => array( 0, 'set_recurring_event' );
-	'declare'             => array( 0, 'declare' );
-	'SMW_NOFACTBOX'       => array( 0, '__NOFACTBOX__' );
-	'SMW_SHOWFACTBOX'     => array( 0, '__SHOWFACTBOX__' );
+	'ask'                 => array( 0, 'ask' ),
+	'show'                => array( 0, 'show' ),
+	'info'                => array( 0, 'info' ),
+	'concept'             => array( 0, 'concept' ),
+	'set'                 => array( 0, 'set' ),
+	'set_recurring_event' => array( 0, 'set_recurring_event' ),
+	'declare'             => array( 0, 'declare' ),
+	'SMW_NOFACTBOX'       => array( 0, '__NOFACTBOX__' ),
+	'SMW_SHOWFACTBOX'     => array( 0, '__SHOWFACTBOX__' )
 );
