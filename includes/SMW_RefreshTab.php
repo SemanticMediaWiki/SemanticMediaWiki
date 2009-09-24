eebb97447f704108a54ad6ae09e8ae4cd78c3ebf
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2009-09-24 08:11:02 +0000

    updated refreshtab code for Vector skin

--- includes/SMW_RefreshTab.php
+++ includes/SMW_RefreshTab.php
@@ -10,22 +10,34 @@
  */
 if (!defined('MEDIAWIKI')) die();
 global $wgHooks;
-$wgHooks[ 'SkinTemplateTabs' ][] = 'smwfAddRefreshTab';
+$wgHooks[ 'SkinTemplateTabs' ][] = 'smwfAddRefreshTab'; // basic tab addition
+$wgHooks[ 'SkinTemplateNavigation' ][] = 'smwfAddStructuredRefreshTab'; // structured version for "Vector"-type skins
 
 /**
- * Adds an action that refreshes the article, i.e. it purges the article from
- * the cache and thus refreshes the inline queries.
+ * Extends the provided array of content actions with an action that refreshes the article,
+ * i.e. it purges the article from the cache and thus refreshes the inline queries.
  */
-function smwfAddRefreshTab($skin, $content_actions) {
+function smwfAddRefreshTab($skin, &$content_actions) {
 	global $wgUser;
-	if($wgUser->isAllowed('delete')){
+ 	if($wgUser->isAllowed('delete')){
 		wfLoadExtensionMessages('SemanticMediaWiki');
 		$content_actions['purge'] = array(
 			'class' => false,
 			'text' => wfMsg('smw_purge'),
 			'href' => $skin->mTitle->getLocalUrl( 'action=purge' )
 		);
-	}
+ 	}
 	return true; // always return true, in order not to stop MW's hook processing!
+}
+
+/**
+ * Adds the refresh action like smwfAddRefreshTab(), but places it into
+ * the structure of actions as used in new "Vector"-type skins
+ */
+function smwfAddStructuredRefreshTab($skin, &$links) {
+	$actions = $links['actions'];
+	smwfAddRefreshTab($skin, $actions);
+	$links['actions'] = $actions;
+	return true;
 }
 
