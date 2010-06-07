560841e20a0b615e9e673b9243593c4d7759312c
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2010-06-07 00:52:58 +0000

    Follow up to r67451

--- SMW_Settings.php
+++ SMW_Settings.php
@@ -23,7 +23,8 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 # web. Change it if required ($wgScriptPath is the path to the base directory
 # of your wiki). No final slash.
 ##
-$smwgScriptPath = ( isset( $wgExtensionAssetsPath ) && $wgExtensionAssetsPath ? $wgExtensionAssetsPath : $wgScriptPath . '/extensions' ) . '/SemanticMediaWiki';
+$useExtensionPath = version_compare( $wgVersion, '1.16', '>=' ) && isset( $wgExtensionAssetsPath ) && $wgExtensionAssetsPath;
+$smwgScriptPath = ( $useExtensionPath ? $wgExtensionAssetsPath : $wgScriptPath . '/extensions' ) . '/SemanticMediaWiki';
 ##
 
 ###
