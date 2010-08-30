4d482a0b4f402403cdb9e5604e5d944298d929b8
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2010-08-30 20:28:34 +0000

    Updated version to SMW 1.5.2, will release soon

--- includes/SMW_Setup.php
+++ includes/SMW_Setup.php
@@ -5,7 +5,7 @@
  * @ingroup SMW
  */
 
-define( 'SMW_VERSION', '1.5.2 rc1' );
+define( 'SMW_VERSION', '1.5.2' );
 define( 'SEMANTIC_EXTENSION_TYPE', true );
 
 require_once( 'SMW_GlobalFunctions.php' );
--- includes/SMW_SetupLight.php
+++ includes/SMW_SetupLight.php
@@ -14,7 +14,7 @@
  * @ingroup SMW
  */
 
-define( 'SMW_VERSION', '1.5.2-light rc1' );
+define( 'SMW_VERSION', '1.5.2-light' );
 
 require_once( 'SMW_GlobalFunctions.php' );
 
