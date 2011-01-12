8f8f1596a7db60bcb00b8681423879a07a012ecf
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-01-12 17:43:50 +0000

    1.5.5 rc1

--- includes/SMW_Setup.php
+++ includes/SMW_Setup.php
@@ -8,7 +8,7 @@
  */
 
 // The SMW version number.
-define( 'SMW_VERSION', '1.5.5 alpha' );
+define( 'SMW_VERSION', '1.5.5 rc' );
 
 // A flag used to indicate SMW defines a semantic extension type for extension crdits.
 define( 'SEMANTIC_EXTENSION_TYPE', true );
--- includes/SMW_SetupLight.php
+++ includes/SMW_SetupLight.php
@@ -14,7 +14,7 @@
  * @ingroup SMW
  */
 
-define( 'SMW_VERSION', '1.5.4-light alpha' );
+define( 'SMW_VERSION', '1.5.5-light rc' );
 
 require_once( 'SMW_GlobalFunctions.php' );
 
