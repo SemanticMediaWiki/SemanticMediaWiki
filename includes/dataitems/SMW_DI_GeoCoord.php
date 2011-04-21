d7aff0e943f73d41a9589dd607106f07918a8d94
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-04-21 18:05:51 +0000

    follow up to r86648, typo fix

--- includes/dataitems/SMW_DI_GeoCoord.php
+++ includes/dataitems/SMW_DI_GeoCoord.php
@@ -11,7 +11,7 @@
  * @licence GNU GPL v3
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-class SMDIGeoCoord extends SMWDataItem {
+class SMWDIGeoCoord extends SMWDataItem {
 
 	protected $coordinateSet;
 
