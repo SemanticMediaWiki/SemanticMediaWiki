71dfb898bf21b29796652aa85c41419ab9f4a99a
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2011-04-22 09:38:15 +0000

    wfWarn() not available at this point; simply issue a PHP Notice if someone still uses this outdated file

--- includes/SMW_Settings.php
+++ includes/SMW_Settings.php
@@ -2,7 +2,8 @@
 
 /**
  * File for backward compatibility with pre SMW 1.5.1.
- * This used to be the main entrypoint for SMW, now moved to SemanticMediaWiki.php
+ * This used to be the main entrypoint for SMW, now moved to
+ * SemanticMediaWiki.php
  *
  * @file SMW_Settings.php
  * @ingroup SMW
@@ -10,6 +11,11 @@
  * @author Jeroen De Dauw
  */
 
-wfWarn( 'Use of outdated SMW entry point. Use SemanticMediaWiki/SemanticMediaWiki.php instead of SemanticMediaWiki/includes/SMW_Setup.php' );
+if ( !defined( 'MEDIAWIKI' ) ) {
+  die( "This file is part of the Semantic MediaWiki extension. It is not a valid entry point.\n" );
+}
+
+// Could be promoted to Warning one version before really removing this file
+trigger_error( 'Outdated SMW entry point. Use SemanticMediaWiki/SemanticMediaWiki.php instead of SemanticMediaWiki/includes/SMW_Setup.php', E_USER_NOTICE );
 
 require_once dirname( __FILE__ ) . '/../SemanticMediaWiki.php';
