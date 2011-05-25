c5067944dbef9b204f1ca108489106d369dcc13a
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2011-05-25 07:35:23 +0000

    minor style adjustments

--- includes/SMW_QueryLanguage.php
+++ includes/SMW_QueryLanguage.php
@@ -70,18 +70,16 @@ final class SMWQueryLanguage {
 	public static function getStringForComparator( $comparator ) {
 		self::initializeComparators();
 		static $reverseCache = false;
-		
+
 		if ( $reverseCache === false ) {
 			$reverseCache = array_flip( self::$comparators );
 		}
 
 		if ( $comparator == SMW_CMP_EQ ) {
 			return '';
-		}
-		else if ( array_key_exists( $comparator, $reverseCache ) ) {
+		} elseif ( array_key_exists( $comparator, $reverseCache ) ) {
 			return $reverseCache[$comparator];
-		}
-		else {
+		} else {
 			throw new Exception( "Comparator $comparator does not have a string representatation" );
 		}
 	}
@@ -94,7 +92,7 @@ final class SMWQueryLanguage {
 	protected static function initializeComparators() {
 		global $smwgQComparators, $smwStrictComparators;
 		static $initialized = false;
-		
+
 		if ( $initialized ) {
 			return;
 		}
@@ -113,7 +111,7 @@ final class SMWQueryLanguage {
 			'!' => SMW_CMP_NEQ,
 			'~' => SMW_CMP_LIKE,
 		);
-		
+
 		$allowedComparators = explode( '|', $smwgQComparators );		
 		
 		// Remove the comparators that are not allowed.
