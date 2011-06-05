16b8b11786fc6636b8fe79fc307258af2f09036e
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-06-05 21:09:50 +0000

    attempt to fix factbox issue reported on mailing list

--- includes/dataitems/SMW_DI_Property.php
+++ includes/dataitems/SMW_DI_Property.php
@@ -331,7 +331,7 @@ class SMWDIProperty extends SMWDataItem {
 				'_MDAT'  =>  array( '_dat', false ), // "modification date"
 				'_ERRP'  =>  array( '_wpp', false ), // "has improper value for"
 				'_LIST'  =>  array( '__pls', true ), // "has fields"
-				'_SKEY'  =>  array( '__key', true ), // sort key of a page
+				'_SKEY'  =>  array( '__key', false ), // sort key of a page
 				'_SF_DF' => array( '__spf', true ), // Semantic Form's default form property
 				'_SF_AF' => array( '__spf', true ),  // Semantic Form's alternate form property
 			);
