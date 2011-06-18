8a7c14c72660f939af07e36dd92f1212c1b06b8f
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-06-18 18:24:21 +0000

    applied DE i18n patch by DaSch

--- languages/SMW_LanguageDe.php
+++ languages/SMW_LanguageDe.php
@@ -45,6 +45,7 @@ class SMWLanguageDe extends SMWLanguage {
 		'_anu' => 'URI-Annotation',  // name of the annotation URI type (OWL annotation property)
 		'_tel' => 'Telefonnummer', // name of the telephone number URI datatype
 		'_rec' => 'Verbund', // name of record data type
+		'_qty' => 'Menge', // name of record data type
 	);
 	
 	protected $m_DatatypeAliases = array(
