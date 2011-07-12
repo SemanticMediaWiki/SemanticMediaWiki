73ea571ac8c46a1d244c475cb2123ba54b0d1d75
Devayon Das <devayon@users.mediawiki.org>
2011-07-12 20:50:05 +0000

    minor fix to JS

--- specials/AskSpecial/SMW_QueryUIHelper.php
+++ specials/AskSpecial/SMW_QueryUIHelper.php
@@ -448,9 +448,9 @@ jQuery(document).ready(function(){
 		minLength: 2,
 		source: function(request, response) {
 			request.term=request.term.substr(request.term.lastIndexOf("\\n")+1);
-			url=wgScriptPath+'/api.php?action=opensearch&limit=10&namespace='+wgNamespaceIds['property']+'&format=jsonfm&search=';
+			url=wgScriptPath+'/api.php?action=opensearch&limit=10&namespace='+wgNamespaceIds['property']+'&format=jsonfm';
 
-			jQuery.getJSON(url+request.term, function(data){
+			jQuery.getJSON(url, 'search='+request.term, function(data){
 				//remove the namespace prefix 'Property:' from returned data and add prefix '?'
 				for(i=0;i<data[1].length;i++) data[1][i]="?"+data[1][i].substr(data[1][i].indexOf(':')+1);
 				response(jQuery.ui.autocomplete.filter(data[1], escapeQuestion(extractLast(request.term))));
