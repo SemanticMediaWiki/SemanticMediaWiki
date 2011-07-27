5b246b709e5f5cc7ada3d7a0bd7b2f8b840b83eb
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-07-27 16:02:24 +0000

    fu r93276

--- specials/SearchTriple/SMW_SpecialBrowse.php
+++ specials/SearchTriple/SMW_SpecialBrowse.php
@@ -354,6 +354,7 @@ class SMWSpecialBrowse extends SpecialPage {
 	 *
 	 * @param[in] $property SMWPropertyValue  The property of interest
 	 * @param[in] $incoming bool  If it is an incoming property
+	 * 
 	 * @return string  The label of the property
 	 */
 	private function getPropertyLabel( SMWPropertyValue $property, $incoming = false ) {
@@ -361,7 +362,7 @@ class SMWSpecialBrowse extends SpecialPage {
 		
 		if ( $incoming && $smwgBrowseShowInverse ) {
 			$oppositeprop = SMWPropertyValue::makeUserProperty( wfMsg( 'smw_inverse_label_property' ) );
-			$labelarray = &smwfGetStore()->getPropertyValues( $property->getDataItem(), $oppositeprop );
+			$labelarray = &smwfGetStore()->getPropertyValues( $property->getDataItem(), $oppositeprop->getDataItem() );
 			$rv = ( count( $labelarray ) > 0 ) ? $labelarray[0]->getLongWikiText():
 			       wfMsg( 'smw_inverse_label_default', $property->getWikiValue() );
 		} else {
