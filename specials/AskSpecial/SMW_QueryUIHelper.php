64cd0ca4d511965c9dc1acd16f959a0a2767de6b
Devayon Das <devayon@users.mediawiki.org>
2011-08-03 11:21:53 +0000

    Follow up: r92530

--- specials/AskSpecial/SMW_QueryUIHelper.php
+++ specials/AskSpecial/SMW_QueryUIHelper.php
@@ -89,7 +89,10 @@ abstract class SMWQueryUI extends SpecialPage {
 						and ( method_exists( $wgOut, 'addFeedlink' ) ) // remove this line after MW 1.5 is no longer supported by SMW
 						and ( array_key_exists( 'rss', $wgFeedClasses ) ) ) {
 					$res = $this->uiCore->getResultObject();
-					$href = $res->getQueryLink()->getURl() . '/format%3Drss/limit%3D' . $this->uiCore->getLimit();
+					$href = $res->getQueryLink();
+					$href->setParameter( 'rss', 'format' );
+					$href->setParameter( $this->uiCore->getLimit(), 'limit' );
+					$href = $href->getURl();
 					$wgOut->addFeedLink( 'rss', $href );
 				}
 
