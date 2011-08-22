00989d1bcce78fed5edb3b61624e5f708e79782d
Niklas Laxström <nikerabbit@users.mediawiki.org>
2011-08-22 08:25:20 +0000

    Followup r95172, use the getter instead of nonexistent member variable

--- includes/SMW_Outputs.php
+++ includes/SMW_Outputs.php
@@ -117,7 +117,7 @@ class SMWOutputs {
 	 */
 	static public function requireFromParserOutput( ParserOutput $parserOutput ) {
 		// Note: we do not attempt to recover which head items where scripts here.
-		self::$headItems = array_merge( (array)self::$headItems, (array)$parserOutput->headItems );
+		self::$headItems = array_merge( (array)self::$headItems, $parserOutput->getHeadItems() );
 		if ( isset( $parserOutput->mModules ) ) {
 			self::$headItems = array_merge( (array)self::$resourceModules, (array)$parserOutput->mModules );
 		}
