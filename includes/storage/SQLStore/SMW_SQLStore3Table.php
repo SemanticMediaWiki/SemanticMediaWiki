fb974fb9426be8595e18a71bca41801882068d43
nischayn22 <nischayn22@gmail.com>
2012-08-13 02:29:01 +0530

    Fix handling of Container objects.
    The following code introduces new handling method of Container DI objects,
    SemanticData stores Containers in an array instead of passing them around as property-values, the writers handle these Subobjects separately on their own.
    The code is divided into small functions to make things cleaner and self-explanatory.
    
    This also fixes some very minor errors in previous commits
    
    Patchset 4: Not deleting smw_ids, these need special code which has to
    come in a separate commit. Not replacing DIContainers with DIWikiPages
    as this leads to difficult handling when removing Containers. Removing
    DIContainers is changed as per comments.
    
    Patchset 5: Containers are replaced with WikiPages after their data is
    copied to subSemanticData, all methods handle subSemanticData too.
    
    Patchset 6: Fix small errors with the code, proper subject compatibility
    checks
    '''Tested to work fine'''
    
    Change-Id: Ib407a10fe7c637b151ea9e342eb9bcb788588eed

--- includes/storage/SQLStore/SMW_SQLStore3Table.php
+++ includes/storage/SQLStore/SMW_SQLStore3Table.php
@@ -73,7 +73,7 @@ class SMWSQLStore3Table {
 	*
 	* @return array
 	*/
-	public function getFields( $store ) {
+	public function getFields( SMWSQLStore3 $store ) {
 		$diHandler = $store->getDataItemHandlerForDIType( $this->diType );
 		return $diHandler->getTableFields();
 	}
