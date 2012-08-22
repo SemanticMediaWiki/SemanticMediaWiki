823da72a9034acff885a7248198b3ddd1fe35cd7
nischayn22 <nischayn22@gmail.com>
2012-08-22 09:40:57 +0530

    using smw_stats in Special Pages
    Patchset2 : using join_conds in query
    
    Patchset3 : getUnusedProperties is made a special case of getProperties
    
    Change-Id: I79317d2325896ad791f97de4cbf311fd23c94d28

--- includes/storage/SQLStore/SMW_SQLStore3_SpecialPageHandlers.php
+++ includes/storage/SQLStore/SMW_SQLStore3_SpecialPageHandlers.php
@@ -30,9 +30,13 @@ Class SMWSQLStore3SpecialPageHandlers {
 	/**
 	 * Implementation of SMWStore::getPropertiesSpecial(). It works by
 	 * querying for all properties in smw_ids by identifying from the
-	 * namespace and then counting their usage one by one by querying their tables.
+	 * namespace and then getting their usage from the smw_stats table.
+	 *
+	 * Note: $extraConds specify filtering on properties, if given the result
+	 * will be array of DIWikiPages only. 
+	 *
 	 */
-	public function getPropertiesSpecial( $requestoptions = null ) {
+	public function getPropertiesSpecial( $requestoptions = null, $extraConds = array() ) {
 		wfProfileIn( "SMWSQLStore3::getPropertiesSpecial (SMW)" );
 		$dbr = wfGetDB( DB_SLAVE );
 		// the query needs to do the filtering of internal properties, else LIMIT is wrong
@@ -45,135 +49,41 @@ Class SMWSQLStore3SpecialPageHandlers {
 			}
 		}
 
-		//TODO - Proper sortkeys for special Properties created by other extensions (this goes wrong somewhere in makeSMWPageID())
+		$conds = array( 'smw_namespace' => SMW_NS_PROPERTY, 'smw_iw' => '' );
+		if( $extraConds != array() ) {
+			$conds = array_merge( $conds, $extraConds );
+		}
 		$res = $dbr->select(
-				'smw_ids',
-				array( 'smw_id', 'smw_title', 'smw_sortkey' ),
-				'smw_namespace = ' . $dbr->addQuotes( SMW_NS_PROPERTY ) . ' AND smw_iw = ' .
-					$dbr->addQuotes( '' ),
+				array( 'smw_ids', 'smw_stats' ),
+				array( 'smw_title', 'usage_count' ),
+				$conds,
 				__METHOD__,
-				$options
+				$options,
+				array( 'smw_ids' => array( 'INNER JOIN', array( 'smw_id=pid' ) ) )
 		);
 
 		$result = array();
-		$proptables = SMWSQLStore3::getPropertyTables();
-
 		foreach ( $res as $row ) {
-			$di = new SMWDIProperty( $row->smw_title );
-			$tableId = SMWSQLStore3::findPropertyTableID( $di );
-			$proptable = $proptables[$tableId];
-			$propRow = $dbr->selectRow(
-					$proptable->name,
-					'Count(*) as count',
-					$proptable->fixedproperty ? array() : array('p_id' => $row->smw_id ),
-					__METHOD__
-			);
-			$result[] = array( $di, $propRow->count );
+			if( $extraConds == array() ) {
+				$result[] = array( new SMWDIProperty( $row->smw_title ), $row->usage_count );
+			} else {
+				$result[] = new SMWDIProperty( $row->smw_title );
+			}
 		}
 
 		$dbr->freeResult( $res );
 		wfProfileOut( "SMWSQLStore3::getPropertiesSpecial (SMW)" );
-
 		return $result;
 	}
 
 	/**
 	 * Implementation of SMWStore::getUnusedPropertiesSpecial(). It works by
-	 * creating a temporary table with all property pages from which all used
-	 * properties are then deleted. This is still a costy operation, and some
-	 * slower but lessdemanding way of getting at this data is required for
-	 * larger wikis.
+	 * calling getPropertiesSpecial() with additional parameters
 	 */
 	public function getUnusedPropertiesSpecial( $requestoptions = null ) {
-		global $wgDBtype;
-
 		wfProfileIn( "SMWSQLStore3::getUnusedPropertiesSpecial (SMW)" );
-		$dbr = wfGetDB( DB_SLAVE );
-
-		// we use a temporary table for executing this costly operation on the DB side
-		$smw_tmp_unusedprops = $dbr->tableName( 'smw_tmp_unusedprops' );
-		if ( $wgDBtype == 'postgres' ) { // PostgresQL: no in-memory tables available
-			$sql = "CREATE OR REPLACE FUNCTION create_smw_tmp_unusedprops() RETURNS void AS "
-				   . "$$ "
-				   . "BEGIN "
-				   . " IF EXISTS(SELECT NULL FROM pg_tables WHERE tablename='smw_tmp_unusedprops' AND schemaname = ANY (current_schemas(true))) "
-				   . " THEN DELETE FROM " . $smw_tmp_unusedprops . "; "
-				   . " ELSE "
-				   . "  CREATE TEMPORARY TABLE " . $smw_tmp_unusedprops . " ( title text ); "
-				   . " END IF; "
-				   . "END; "
-				   . "$$ "
-				   . "LANGUAGE 'plpgsql'; "
-				   . "SELECT create_smw_tmp_unusedprops(); ";
-		} else { // MySQL: use temporary in-memory table
-			$sql = "CREATE TEMPORARY TABLE " . $smw_tmp_unusedprops . "( title VARCHAR(255) ) ENGINE=MEMORY";
-		}
-
-		$dbr->query( $sql, __METHOD__ );
-
-		$dbr->insertSelect( $smw_tmp_unusedprops, 'page', array( 'title' => 'page_title' ),
-		                  array( "page_namespace" => SMW_NS_PROPERTY ),  __METHOD__ );
-
-		$smw_ids = $dbr->tableName( 'smw_ids' );
-
-		// all tables occurring in some property table are used:
-		foreach ( SMWSQLStore3::getPropertyTables() as $proptable ) {
-			if ( $proptable->fixedproperty == false ) {
-				// MW does not seem to have a wrapper for this:
-				if ( $wgDBtype == 'postgres' ) { // PostgresQL: don't repeat the FROM table in USING
-					$sql = "DELETE FROM $smw_tmp_unusedprops USING " .
-						$dbr->tableName( $proptable->name ) .
-						" INNER JOIN $smw_ids ON p_id=smw_id WHERE" .
-						" title=smw_title AND smw_iw=" . $dbr->addQuotes( '' );
-				} else {
-					$sql = "DELETE FROM $smw_tmp_unusedprops USING " .
-					"$smw_tmp_unusedprops INNER JOIN " . $dbr->tableName( $proptable->name ) .
-					" INNER JOIN $smw_ids ON p_id=smw_id AND title=smw_title" .
-					" AND smw_iw=" . $dbr->addQuotes( '' );
-				}
-				$dbr->query( $sql, __METHOD__ );
-			} // else: todo
-		}
-
-		// properties that have subproperties are considered to be used
-		$propertyTables = SMWSQLStore3::getPropertyTables();
-		$subPropertyTableId = SMWSQLStore3::$special_tables['_SUBP'];
-		$subPropertyTable = $propertyTables[$subPropertyTableId];
-
-		// Again we have no fitting MW wrapper here:
-		if ( $wgDBtype == 'postgres' ) { // PostgresQL: don't repeat the FROM table in USING
-			$sql = "DELETE FROM $smw_tmp_unusedprops USING " .
-				$dbr->tableName( $subPropertyTable->name ) .
-				" INNER JOIN $smw_ids ON o_id=smw_id WHERE title=smw_title";
-		} else {
-			$sql = "DELETE $smw_tmp_unusedprops.* FROM $smw_tmp_unusedprops," .
-				$dbr->tableName( $subPropertyTable->name ) .
-				" INNER JOIN $smw_ids ON o_id=smw_id WHERE title=smw_title";
-		}
-		$dbr->query( $sql, __METHOD__ );
-
-		// properties that are redirects are considered to be used:
-		//   (a stricter and more costy approach would be to delete only redirects to used properties;
-		//    this would need to be done with an addtional query in the above loop)
-		// The redirect table is a fixed part of this store, no need to find its name.
-		$dbr->deleteJoin( $smw_tmp_unusedprops, 'smw_redi', 'title', 's_title', array( 's_namespace' => SMW_NS_PROPERTY ), __METHOD__ );
-
-		$options = $this->store->getSQLOptions( $requestoptions, 'title' );
-		$options['ORDER BY'] = 'title';
-		$res = $dbr->select( $smw_tmp_unusedprops, 'title', '', __METHOD__, $options );
-
-		$result = array();
-
-		foreach ( $res as $row ) {
-			$result[] = new SMWDIProperty( $row->title );
-		}
-
-		$dbr->freeResult( $res );
-
-		$dbr->query( "DROP " . ( $wgDBtype == 'postgres' ? '' : 'TEMPORARY' ) .
-			" TABLE $smw_tmp_unusedprops", __METHOD__ );
+		$result = $this->getPropertiesSpecial( $requestoptions, array( 'usage_count' => 0 ) );
 		wfProfileOut( "SMWSQLStore3::getUnusedPropertiesSpecial (SMW)" );
-
 		return $result;
 	}
 
