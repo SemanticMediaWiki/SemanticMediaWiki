899bc637c008fdb0352511be45f5bb6a385da771
mwjames <jamesin.hongkong.1@gmail.com>
2012-10-09 12:40:28 +0900

    Split $.smwTooltip into two methods (init and add)
    ## Description
    Split $.smwTooltip into two methods (init and add). The add method can be
    used on any element and handles the creation of the icon/content element
    and also initializes the tooltip with a qtip2 instance.
    
    ## Usage
    
    $( ... ).smwTooltip( 'add', {
    	title: ...,
    	type: ...,
    	content:...
    } );
    
    I'd like to use a namespace for this such as smw.tooltip/
    semanticMediawiki.Tooltip but since jquery and namespaces usage sucks
    (in order to allow normal chaining behaviour ) I have to leave this for
    and use fn.smw...
    
    ## Dependency
    on change I88adbae3
    
    Change-Id: Icc9c43e428189a029c589a0869541d3dc4d54251

--- resources/ext.smw.util.tooltip.js
+++ resources/ext.smw.util.tooltip.js
@@ -27,16 +27,28 @@
 	 *
 	 */
 	var defaults = {
-			position: {
-				viewport: $(window),
-				at: 'top center',
-				my: 'bottom center'
+			qtip: {
+				position: {
+					viewport: $(window),
+					at: 'top center',
+					my: 'bottom center'
+				},
+				show: {
+					solo: true
+				},
+				content: {
+						title: {
+							button: false
+						}
+				},
+				style: {
+					classes: 'ui-tooltip-shadow ui-tooltip-bootstrap'
+				}
 			},
-			show: {
-				solo: true
-			},
-			style: {
-				classes: 'ui-tooltip-shadow ui-tooltip-bootstrap'
+			classes: {
+				iconClass: 'smwtticon',
+				contentClass: 'smwttcontent',
+				entityClass: 'smwttpersist'
 			}
 	};
 
@@ -55,20 +67,74 @@
 	 *
 	 * @since: 1.8
 	 */
-	$.fn.smwTooltip = function( options ){
-		this.each( function() {
-			$( this ).qtip( $.extend( {}, defaults, {
-				hide: options.button ? 'unfocus' : undefined,
-				show: { event: options.event, solo: true },
-				content: {
-					text: options.content,
-					title: {
-						text: options.title,
-						button: options.button
+	var methods = {
+		/**
+		 * Init method initializes the qtip2 instance and does run without
+		 * explicitly mentioning this method
+		 *
+		 * Example: $this.smwTooltip( { title: ..., type: ..., content: ..., button: ..., event: ... } );
+		 *
+		 * @since 1.8
+		 */
+		init : function( options ) {
+			return this.each( function() {
+				$( this ).qtip( $.extend( {}, defaults.qtip, {
+					hide: options.button ? 'unfocus' : undefined,
+					show: { event: options.event, solo: true },
+					content: {
+						text: options.content,
+						title: {
+							text: options.title,
+							button: options.button
+						}
 					}
-				}
 			} ) );
-		} );
+			} );
+		},
+
+		/**
+		 * The add method is a convenience method which allows to create a tooltip element
+		 * and create an instance
+		 *
+		 * Example: $this.smwTooltip( 'add', { title: ..., type: ..., content: ... } );
+		 *
+		 * @since 1.8
+		 */
+		add : function( options ) {
+			// Defaults
+			var option = $.extend( true, defaults.classes, options );
+
+			// Add html element
+			var h = mw.html,
+				element = h.element( 'span', { 'class' : option.entityClass },
+				new h.Raw(
+					h.element( 'span', { 'class' : option.iconClass, 'data-type': option.type }, null ) +
+					h.element( 'span', { 'class' : option.contentClass }, new h.Raw( option.content ) ) )
+				);
+
+			// Append elements
+			this.prepend( element );
+
+			// Ensure the rigth scope and use the icon as hoover/click element
+			// The class [] selector is not the fastest but the safest otherwise if spaces are
+			// used in the class definition it will break the selection
+			methods.init.call(
+				this.find( "[class='" + option.iconClass + "']" ),
+				$.extend( true, options, { content: this.find( "[class='" + option.contentClass + "']" ) } )
+			);
+		}
+	};
+
+	// Extends jquery
+	$.fn.smwTooltip = function( method ) {
+
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on smwTooltip' );
+		}
 	};
 
 	/////////////////////////////// DOM //////////////////////////////
