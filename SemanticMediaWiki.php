53ef89476cae5ae53fb350382968f1da8703669a
jeroendedauw <jeroendedauw@gmail.com>
2012-12-02 18:53:38 +0100

    Bump version to 1.8
    Change-Id: I3607eb4f0b3909bcf39d8d8df603d1d42097771b

--- SemanticMediaWiki.php
+++ SemanticMediaWiki.php
@@ -45,7 +45,7 @@ if ( version_compare( Validator_VERSION, '0.5c', '<' ) ) {
 }
 
 // The SMW version number.
-define( 'SMW_VERSION', '1.8 rc1' );
+define( 'SMW_VERSION', '1.8' );
 
 // Registration of the extension credits, see Special:Version.
 $wgExtensionCredits['semantic'][] = array(
