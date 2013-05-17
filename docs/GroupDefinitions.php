9381e0e414ebc8d412602e47df56a22ac2ac9070
mwjames <jamesin.hongkong.1@gmail.com>
2013-05-17 09:01:36 +0900

    Improve doxygen group definition generation
    Change-Id: Ic88db7487863c141bbca05e2c2708eae018685e7

---
+++ docs/GroupDefinitions.php
@@ -0,0 +1,31 @@
+<?php
+
+die("Not a valid entry point\n");
+
+/**
+ * This file does not hold any code. It is used mainly during the doxygen
+ * auto-documentation process
+ *
+ * @file
+ */
+
+/**
+ * This group contains members that are relate to message, error, and parameter formatting
+ *
+ * @defgroup Formatters Formatters
+ * @ingroup SMW
+ */
+
+/**
+ * This group contains members that are relate to parser hooks and functions
+ *
+ * @defgroup ParserFunction ParserFunction
+ * @ingroup SMW
+ */
+
+/**
+ * This group contains members that are relate to unit tests
+ *
+ * @defgroup Test Test
+ * @ingroup SMW
+ */
