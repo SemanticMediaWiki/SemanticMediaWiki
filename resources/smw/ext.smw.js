49fe447c7d5f3ba72af2e1f805d762fc5f177db1
mwjames <jamesin.hongkong.1@gmail.com>
2013-05-25 11:10:39 +0900

    Improve smw constructor
    Change-Id: I4f862761e3a649fdcb9c44358aa09b37d8585f7b

--- resources/smw/ext.smw.js
+++ resources/smw/ext.smw.js
@@ -28,328 +28,322 @@
  * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
  * @author mwjames
  */
-
-/**
- * Declares methods and properties that are available through the smw namespace
- *
- * @since  1.8
- * @class smw
- * @singleton
- */
-var instance = ( function ( $ ) {
+var smw = ( function ( $, undefined ) {
 	'use strict';
 
 	/*global console:true message:true */
 
-	var instance = {};
-
 	/**
-	 * Outputs a debug log
+	 *
+	 * Declares methods and properties that are available through the smw namespace
 	 *
 	 * @since 1.8
-	 *
-	 * @return {string}
+	 * @class smw
+	 * @alternateClassName semanticMediaWiki
+	 * @singleton
 	 */
-	instance.log = function( message ) {
-		if ( typeof mediaWiki === 'undefined' ) {
-			if ( typeof console !== 'undefined' ) {
-				console.log( 'SMW: ', message );
-			}
-		} else {
-			return mediaWiki.log.call( mediaWiki.log, 'SMW: ', message );
-		}
-	};
+	return {
 
-	/**
-	 * Outputs a message
-	 *
-	 * @since 1.8
-	 *
-	 * @return {string}
-	 */
-	instance.msg = function() {
-		if ( typeof mediaWiki === 'undefined' ) {
-			message = window.wgSMWMessages[arguments[0]];
-
-			for ( var i = arguments.length - 1; i > 0; i-- ) {
-				message = message.replace( '$' + i, arguments[i] );
-			}
-			return message;
-		} else {
-			return mediaWiki.msg.apply( mediaWiki.msg, arguments );
-		}
-	};
-
-	/**
-	 * Returns current debug status
-	 *
-	 * @since 1.9
-	 *
-	 * @return {boolean}
-	 */
-	instance.debug = function() {
-		return mediaWiki.config.get( 'debug' );
-	};
-
-	/**
-	 * Returns Semantic MediaWiki version
-	 *
-	 * @since 1.9
-	 *
-	 * @return {string}
-	 */
-	instance.version = function() {
-		return mediaWiki.config.get( 'smw-config' ).version;
-	};
-
-	/**
-	 * Declares methods to access utility functions
-	 *
-	 * @since  1.9
-	 *
-	 * @static
-	 * @class smw.util
-	 * @alias smw.Util
-	 */
-	instance.util = {
+		/* Public Members */
 
 		/**
-		 * Strip some illegal chars: control chars, colon, less than, greater than,
-		 * brackets, braces, pipe, whitespace and normal spaces. This still leaves some insanity
-		 * intact, like unicode bidi chars, but it's a good start..
+		 * Outputs a debug log
 		 *
-		 * Borrowed from mw.Title
+		 * @since 1.8
 		 *
-		 * @ignore
-		 * @param {string} s
 		 * @return {string}
 		 */
-		clean: function ( s ) {
-			if ( s !== undefined ) {
-				return s.trim().replace( /[\x00-\x1f\x23\x3c\x3e\x5b\x5d\x7b\x7c\x7d\x7f\s]+/g, '_' );
+		log: function( message ) {
+			if ( typeof mediaWiki === 'undefined' ) {
+				if ( typeof console !== 'undefined' ) {
+					console.log( 'SMW: ', message );
+				}
+			} else {
+				return mediaWiki.log.call( mediaWiki.log, 'SMW: ', message );
 			}
 		},
 
 		/**
-		 * Capitalizes the first letter of a string
+		 * Outputs a message
 		 *
-		 * @ignore
-		 * @param {string} s
+		 * @since 1.8
+		 *
 		 * @return {string}
 		 */
-		ucFirst: function( s ) {
-			return s.charAt(0).toUpperCase() + s.slice(1);
+		msg: function() {
+			if ( typeof mediaWiki === 'undefined' ) {
+				message = window.wgSMWMessages[arguments[0]];
+
+				for ( var i = arguments.length - 1; i > 0; i-- ) {
+					message = message.replace( '$' + i, arguments[i] );
+				}
+				return message;
+			} else {
+				return mediaWiki.msg.apply( mediaWiki.msg, arguments );
+			}
 		},
 
 		/**
-		 * Declares methods to access information about namespace settings
+		 * Returns current debug status
+		 *
+		 * @since 1.9
+		 *
+		 * @return {boolean}
+		 */
+		debug: function() {
+			return mediaWiki.config.get( 'debug' );
+		},
 
-		 * Example to find localised name:
-		 *         smw.util.namespace.getName( 'property' );
-		 *         smw.util.namespace.getName( 'file' );
+		/**
+		 * Returns Semantic MediaWiki version
+		 *
+		 * @since 1.9
+		 *
+		 * @return {string}
+		 */
+		version: function() {
+			return mediaWiki.config.get( 'smw-config' ).version;
+		},
+
+		/**
+		 * Declares methods to access utility functions
 		 *
 		 * @since  1.9
 		 *
 		 * @static
-		 * @class smw.util.namespace
+		 * @class smw.util
+		 * @alias smw.Util
 		 */
-		namespace: {
+		util: {
 
 			/**
-			 * Returns list of available namespaces
+			 * Strip some illegal chars: control chars, colon, less than, greater than,
+			 * brackets, braces, pipe, whitespace and normal spaces. This still leaves some insanity
+			 * intact, like unicode bidi chars, but it's a good start..
+			 *
+			 * Borrowed from mw.Title
+			 *
+			 * @ignore
+			 * @param {string} s
+			 * @return {string}
+			 */
+			clean: function ( s ) {
+				if ( s !== undefined ) {
+					return s.trim().replace( /[\x00-\x1f\x23\x3c\x3e\x5b\x5d\x7b\x7c\x7d\x7f\s]+/g, '_' );
+				}
+			},
+
+			/**
+			 * Capitalizes the first letter of a string
+			 *
+			 * @ignore
+			 * @param {string} s
+			 * @return {string}
+			 */
+			ucFirst: function( s ) {
+				return s.charAt(0).toUpperCase() + s.slice(1);
+			},
+
+			/**
+			 * Declares methods to access information about namespace settings
+
+			 * Example to find localised name:
+			 *         smw.util.namespace.getName( 'property' );
+			 *         smw.util.namespace.getName( 'file' );
+			 *
+			 * @since  1.9
+			 *
+			 * @static
+			 * @class smw.util.namespace
+			 */
+			namespace: {
+
+				/**
+				 * Returns list of available namespaces
+				 *
+				 * @since 1.9
+				 *
+				 * @return {Object}
+				 */
+				getList: function() {
+					return smw.settings.get( 'namespace' );
+				},
+
+				/**
+				 * Returns namespace Id
+				 *
+				 * @since 1.9
+				 *
+				 * @param {string} key
+				 *
+				 * @return {number}
+				 */
+				getId: function( key ) {
+					if( typeof key === 'string' ) {
+						return this.getList()[ smw.util.ucFirst( smw.util.clean( key ) ) ];
+					}
+					return undefined;
+				},
+
+				/**
+				 * Returns formatted localized name for a selected namespace
+				 *
+				 * @since 1.9
+				 *
+				 * @param {string} key
+				 *
+				 * @return {string}
+				 */
+				getName: function( key ) {
+					if( typeof key === 'string' ) {
+						var id = this.getId( key );
+						return id && mediaWiki.config.get( 'wgFormattedNamespaces' )[id.toString()];
+					}
+					return undefined;
+				}
+			}
+		},
+
+		/**
+		 * Declares methods to improve browser responsiveness by loading
+		 * invoked methods asynchronously using the jQuery.eachAsync plug-in
+		 *
+		 * Example:
+		 *         var fn = function( options ) {};
+		 *         smw.async.load( $( this ), fn, {} );
+		 *
+		 * @since  1.9
+		 *
+		 * @singleton
+		 * @class smw.async
+		 */
+		async: {
+
+			/**
+			 * Returns if eachAsync is available for asynchronous loading
+			 *
+			 * @return {boolean}
+			 */
+			isEnabled: function() {
+				return $.isFunction( $.fn.eachAsync );
+			},
+
+			/**
+			 * Negotiates and executes asynchronous loading
+			 *
+			 * @since  1.9
+			 *
+			 * @param {object} context
+			 * @param {function} method
+			 * @param {object|string} args
+			 *
+			 * @return {boolean}
+			 * @throws {Error} Missing callback
+			 */
+			load: function( context, method ) {
+				if ( typeof method !== 'function' ) {
+					throw new Error( 'Invoked parameter was not a function' );
+				}
+
+				// Filter arguments that are attached to the caller
+				var args = Array.prototype.slice.call( arguments, 2 );
+
+				if ( this.isEnabled() ) {
+					context.eachAsync( {
+						delay: 100,
+						bulk: 0,
+						loop: function() {
+							method.apply( $( this ), args );
+						}
+					} );
+				} else {
+					context.each( function() {
+						method.apply( $( this ), args );
+					} );
+				}
+			}
+		},
+
+		/**
+		 * Declares methods to access information about available formats
+		 *
+		 * @since  1.9
+		 *
+		 * @class smw.formats
+		 * @alias smw.Formats
+		 */
+		formats: {
+
+			/**
+			 * Returns list of available formats
+			 *
+			 * @since 1.9
+			 * @extends smw.formats
+			 *
+			 * @return {Object}
+			 */
+			getList: function() {
+				return mediaWiki.config.get( 'smw-config' ).formats;
+			},
+
+			/**
+			 * Returns localized name for  a select format
+			 *
+			 * @since 1.9
+			 *
+			 * @param {string} format
+			 *
+			 * @return {string}
+			 */
+			getName: function( format ) {
+				if( typeof format === 'string' ){
+					return this.getList()[ smw.util.clean( format ).toLowerCase() ];
+				}
+				return undefined;
+			}
+		},
+
+		/**
+		 * Declares methods to access information about invoked settings (see also
+		 * SMWHooks::onResourceLoaderGetConfigVars)
+		 *
+		 * @since 1.9
+		 *
+		 * @class smw.settings
+		 * @singleton
+		 */
+		settings: {
+
+			/**
+			 * Returns list of available settings
 			 *
 			 * @since 1.9
 			 *
 			 * @return {Object}
 			 */
 			getList: function() {
-				return instance.settings.get( 'namespace' );
+				return mediaWiki.config.get( 'smw-config' ).settings;
 			},
 
 			/**
-			 * Returns namespace Id
+			 * Returns a specific settings value (see SMW\Settings::get)
 			 *
 			 * @since 1.9
 			 *
-			 * @param {string} key
+			 * @param  {string} key to be selected
 			 *
-			 * @return {number}
+			 * @return {mixed}
 			 */
-			getId: function( key ) {
+			get: function( key ) {
 				if( typeof key === 'string' ) {
-					return this.getList()[ instance.util.ucFirst( instance.util.clean( key ) ) ];
-				}
-				return undefined;
-			},
-
-			/**
-			 * Returns formatted localized name for a selected namespace
-			 *
-			 * @since 1.9
-			 *
-			 * @param {string} key
-			 *
-			 * @return {string}
-			 */
-			getName: function( key ) {
-				if( typeof key === 'string' ) {
-					var id = this.getId( key );
-					return id && mediaWiki.config.get( 'wgFormattedNamespaces' )[id.toString()];
+					return this.getList()[key];
 				}
 				return undefined;
 			}
 		}
 	};
-
-	/**
-	 * Declares methods to improve browser responsiveness by loading
-	 * invoked methods asynchronously using the jQuery.eachAsync plug-in
-	 *
-	 * Example:
-	 *         var fn = function( options ) {};
-	 *         smw.async.load( $( this ), fn, {} );
-	 *
-	 * @since  1.9
-	 *
-	 * @singleton
-	 * @class smw.async
-	 */
-	instance.async = {
-
-		/**
-		 * Returns if eachAsync is available for asynchronous loading
-		 *
-		 * @return {boolean}
-		 */
-		isEnabled: function() {
-			return $.isFunction( $.fn.eachAsync );
-		},
-
-		/**
-		 * Negotiates and executes asynchronous loading
-		 *
-		 * @since  1.9
-		 *
-		 * @param {object} context
-		 * @param {function} method
-		 * @param {object|string} args
-		 *
-		 * @return {boolean}
-		 * @throws {Error} Missing callback
-		 */
-		load: function( context, method ) {
-			if ( typeof method !== 'function' ) {
-				throw new Error( 'Invoked parameter was not a function' );
-			}
-
-			// Filter arguments that are attached to the caller
-			var args = Array.prototype.slice.call( arguments, 2 );
-
-			if ( this.isEnabled() ) {
-				context.eachAsync( {
-					delay: 100,
-					bulk: 0,
-					loop: function() {
-						method.apply( $( this ), args );
-					}
-				} );
-			} else {
-				context.each( function() {
-					method.apply( $( this ), args );
-				} );
-			}
-		}
-	};
-
-
-	/**
-	 * Declares methods to access information about available formats
-	 *
-	 * @since  1.9
-	 *
-	 * @class smw.formats
-	 * @alias smw.Formats
-	 */
-	instance.formats = {
-
-		/**
-		 * Returns list of available formats
-		 *
-		 * @since 1.9
-		 * @extends smw.formats
-		 *
-		 * @return {Object}
-		 */
-		getList: function() {
-			return mediaWiki.config.get( 'smw-config' ).formats;
-		},
-
-		/**
-		 * Returns localized name for  a select format
-		 *
-		 * @since 1.9
-		 *
-		 * @param {string} format
-		 *
-		 * @return {string}
-		 */
-		getName: function( format ) {
-			if( typeof format === 'string' ){
-				return this.getList()[ instance.util.clean( format ).toLowerCase() ];
-			}
-			return undefined;
-		}
-	};
-
-	/**
-	 * Declares methods to access information about invoked settings (see also
-	 * SMWHooks::onResourceLoaderGetConfigVars)
-	 *
-	 * @since 1.9
-	 *
-	 * @class smw.settings
-	 * @singleton
-	 */
-	instance.settings = {
-
-		/**
-		 * Returns list of available settings
-		 *
-		 * @since 1.9
-		 *
-		 * @return {Object}
-		 */
-		getList: function() {
-			return mediaWiki.config.get( 'smw-config' ).settings;
-		},
-
-		/**
-		 * Returns a specific settings value (see SMW\Settings::get)
-		 *
-		 * @since 1.9
-		 *
-		 * @param  {string} key to be selected
-		 *
-		 * @return {mixed}
-		 */
-		get: function( key ) {
-			if( typeof key === 'string' ) {
-				return this.getList()[key];
-			}
-			return undefined;
-		}
-	};
-
-	// Define aliases
-	instance.Util = instance.util;
-
-	// Expose invoked methods
-	return instance;
 
 } )( jQuery );
 
 // Assign namespace
-window.smw = window.semanticMediaWiki = instance;
-
-( function( $ ) { 'use strict'; $( document ).ready( function() {
-} ); } )( jQuery );
+window.smw = window.semanticMediaWiki = smw;
