5173bf86fe4600fb1c5f963cda3dfb31a6f887ec
jeroendedauw <jeroendedauw@gmail.com>
2013-08-06 08:19:36 +0200

    Add coveralls.io support to TravisCI config
    Change-Id: I04b690fafc38ed8918c13224c43f6fc034aeadab

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+originalDirectory=pwd
+
 cd ..
 
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
@@ -11,15 +13,20 @@ cd phase3
 mysql -e 'create database its_a_mw;'
 php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
 
-cd extensions
-composer create-project mediawiki/semantic-mediawiki:dev-master SemanticMediaWiki --keep-vcs
+composer require mediawiki/semantic-mediawiki:dev-master
+composer require satooshi/php-coveralls:dev-master
 
-cd ..
-echo 'require_once( __DIR__ . "/extensions/SemanticMediaWiki/SemanticMediaWiki.php" );' >> LocalSettings.php
+# Replace SemanticMediaWiki with the version that should be tested
+rm -rf SemanticMediaWiki
+mkdir SemanticMediaWiki
+cp -r $originalDirectory/* SemanticMediaWiki
+
+echo 'require_once( __DIR__ . "/vendor/mediawiki/semantic-mediawiki/SemanticMediaWiki.php" );' >> LocalSettings.php
 
 echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
 echo 'ini_set("display_errors", 1);' >> LocalSettings.php
 echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
 echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
 
 php maintenance/update.php --quick
