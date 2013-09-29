a06ae55d3ae9652e7237c5a353df79acd2004ab3
jeroendedauw <jeroendedauw@gmail.com>
2013-09-29 13:56:46 +0200

    Add DIWikiPageTest
    Change-Id: Ie833d9eee9b665d23b9706d34011303c9c4c5c6e

---
+++ tests/phpunit/includes/dataitems/DIWikiPageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SMW\Tests;
+
+use SMW\DIWikiPage;
+
+/**
+ * @covers \SMW\DIWikiPage
+ * @covers SMWDataItem
+ *
+ * @group SMW
+ * @group SMWExtension
+ * @group SMWDataItems
+ *
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class DIWikiPageTest extends DataItemTest {
+
+	/**
+	 * @see DataItemTest::getClass
+	 *
+	 * @since 1.9
+	 *
+	 * @return string
+	 */
+	public function getClass() {
+		return 'SMW\DIWikiPage';
+	}
+
+	/**
+	 * @see DataItemTest::constructorProvider
+	 *
+	 * @since 1.9
+	 *
+	 * @return array
+	 */
+	public function constructorProvider() {
+		return array(
+			array( true, 'Foo', NS_MAIN, '' ),
+			array( true, 'Foo_Bar', NS_MAIN, '' ),
+			array( true, 'Foo_Bar_Baz', NS_MAIN, '', 'spam' ),
+		);
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetTitleAndNewFromTitleRoundrtip( DIWikiPage $di ) {
+		$newDi = DIWikiPage::newFromTitle( $di->getTitle() );
+		$this->assertTrue( $newDi->equals( $di ) );
+	}
+
+}
