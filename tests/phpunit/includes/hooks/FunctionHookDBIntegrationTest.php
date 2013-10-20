a62ca4333f2b6fda0d9cb10afcb75572b4f50f70
mwjames <jamesin.hongkong.1@gmail.com>
2013-10-20 11:14:11 +0900

    Add FunctionHookDBIntegrationTest
    Change-Id: I23e0840909a9561e5928590b2728716812608dbd

---
+++ tests/phpunit/includes/hooks/FunctionHookDBIntegrationTest.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace SMW\Test;
+
+use SMW\NewRevisionFromEditComplete;
+use SMW\OutputPageParserOutput;
+use SMW\ArticlePurge;
+use SMW\BaseContext;
+
+use WikiPage;
+use Title;
+
+/**
+ * @covers \SMW\FunctionHook
+ *
+ * @ingroup Test
+ *
+ * @group SMW
+ * @group SMWExtension
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @since 1.9
+ *
+ * @author mwjames
+ */
+class FunctionHookIntegrationTest extends \MediaWikiTestCase {
+
+	/**
+	 * @return string|false
+	 */
+	public function getClass() {
+		return false;
+	}
+
+	/**
+	 * @since 1.9
+	 */
+	public function newWikiPage( $text = 'Foo' ) {
+
+		if ( !method_exists( 'WikiPage', 'doEditContent' ) ) {
+			$this->markTestSkipped(
+				'Skipped test due to missing method (probably MW 1.19 or lower).'
+			);
+		}
+
+		$wikiPage = new WikiPage( Title::newFromText( $text ) );
+		$user = new MockSuperUser();
+
+		$content = \ContentHandler::makeContent(
+			'testing',
+			$wikiPage->getTitle(),
+			CONTENT_MODEL_WIKITEXT
+		);
+
+		$wikiPage->doEditContent( $content, "testing " . __METHOD__, EDIT_NEW, false, $user );
+
+		return $wikiPage;
+	}
+
+	/**
+	 * @since 1.9
+	 */
+	public function testOnArticlePurgeOnDatabase() {
+
+		$wikiPage = $this->newWikiPage();
+
+		$instance = new ArticlePurge( $wikiPage );
+		$instance->invokeContext( new BaseContext() );
+
+		$this->assertTrue( $instance->process() );
+
+		// Always make sure to clean-up
+		if ( $wikiPage->exists() ) {
+			$wikiPage->doDeleteArticle( "testing done on " . __METHOD__ );
+		}
+
+	}
+
+	/**
+	 * @since 1.9
+	 */
+	public function testOnNewRevisionFromEditCompleteOnDatabase() {
+
+		$wikiPage = $this->newWikiPage( 'Bam' );
+
+		$this->assertTrue( $wikiPage->getId() > 0, "WikiPage should have new page id" );
+		$revision = $wikiPage->getRevision();
+		$user = new MockSuperUser();
+
+		$instance = new NewRevisionFromEditComplete( $wikiPage, $revision, $wikiPage->getId(), $user );
+		$instance->invokeContext( new BaseContext() );
+
+		$this->assertTrue( $instance->process() );
+
+		// Always make sure the clean-up
+		if ( $wikiPage->exists() ) {
+			$wikiPage->doDeleteArticle( "testing done on " . __METHOD__ );
+		}
+
+	}
+
+	/**
+	 * @since 1.9
+	 */
+	public function testOnOutputPageParserOutputeOnDatabase() {
+
+		$text = __METHOD__;
+		$wikiPage = $this->newWikiPage( 'Bar' );
+
+		$title = $wikiPage->getTitle();
+
+		$parserOutput = new \ParserOutput();
+		$parserOutput->setTitleText( $title->getPrefixedText() );
+
+		$context = new \RequestContext();
+		$context->setTitle( $title );
+		$outputPage = new \OutputPage( $context );
+
+		$instance = new OutputPageParserOutput( $outputPage, $parserOutput );
+		$instance->invokeContext( new BaseContext() );
+
+		$this->assertTrue( $instance->process() );
+
+		// Always make sure the clean-up
+		if ( $wikiPage->exists() ) {
+			$wikiPage->doDeleteArticle( "testing done on " . __METHOD__ );
+		}
+
+	}
+
+	/**
+	 * @since 1.9
+	 */
+	public function testTitleMoveCompleteOnDatabase() {
+
+		// For some mysterious reasons this test causes
+		// SMW\Test\ApiAskTest::testExecute ... to fail with DBQueryError:
+		// Query: SELECT  o_serialized AS v0  FROM unittest_unittest_smw_fpt_mdat
+		// WHERE s_id='5'; it seems that the temp. unittest tables are
+		// being deleted while this test runs
+		$skip = true;
+
+		if ( !$skip && method_exists( 'WikiPage', 'doEditContent' ) ) {
+			$wikiPage = $this->newPage();
+			$user = $this->getUser();
+
+			$title = $wikiPage->getTitle();
+			$newTitle = $this->getTitle();
+			$pageid = $wikiPage->getId();
+
+			$content = \ContentHandler::makeContent(
+				'testing',
+				$title,
+				CONTENT_MODEL_WIKITEXT
+			);
+			$wikiPage->doEditContent( $content, "testing", EDIT_NEW, false, $user );
+
+		//	$result = SMWHooks::onTitleMoveComplete( $title, $newTitle, $user, $pageid, $pageid );
+
+			// Always make sure to clean-up
+			if ( $wikiPage->exists() ) {
+				$wikiPage->doDeleteArticle( "testing done." );
+			}
+
+			$this->assertTrue( $result );
+		} else {
+			$this->assertTrue( $skip );
+		}
+	}
+
+}
