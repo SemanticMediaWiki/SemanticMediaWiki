902950cc4eb9df603e877c908b026ba9f0308fe9
jeroendedauw <jeroendedauw@gmail.com>
2013-11-04 05:14:48 +0100

    Have TravisCI use the SMW PHPUnit config
    Hopefully this will speed up the code coverage report generation
    
    Change-Id: Ief66e54eadbd254193b40f67491e98996fbdb47d

--- .travis.test.sh
+++ .travis.test.sh
@@ -4,7 +4,7 @@ cd ../phase3/tests/phpunit
 
 if [ "$MW-$DBTYPE" == "master-mysql" ]
 then
-	php phpunit.php --group SMWExtension --coverage-clover ../../extensions/SemanticMediaWiki/build/logs/clover.xml
+	php phpunit.php --group SMWExtension -c ../../extensions/SemanticMediaWiki/phpunit.xml.dist --coverage-clover ../../extensions/SemanticMediaWiki/build/logs/clover.xml
 else
-	php phpunit.php --group SMWExtension
+	php phpunit.php --group SMWExtension -c ../../extensions/SemanticMediaWiki/phpunit.xml.dist
 fi
