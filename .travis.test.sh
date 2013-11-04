958c9cb0e63d019db1055bd04522a7a7750cd6e3
jeroendedauw <jeroendedauw@gmail.com>
2013-11-04 11:17:59 +0100

    Improve TravisCI build time
    Change-Id: Ic4356d2d50d48a5e72a8391f3c33adf6c4054806

--- .travis.test.sh
+++ .travis.test.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+cd ../../tests/phpunit
+
 if [ "$MW-$DBTYPE" == "master-mysql" ]
 then
-	cd ../../tests/phpunit
 	php phpunit.php --group SMWExtension -c ../../extensions/SemanticMediaWiki/phpunit.xml.dist --coverage-clover ../../extensions/SemanticMediaWiki/build/logs/clover.xml
 
 	cd ../../extensions/SemanticMediaWiki
 	composer require satooshi/php-coveralls:dev-master
 	php vendor/bin/coveralls -v
+else
+	php phpunit.php --group SMWExtension -c ../../extensions/SemanticMediaWiki/phpunit.xml.dist
 fi
