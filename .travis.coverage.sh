915f568a1b414e29ad9f7746d3fa9054ae0bf298
jeroendedauw <jeroendedauw@gmail.com>
2013-11-04 10:26:48 +0100

    Fix path in TravisCI config

--- .travis.coverage.sh
+++ .travis.coverage.sh
@@ -2,7 +2,7 @@
 
 if [ "$MW-$DBTYPE" == "master-mysql" ]
 then
-	cd ../phase3/tests/phpunit
+	cd ../../tests/phpunit
 	php phpunit.php --group SMWExtension -c ../../extensions/SemanticMediaWiki/phpunit.xml.dist --coverage-clover ../../extensions/SemanticMediaWiki/build/logs/clover.xml
 
 	cd ../../extensions/SemanticMediaWiki
