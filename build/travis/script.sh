a2645108c6af18bffab24d156abaae8ccf23e4b0
jeroendedauw <jeroendedauw@gmail.com>
2013-11-08 16:51:29 +0100

    Restructure TravisCI code to not have coveralls affect the build result
    Change-Id: I93ee74f4091c19b2c7e965387d392fbb3cb3ba00

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
+set -x
+
 cd ../phase3/tests/phpunit
 
 if [ "$MW-$DBTYPE" == "master-mysql" ]
 then
 	php phpunit.php --group SMWExtension -c ../../extensions/SemanticMediaWiki/phpunit.xml.dist --coverage-clover ../../extensions/SemanticMediaWiki/build/logs/clover.xml
-
-	cd ../../extensions/SemanticMediaWiki
-	composer require satooshi/php-coveralls:dev-master
-	php vendor/bin/coveralls -v
 else
 	php phpunit.php --group SMWExtension -c ../../extensions/SemanticMediaWiki/phpunit.xml.dist
 fi
