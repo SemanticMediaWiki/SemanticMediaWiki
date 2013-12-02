15f02002f2693a987c0660cf39963c6c9ae5f60b
mwjames <jamesin.hongkong.1@gmail.com>
2013-12-03 02:34:25 +0900

    Add installation settings system test

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -22,12 +22,11 @@ git checkout $MW
 if [ "$DBTYPE" == "postgres" ]
 then
   psql -c 'create database its_a_mw;' -U postgres
-  php maintenance/install.php --dbtype $DBTYPE --dbuser postgres --dbname its_a_mw --pass nyan TravisWiki admin
+  php maintenance/install.php --dbtype $DBTYPE --dbuser postgres --dbname its_a_mw --pass nyan TravisWiki admin --scriptpath /TravisWiki
 else
   mysql -e 'create database its_a_mw;'
-  php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
+  php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin --scriptpath /TravisWiki
 fi
-
 
 cd extensions
 
@@ -40,6 +39,7 @@ cd ../..
 
 echo 'require_once( __DIR__ . "/extensions/SemanticMediaWiki/SemanticMediaWiki.php" );' >> LocalSettings.php
 
+echo '$wgScriptPath = "/TravisWiki";' >> LocalSettings.php
 echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
 echo 'ini_set("display_errors", 1);' >> LocalSettings.php
 echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
