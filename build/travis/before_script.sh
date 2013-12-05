d942a9c3883fffd4478723e61acb67f8208dbd75
mwjames <jamesin.hongkong.1@gmail.com>
2013-12-06 08:06:39 +0900

    Clean-up before script

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -1,35 +1,49 @@
 #! /bin/bash
 
-set -x
+## Fetching MediaWiki installation base
+function runMWInstaller {
 
-originalDirectory=$(pwd)
+	if [ "$MW" == "master" ]
+	then
+		git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
+		git checkout $MW
+	else
+		wget https://github.com/wikimedia/mediawiki-core/archive/$MW.tar.gz
+		tar -zxf $MW.tar.gz
+		mv mediawiki-core-$MW phase3
+	fi
 
-cd ..
+	cd phase3
 
-if [ "$MW" == "master" ]
-then
-	git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
-else
-	wget https://github.com/wikimedia/mediawiki-core/archive/$MW.tar.gz
-	tar -zxf $MW.tar.gz
-	mv mediawiki-core-$MW phase3
-fi
+	## Run Database maintenance script
+	if [ "$DBTYPE" == "postgres" ]
+	then
+	  psql -c 'create database its_a_mw;' -U postgres
+	  php maintenance/install.php --dbtype $DBTYPE --dbuser postgres --dbname its_a_mw --pass nyan TravisWiki admin --scriptpath /TravisWiki
+	else
+	  mysql -e 'create database its_a_mw;'
+	  php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin --scriptpath /TravisWiki
+	fi
 
-cd phase3
+}
 
-git checkout $MW
+## Run SemanticMediaWiki dependency install
+function runSMWInstaller {
+	if [ "$TYPE" == "composer" ]
+	then
+		runComposerInstallByMediaWiki
+	else
+		cd extensions
+		cp -r $originalDirectory SemanticMediaWiki
+		cd SemanticMediaWiki
+		composer install
+	fi
 
-if [ "$DBTYPE" == "postgres" ]
-then
-  psql -c 'create database its_a_mw;' -U postgres
-  php maintenance/install.php --dbtype $DBTYPE --dbuser postgres --dbname its_a_mw --pass nyan TravisWiki admin --scriptpath /TravisWiki
-else
-  mysql -e 'create database its_a_mw;'
-  php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin --scriptpath /TravisWiki
-fi
+	cd ../..
+}
 
-if [ "$TYPE" == "composer" ]
-then
+# Run Composer installation from the MW root directory
+function runComposerInstallByMediaWiki {
 	composer require mediawiki/semantic-media-wiki "dev-master"
 
 	cd extensions
@@ -44,24 +58,28 @@ then
 		git fetch origin "$TRAVIS_BRANCH"
 		git checkout -qf FETCH_HEAD
 	fi
+}
 
-	cd ../..
+## Generate LocalSettings
+function runSettingsGenerator {
+	echo 'require_once( __DIR__ . "/extensions/SemanticMediaWiki/SemanticMediaWiki.php" );' >> LocalSettings.php
 
-else
-	cd extensions
-	cp -r $originalDirectory SemanticMediaWiki
-	cd SemanticMediaWiki
-	composer install
-	cd ../..
-fi
+	# Error reporting
+	echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
+	echo 'ini_set("display_errors", 1);' >> LocalSettings.php
+	echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
+	echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+	echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
+}
 
-echo 'require_once( __DIR__ . "/extensions/SemanticMediaWiki/SemanticMediaWiki.php" );' >> LocalSettings.php
+set -x
 
-echo '$wgScriptPath = "/TravisWiki";' >> LocalSettings.php
-echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
-echo 'ini_set("display_errors", 1);' >> LocalSettings.php
-echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
-echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
-echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
+originalDirectory=$(pwd)
+
+cd ..
+
+runMWInstaller
+runSMWInstaller
+runSettingsGenerator
 
 php maintenance/update.php --quick
