949b7b7b76a8496b2de9ba91a18a101d18d63e93
mwjames <jamesin.hongkong.1@gmail.com>
2013-12-10 18:42:00 +0900

    Adjust scrutinizer settings to use external code coverage
    [1] http://www.adayinthelifeof.nl/2013/11/20/external-code-coverage-with-travis-scrutinizer/

--- build/travis/after_success.sh
+++ build/travis/after_success.sh
@@ -2,9 +2,14 @@
 
 set -x
 
+originalDirectory=$(pwd)
+
 if [ "$TYPE" == "coverage" ]
 then
-	cd ../../extensions/SemanticMediaWiki
-	composer require satooshi/php-coveralls:dev-master
-	php vendor/bin/coveralls -v
+	# Use for coveralls.io but currently no working
+	# composer require satooshi/php-coveralls:dev-master
+	# php vendor/bin/coveralls -v
+
+	wget https://scrutinizer-ci.com/ocular.phar
+	php ocular.phar code-coverage:upload --format=php-clover $originalDirectory/build/coverage.clover
 fi
