83e1784183ee5deff77dd0880905a1d5787617a7
mwjames <jamesin.hongkong.1@gmail.com>
2014-01-26 16:16:23 +0900

    Clean-up some common methods used in tests
    - Move MockDBConnectionProvider

---
+++ tests/phpunit/mocks/MockDBConnectionProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SMW\Tests;
+
+use SMW\DBConnectionProvider;
+
+/**
+ * @ingroup Test
+ *
+ * @group SMW
+ * @group SMWExtension
+ *
+ * @licence GNU GPL v2+
+ * @since 1.9.0.2
+ *
+ * @author mwjames
+ */
+class MockDBConnectionProvider extends \PHPUnit_Framework_TestCase implements DBConnectionProvider {
+
+	public function getConnection() {
+		return $this->createMockDBConnectionProvider()->getConnection();
+	}
+
+	public function releaseConnection() {}
+
+	public function getMockDatabase() {
+
+		if ( !isset( $this->database ) ) {
+			$this->database = $this->getMockBuilder( 'DatabaseMysql' )
+				->disableOriginalConstructor()
+				->getMock();
+		}
+
+		return $this->database;
+	}
+
+	protected function createMockDBConnectionProvider() {
+
+		$provider = $this->getMockForAbstractClass( '\SMW\DBConnectionProvider' );
+
+		$provider->expects( $this->any() )
+			->method( 'getConnection' )
+			->will( $this->returnValue( $this->getMockDatabase() ) );
+
+		return $provider;
+	}
+
+}
