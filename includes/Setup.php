526ff91f1b35e43a4e1098fbf2f189ec3c5baf9b
jeroendedauw <jeroendedauw@gmail.com>
2014-01-31 00:39:30 +0100

    Fix resource paths

--- includes/Setup.php
+++ includes/Setup.php
@@ -274,7 +274,9 @@ final class Setup implements ContextAware {
 	 */
 	protected function registerFooterIcon() {
 		$this->globals['wgFooterIcons']['poweredby']['semanticmediawiki'] = array(
-			'src' => substr( __DIR__, strlen( $GLOBALS['IP'] ) - strlen( $GLOBALS['wgScriptPath'] ), -strlen( 'includes/' ) ) . '/resources/images/smw_button.png',
+			'src' => $this->globals['wgScriptPath'] . '/extensions/'
+				. end( ( explode( DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR , __DIR__, 2 ) ) )
+				. '/../resources/images/smw_button.png',
 			'url' => 'https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki',
 			'alt' => 'Powered by Semantic MediaWiki',
 		);
