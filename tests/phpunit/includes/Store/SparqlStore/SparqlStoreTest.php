a2ad866c5b8c1cf32cac95fc119d74c98bb8487b
mwjames <jamesin.hongkong.1@gmail.com>
2014-03-07 14:09:28 +0900

    SMWSparqlStore add proper SparqlDatabase injection to enable unit tests

---
+++ tests/phpunit/includes/Store/SparqlStore/SparqlStoreTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace SMW\Tests\Store\SparqlStore;
+
+use SMWSparqlStore;
+use SMW\DIWikiPage;
+
+use Title;
+
+/**
+ * @covers \SMWSparqlStore
+ *
+ * @ingroup Test
+ *
+ * @group SMW
+ * @group SMWExtension
+ *
+ * @license GNU GPL v2+
+ * @since 1.9.1.2
+ *
+ * @author mwjames
+ */
+class SparqlStoreTest extends \PHPUnit_Framework_TestCase {
+
+	public function testCanConstruct() {
+		$this->assertInstanceOf(
+			'\SMWSparqlStore',
+			new SMWSparqlStore()
+		);
+	}
+
+	public function testGetSemanticDataOnMockBaseStore() {
+
+		$subject = DIWikiPage::newFromTitle( Title::newFromText( __METHOD__ ) );
+
+		$semanticData = $this->getMockBuilder( '\SMW\SemanticData' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$baseStore = $this->getMockBuilder( '\SMWStore' )
+			->disableOriginalConstructor()
+			->getMockForAbstractClass();
+
+		$baseStore->expects( $this->once() )
+			->method( 'getSemanticData' )
+			->with( $this->equalTo( $subject ) )
+			->will( $this->returnValue( $semanticData ) );
+
+		$instance = new SMWSparqlStore( $baseStore );
+
+		$this->assertInstanceOf(
+			'\SMW\SemanticData',
+			$instance->getSemanticData( $subject )
+		);
+	}
+
+	public function testDeleteSubjectOnMockBaseStore() {
+
+		$title = Title::newFromText( 'DeleteSubjectOnMockBaseStore' );
+		$expResource = \SMWExporter::getDataItemExpElement( DIWikiPage::newFromTitle( $title ) );
+
+		$resourceUri = \SMWTurtleSerializer::getTurtleNameForExpElement( $expResource );
+
+		$extraNamespaces = array(
+			$expResource->getNamespaceId() => $expResource->getNamespace()
+		);
+
+		$baseStore = $this->getMockBuilder( '\SMWStore' )
+			->disableOriginalConstructor()
+			->getMockForAbstractClass();
+
+		$baseStore->expects( $this->once() )
+			->method( 'deleteSubject' )
+			->with( $this->equalTo( $title ) )
+			->will( $this->returnValue( true ) );
+
+		$sparqlDatabase = $this->getMockBuilder( '\SMWSparqlDatabase' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$sparqlDatabase->expects( $this->once() )
+			->method( 'deleteContentByValue' )
+			->will( $this->returnValue( true ) );
+
+		$sparqlDatabase->expects( $this->once() )
+			->method( 'delete' )
+			->with(
+				$this->equalTo( "{$resourceUri} ?p ?o" ),
+				$this->equalTo( "{$resourceUri} ?p ?o" ),
+				$this->equalTo( $extraNamespaces ) )
+			->will( $this->returnValue( true ) );
+
+		$instance = new SMWSparqlStore( $baseStore );
+		$instance->setSparqlDatabase( $sparqlDatabase );
+
+		$instance->deleteSubject( $title );
+	}
+
+}
