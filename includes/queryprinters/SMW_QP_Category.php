8fbaf8437b47d3a890e8af58e385899c984726ea
mwjames <jamesin.hongkong.1@gmail.com>
2014-05-12 01:28:16 +0900

    Fix Fatal error on getDIType() for SMW_QP_Category
    Needs https://gerrit.wikimedia.org/r/#/c/132815/
    
    Reported by [0]
    
    [0] http://wikimedia.7.x6.nabble.com/Odd-error-on-ask-tp5027423.html

--- includes/queryprinters/SMW_QP_Category.php
+++ includes/queryprinters/SMW_QP_Category.php
@@ -3,11 +3,11 @@
  * Print query results in alphabetic groups displayed in columns, a la the
  * standard Category pages and the default view in Semantic Drilldown.
  * Based on SMW_QP_List by Markus Krötzsch.
- * 
+ *
  * @author David Loomer
  * @author Yaron Koren
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- * 
+ *
  * @file
  * @ingroup SMWQuery
  */
@@ -26,21 +26,21 @@ class SMWCategoryResultPrinter extends SMWResultPrinter {
 
 	/**
 	 * @see SMWResultPrinter::handleParameters
-	 * 
+	 *
 	 * @since 1.6.2
-	 * 
+	 *
 	 * @param array $params
 	 * @param $outputmode
 	 */
 	protected function handleParameters( array $params, $outputmode ) {
 		parent::handleParameters( $params, $outputmode );
-		
+
 		$this->mUserParam = trim( $params['userparam'] );
 		$this->mDelim = trim( $params['delim'] );
 		$this->mNumColumns = $params['columns'];
 		$this->mTemplate = $params['template'];
-	}	
-	
+	}
+
 	public function getName() {
 		// Give grep a chance to find the usages:
 		// smw_printername_category
@@ -50,10 +50,7 @@ class SMWCategoryResultPrinter extends SMWResultPrinter {
 	protected function getResultText( SMWQueryResult $res, $outputmode ) {
 		global $wgContLang;
 
-		// <H3> will generate TOC entries otherwise.  Probably need another way
-		// to accomplish this -- user might still want TOC for other page content.
-		$result = '__NOTOC__';
-
+		$result = '';
 		$num = $res->getCount();
 
 		$prev_first_char = "";
@@ -64,24 +61,34 @@ class SMWCategoryResultPrinter extends SMWResultPrinter {
 		// Print all result rows:
 		$rowindex = 0;
 		$row = $res->getNext();
-		
+
 		while ( $row !== false ) {
 			$nextrow = $res->getNext(); // look ahead
 
+			if ( !isset( $row[0] ) ) {
+				$row = $nextrow;
+				continue;
+			}
+
 			$content = $row[0]->getContent();
-			
+
+			if ( !isset( $content[0] ) || !( $content[0] instanceof SMWDataItem ) ) {
+				$row = $nextrow;
+				continue;
+			}
+
 			$cur_first_char = $wgContLang->firstChar(
 				$content[0]->getDIType() == SMWDataItem::TYPE_WIKIPAGE ?
 					$res->getStore()->getWikiPageSortKey( $content[0] )
 					: $content[0]->getSortKey()
 			);
-			
+
 			if ( $rowindex % $rows_per_column == 0 ) {
 				$result .= "\n			<div style=\"float: left; width: $column_width%;\">\n";
 				if ( $cur_first_char == $prev_first_char )
 					$result .= "				<h3>$cur_first_char " . wfMessage( 'listingcontinuesabbrev' )->text() . "</h3>\n				<ul>\n";
 			}
-			
+
 			// if we're at a new first letter, end
 			// the last list and start a new one
 			if ( $cur_first_char != $prev_first_char ) {
@@ -93,34 +100,34 @@ class SMWCategoryResultPrinter extends SMWResultPrinter {
 
 			$result .= '<li>';
 			$first_col = true;
-			
+
 			if ( $this->mTemplate !== '' ) { // build template code
 				$this->hasTemplates = true;
 				$wikitext = ( $this->mUserParam ) ? "|userparam=$this->mUserParam":'';
 				$i = 1; // explicitly number parameters for more robust parsing (values may contain "=")
-				
+
 				foreach ( $row as $field ) {
 					$wikitext .= '|' . $i++ . '=';
 					$first_value = true;
-					
+
 					while ( ( $text = $field->getNextText( SMW_OUTPUT_WIKI, $this->getLinker( $first_col ) ) ) !== false ) {
 						if ( $first_value ) $first_value = false; else $wikitext .= $this->mDelim . ' ';
 						$wikitext .= $text;
 					}
-					
+
 					$first_col = false;
 				}
-				
+
 				$wikitext .= "|#=$rowindex";
 				$result .= '{{' . $this->mTemplate . $wikitext . '}}';
 				// str_replace('|', '&#x007C;', // encode '|' for use in templates (templates fail otherwise) -- this is not the place for doing this, since even DV-Wikitexts contain proper "|"!
 			} else {  // build simple list
 				$first_col = true;
 				$found_values = false; // has anything but the first column been printed?
-				
+
 				foreach ( $row as $field ) {
 					$first_value = true;
-					
+
 					while ( ( $text = $field->getNextText( SMW_OUTPUT_WIKI, $this->getLinker( $first_col ) ) ) !== false ) {
 						if ( !$first_col && !$found_values ) { // first values after first column
 							$result .= ' (';
@@ -129,24 +136,24 @@ class SMWCategoryResultPrinter extends SMWResultPrinter {
 							// any value after '(' or non-first values on first column
 							$result .= ', ';
 						}
-						
+
 						if ( $first_value ) { // first value in any column, print header
 							$first_value = false;
-							
+
 							if ( $this->mShowHeaders && ( $field->getPrintRequest()->getLabel() !== '' ) ) {
 								$result .= $field->getPrintRequest()->getText( SMW_OUTPUT_WIKI, $this->mLinker ) . ' ';
 							}
 						}
-						
+
 						$result .= $text; // actual output value
 					}
-					
+
 					$first_col = false;
 				}
-				
+
 				if ( $found_values ) $result .= ')';
 			}
-			
+
 			$result .= '</li>';
 			$row = $nextrow;
 
@@ -159,15 +166,28 @@ class SMWCategoryResultPrinter extends SMWResultPrinter {
 			$rowindex++;
 		}
 
+		if ( $result === '' ) {
+
+			$res->addErrors( array(
+				$this->msg( 'smw-qp-empty-data' )->inContentLanguage()->text()
+			) );
+
+			return $result;
+		}
+
 		// Make label for finding further results
 		if ( $this->linkFurtherResults( $res ) ) {
 			$result .= '<br /><li>' . $this->getLink( $res, $outputmode )->getText( SMW_OUTPUT_WIKI, $this->mLinker ) . '</li>';
 		}
 
-		$result .= "				</ul>\n			</div> <!-- end column -->";
+		$result .= "</ul>\n</div> <!-- end column -->";
 		// clear all the CSS floats
 		$result .= "\n" . '<br style="clear: both;"/>';
-		
+
+		// <H3> will generate TOC entries otherwise.  Probably need another way
+		// to accomplish this -- user might still want TOC for other page content.
+		$result .= '__NOTOC__';
+
 		return $result;
 	}
 
