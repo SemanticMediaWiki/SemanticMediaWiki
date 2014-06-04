c532405c76e132991592033c8430b4fb8c74c640
mwjames <jamesin.hongkong.1@gmail.com>
2014-06-04 23:32:29 +0900

    Revert #303
    This reverts #303 in trying to find the cause of thingles/WikiApiary/issues/114

---
+++ tests/phpunit/Util/ParserFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SMW\Tests\Util;
+
+use SMW\Tests\Util\Mock\MockSuperUser;
+
+use Title;
+use ParserOptions;
+use Parser;
+use User;
+
+/**
+ * @ingroup Test
+ *
+ * @group SMW
+ * @group SMWExtension
+ *
+ * @license GNU GPL v2+
+ * @since 1.9.3
+ */
+class ParserFactory {
+
+	public static function newFromTitle( Title $title, User $user = null ) {
+
+		if ( $user === null ) {
+			$user = new MockSuperUser();
+		}
+
+		// $wikiPage = new \WikiPage( $title );
+		// $wikiPage->makeParserOptions( $user );
+
+		$parser = new Parser( $GLOBALS['wgParserConf'] );
+		$parser->setTitle( $title );
+		$parser->setUser( $user );
+		$parser->Options( new ParserOptions( $user ) );
+		$parser->clearState();
+
+		return $parser;
+	}
+
+}
