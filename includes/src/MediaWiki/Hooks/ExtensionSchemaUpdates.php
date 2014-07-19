afc305bdc3f10c53a0ec1e2ef7e859e29d6e9f57
mwjames <jamesin.hongkong.1@gmail.com>
2014-07-19 18:57:24 +0900

    SemanticMediaWiki.hooks
    - ArticleFromTitle
    - BeforeDisplayNoArticleText
    - ExtensionSchemaUpdates
    - ExtensionTypes
    - ResourceLoaderTestModules
    - TitleIsAlwaysKnown
    
    + tests

---
+++ includes/src/MediaWiki/Hooks/ExtensionSchemaUpdates.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SMW\MediaWiki\Hooks;
+
+use DatabaseUpdater;
+
+/**
+ * Schema update to set up the needed database tables
+ *
+ * @see https://www.mediawiki.org/wiki/Manual:Hooks/LoadExtensionSchemaUpdates
+ *
+ * @license GNU GPL v2+
+ * @since 2.0
+ *
+ * @author mwjames
+ */
+class ExtensionSchemaUpdates {
+
+	/**
+	 * @var DatabaseUpdater
+	 */
+	protected $updater = null;
+
+	/**
+	 * @since  2.0
+	 *
+	 * @param DatabaseUpdater $updater = null
+	 */
+	public function __construct( DatabaseUpdater $updater = null ) {
+		$this->updater = $updater;
+	}
+
+	/**
+	 * @since 2.0
+	 *
+	 * @return true
+	 */
+	public function process() {
+
+		$this->updater->addExtensionUpdate( array( 'SMWStore::setupStore' ) );
+
+		return true;
+	}
+
+}
---
+++ includes/src/MediaWiki/Hooks/TitleIsAlwaysKnown.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace SMW\MediaWiki\Hooks;
+
+use SMW\DIProperty;
+
+use Title;
+
+/**
+ * Allows overriding default behaviour for determining if a page exists
+ *
+ * @see https://www.mediawiki.org/wiki/Manual:Hooks/TitleIsAlwaysKnown
+ *
+ * @license GNU GPL v2+
+ * @since 2.0
+ *
+ * @author mwjames
+ */
+class TitleIsAlwaysKnown {
+
+	/**
+	 * @var Title
+	 */
+	private $title;
+
+	/**
+	 * @var mixed
+	 */
+	private $result;
+
+	/**
+	 * @since  2.0
+	 *
+	 * @param Title $title
+	 * @param mixed &$result
+	 */
+	public function __construct( Title $title, &$result ) {
+		$this->title = $title;
+		$this->result =& $result;
+	}
+
+	/**
+	 * @since 2.0
+	 *
+	 * @return boolean
+	 */
+	public function process() {
+
+		// Two possible ways of going forward:
+		//
+		// The FIRST seen here is to use the hook to override the known status
+		// for predefined properties in order to avoid any edit link
+		// which makes no-sense for predefined properties
+		//
+		// The SECOND approach is to inject SMWWikiPageValue with a setLinkOptions setter
+		// that enables to set the custom options 'known' for each invoked linker during
+		// getShortHTMLText
+		// $linker->link( $this->getTitle(), $caption, $customAttributes, $customQuery, $customOptions )
+		//
+		// @see also HooksTest::testOnTitleIsAlwaysKnown
+
+		if ( $this->title->getNamespace() === SMW_NS_PROPERTY ) {
+			if ( !DIProperty::newFromUserLabel( $this->title->getText() )->isUserDefined() ) {
+				$this->result = true;
+			}
+		}
+
+		return true;
+	}
+
+}
