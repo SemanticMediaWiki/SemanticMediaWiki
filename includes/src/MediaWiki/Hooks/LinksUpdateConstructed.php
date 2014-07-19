464955e78868aaa2dadbed54d2999325dfbd3065
mwjames <jamesin.hongkong.1@gmail.com>
2014-07-19 19:16:01 +0900

    MW 1.23+ / Hotfix for empty ParserOutput caused by RefreshLinksJob
    For details on the issue, see [0] as well as the method to replicate
    the problem.

--- includes/src/MediaWiki/Hooks/LinksUpdateConstructed.php
+++ includes/src/MediaWiki/Hooks/LinksUpdateConstructed.php
@@ -3,14 +3,15 @@
 namespace SMW\MediaWiki\Hooks;
 
 use SMW\Application;
+use SMW\ContentParser;
+use SMW\SemanticData;
+
 use LinksUpdate;
 
 /**
- * LinksUpdateConstructed hook is called at the end of LinksUpdate() is contruction
+ * LinksUpdateConstructed hook is called at the end of LinksUpdate()
  *
  * @see http://www.mediawiki.org/wiki/Manual:Hooks/LinksUpdateConstructed
- *
- * @ingroup FunctionHook
  *
  * @license GNU GPL v2+
  * @since 1.9
@@ -23,6 +24,11 @@ class LinksUpdateConstructed {
 	 * @var LinksUpdate
 	 */
 	protected $linksUpdate = null;
+
+	/**
+	 * @var Application
+	 */
+	private $application = null;
 
 	/**
 	 * @since  1.9
@@ -40,17 +46,49 @@ class LinksUpdateConstructed {
 	 */
 	public function process() {
 
+		$this->application = Application::getInstance();
+
 		/**
 		 * @var ParserData $parserData
 		 */
-		$parserData = Application::getInstance()->newParserData(
-			$this->linksUpdate->getTitle(),
-			$this->linksUpdate->getParserOutput()
-		);
+		$parserData = $this->application
+			->newParserData( $this->linksUpdate->getTitle(), $this->linksUpdate->getParserOutput() );
+
+		if ( $parserData->getSemanticData()->isEmpty() &&
+			( $semanticData = $this->refetchSemanticData() ) instanceOf SemanticData ) {
+			$parserData->setSemanticData( $semanticData );
+		}
 
 		$parserData->updateStore();
 
 		return true;
+	}
+
+	/**
+	 * #347 showed that an external process (e.g. RefreshLinksJob) can inject a
+	 * ParserOutput without SemanticData which forces the Store updater to create
+	 * an empty container and clear all existing data. To ensure that for a Title
+	 * and its current revision are really "empty" (e.g. delete action initiated
+	 * by a human) content is re-parsed
+	 *
+	 * @note Parsing is expensive but it is more expensive to loose data or to
+	 * expect that MW adheres the object contract
+	 */
+	private function refetchSemanticData() {
+		wfDebug( __METHOD__ . ' Empty SemanticData / re-parsing: ' . $this->linksUpdate->getTitle()->getPrefixedDBkey() . "\n" );
+
+		$contentParser = $this->application->newContentParser( $this->linksUpdate->getTitle() );
+		$parserOutput = $contentParser->parse()->getOutput();
+
+		if ( $parserOutput === null ) {
+			return null;
+		}
+
+		if ( method_exists( $parserOutput, 'getExtensionData' ) ) {
+			return $parserOutput->getExtensionData( 'smwdata' );
+		}
+
+		return $parserOutput->mSMWData;
 	}
 
 }
