9de23882181cddc8d0e2462c37eeef53a04c79eb
mwjames <jamesin.hongkong.1@gmail.com>
2015-02-03 03:06:08 +0900

    postgres / add sleep per suggestion of Travis@support

--- build/travis/install-mediawiki.sh
+++ build/travis/install-mediawiki.sh
@@ -32,7 +32,13 @@ if [ "$DB" == "postgres" ]
 then
   # See #458
   sudo /etc/init.d/postgresql stop
+
+  # Travis@support: Try adding a sleep of a few seconds between starting PostgreSQL
+  # and the first command that accesses PostgreSQL
+  sleep 3
+
   sudo /etc/init.d/postgresql start
+  sleep 3
 
   psql -c 'create database its_a_mw;' -U postgres
   php maintenance/install.php --dbtype $DB --dbuser postgres --dbname its_a_mw --pass nyan TravisWiki admin --scriptpath /TravisWiki
