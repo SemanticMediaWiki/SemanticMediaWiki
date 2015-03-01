36f16d93366f9656fb7a851afb58adb2763f0eea
mwjames <jamesin.hongkong.1@gmail.com>
2015-03-01 23:12:15 +0900

    Fix missing interwiki encoding for the RDF export

--- includes/export/SMW_Exporter.php
+++ includes/export/SMW_Exporter.php
@@ -763,10 +763,14 @@ class SMWExporter {
 
 		$localName = '';
 
+		if ( $diWikiPage->getInterwiki() !== '' ) {
+			$localName = $diWikiPage->getInterwiki() . ':';
+		}
+
 		if ( $diWikiPage->getNamespace() !== 0 ) {
-			$localName = str_replace( ' ', '_', $GLOBALS['wgContLang']->getNSText( $diWikiPage->getNamespace() ) ) . ':' . $diWikiPage->getDBkey();
+			$localName .= str_replace( ' ', '_', $GLOBALS['wgContLang']->getNSText( $diWikiPage->getNamespace() ) ) . ':' . $diWikiPage->getDBkey();
 		} else {
-			$localName = $diWikiPage->getDBkey();
+			$localName .= $diWikiPage->getDBkey();
 		}
 
 		return self::encodeURI( wfUrlencode( $localName ) );
