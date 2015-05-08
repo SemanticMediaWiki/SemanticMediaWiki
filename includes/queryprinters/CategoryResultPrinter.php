c172deb16fc56f43671bb65393c898192c8fab3a
mwjames <jamesin.hongkong.1@gmail.com>
2015-05-08 14:08:24 +0900

    Respect the DEFAULTSORT in CategoryResultPrinter, refs 699

--- includes/queryprinters/CategoryResultPrinter.php
+++ includes/queryprinters/CategoryResultPrinter.php
@@ -75,7 +75,7 @@ class CategoryResultPrinter extends ResultPrinter {
 				continue;
 			}
 
-			$cur_first_char = $this->getFirstLetterForCategory( $res, $content );
+			$cur_first_char = $this->getFirstLetterForCategory( $res, $content[0] );
 
 			if ( !isset( $contentsByIndex[$cur_first_char] ) ) {
 				$contentsByIndex[$cur_first_char] = array();
@@ -91,6 +91,7 @@ class CategoryResultPrinter extends ResultPrinter {
 				}
 
 				$this->addRowFieldsToTemplate(
+					$res,
 					$row,
 					$first_col,
 					$columnIndex,
@@ -111,9 +112,7 @@ class CategoryResultPrinter extends ResultPrinter {
 				foreach ( $row as $field ) {
 					$first_value = true;
 
-					$columnIndex = ByLanguageCollationMapper::getInstance()->findFirstLetterForCategory(
-						$field->getResultSubject()->getSortKey()
-					);
+					$columnIndex = $this->getFirstLetterForCategory( $res, $field->getResultSubject() );
 
 					while ( ( $text = $field->getNextText( SMW_OUTPUT_WIKI, $this->getLinker( $first_col ) ) ) !== false ) {
 						if ( !$first_col && !$found_values ) { // first values after first column
@@ -202,18 +201,18 @@ class CategoryResultPrinter extends ResultPrinter {
 		) );
 	}
 
-	private function getFirstLetterForCategory( SMWQueryResult $res, $content ) {
+	private function getFirstLetterForCategory( SMWQueryResult $res, SMWDataItem $dataItem ) {
 
-		$sortKey = $content[0]->getSortKey();
+		$sortKey = $dataItem->getSortKey();
 
-		if ( $content[0]->getDIType() == SMWDataItem::TYPE_WIKIPAGE ) {
-			$sortKey = $res->getStore()->getWikiPageSortKey( $content[0] );
+		if ( $dataItem->getDIType() == SMWDataItem::TYPE_WIKIPAGE ) {
+			$sortKey = $res->getStore()->getWikiPageSortKey( $dataItem );
 		}
 
 		return ByLanguageCollationMapper::getInstance()->findFirstLetterForCategory( $sortKey );
 	}
 
-	private function addRowFieldsToTemplate( $row, &$first_col, &$columnIndex, $templateRenderer ) {
+	private function addRowFieldsToTemplate( $res, $row, &$first_col, &$columnIndex, $templateRenderer ) {
 
 		// explicitly number parameters for more robust parsing (values may contain "=")
 		$i = 0;
@@ -234,9 +233,7 @@ class CategoryResultPrinter extends ResultPrinter {
 			$first_value = true;
 			$fieldValue = '';
 
-			$columnIndex = ByLanguageCollationMapper::getInstance()->findFirstLetterForCategory(
-				$field->getResultSubject()->getSortKey()
-			);
+			$columnIndex = $this->getFirstLetterForCategory( $res, $field->getResultSubject() );
 
 			while ( ( $text = $field->getNextText( SMW_OUTPUT_WIKI, $this->getLinker( $first_col ) ) ) !== false ) {
 
