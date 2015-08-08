f242b37e1895e067f6a33c98fbec27737e33ab0a
mwjames <jamesin.hongkong.1@gmail.com>
2015-08-08 16:17:37 +0200

    #set to indicate last-element in template
    Allows to do something like `<includeonly>{{{value}}}{{#if: {{{last-element|}}} | |,}} </includeonly>
    `

--- tests/phpunit/includes/parserhooks/SetParserFunctionTest.php
+++ tests/phpunit/includes/parserhooks/SetParserFunctionTest.php
@@ -137,13 +137,13 @@ class SetParserFunctionTest extends \PHPUnit_Framework_TestCase {
 
 	public function testTemplateSupport() {
 
-		$params = array( 'Foo=bar', 'BarFoo=9001', 'template=FooTemplate' );
+		$params = array( 'Foo=bar', 'Foo=foobar', 'BarFoo=9001', 'template=FooTemplate' );
 
 		$expected = array(
 			'errors' => 0,
 			'propertyCount'  => 2,
 			'propertyLabels' => array( 'Foo', 'BarFoo' ),
-			'propertyValues' => array( 'Bar', '9001' )
+			'propertyValues' => array( 'Bar', '9001', 'Foobar' )
 		);
 
 		$parserData = $this->applicationFactory->newParserData(
@@ -166,7 +166,10 @@ class SetParserFunctionTest extends \PHPUnit_Framework_TestCase {
 		$parser->expects( $this->once() )
 			->method( 'recursiveTagParse' )
 			->with(
-				$this->stringContains( '{{FooTemplate|property=Foo|value=bar|#=0}}{{FooTemplate|property=BarFoo|value=9001|#=1}}' ) );
+				$this->stringContains(
+					'{{FooTemplate|property=Foo|value=bar|last-element=|#=0}}' .
+					'{{FooTemplate|property=Foo|value=foobar|last-element=1|#=1}}' .
+					'{{FooTemplate|property=BarFoo|value=9001|last-element=1|#=2}}' ) );
 
 		$templateRenderer = new HtmlTemplateRenderer(
 			new WikitextTemplateRenderer(),
