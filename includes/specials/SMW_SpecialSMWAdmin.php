ffbc1d84daff95a1c1889e3a0ea1864ccadebda2
jeroendedauw <jeroendedauw@gmail.com>
2016-06-17 08:26:03 +0200

    Made function call more obvious in printRawOutput

--- includes/specials/SMW_SpecialSMWAdmin.php
+++ includes/specials/SMW_SpecialSMWAdmin.php
@@ -367,7 +367,7 @@ class SMWAdmin extends SpecialPage {
 		print "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\" dir=\"ltr\">\n<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>Semantic MediaWiki</title></head><body><p><pre>";
 		// @codingStandardsIgnoreEnd
 		// header( "Content-type: text/html; charset=UTF-8" );
-		is_callable( $text ) ? $text( $this ) : $text;
+		is_callable( $text ) ? $text( $this ) : $text();
 		print '</pre></p>';
 		// @codingStandardsIgnoreStart phpcs, ignore --sniffs=Generic.Files.LineLength.MaxExceeded
 		print '<b> ' . wfMessage( 'smw_smwadmin_return', '<a href="' . htmlspecialchars( SpecialPage::getTitleFor( 'SMWAdmin' )->getFullURL() ) . '">Special:SMWAdmin</a>' )->text() . "</b>\n";
