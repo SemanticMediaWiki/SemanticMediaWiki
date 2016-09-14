3318d37090c38b4cfa9a77998bbbf96a43e09c3c
mwjames <jamesin.hongkong.1@gmail.com>
2016-09-14 07:44:37 +0000

    Update RequestOptions.php
    [skip ci]

--- src/RequestOptions.php
+++ src/RequestOptions.php
@@ -85,6 +85,42 @@ class RequestOptions {
 	}
 
 	/**
+	 * @since 2.5
+	 *
+	 * @param integer $limit
+	 */
+	public function setLimit( $limit ) {
+		$this->limit = $limit;
+	}
+
+	/**
+	 * @since 2.5
+	 *
+	 * @return integer
+	 */
+	public function getLimit() {
+		return $this->limit;
+	}
+
+	/**
+	 * @since 2.5
+	 *
+	 * @param integer $offset
+	 */
+	public function setOffset( $offset ) {
+		$this->offset = $offset;
+	}
+
+	/**
+	 * @since 2.5
+	 *
+	 * @return integer
+	 */
+	public function getOffset() {
+		return $this->offset;
+	}
+
+	/**
 	 * @since 2.4
 	 *
 	 * @return string
