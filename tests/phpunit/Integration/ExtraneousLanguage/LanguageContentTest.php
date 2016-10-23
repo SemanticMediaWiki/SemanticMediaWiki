c90943e5c61f9ef3bcd5acd72a8c44e2b778d357
mwjames <jamesin.hongkong.1@gmail.com>
2016-10-23 20:02:27 +0000

    ExtraneousLanguage / Extend the CanonicalPropertyAliases members, refs 1848 (#1943)

---
+++ tests/phpunit/Integration/ExtraneousLanguage/LanguageContentTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace SMW\Tests\Integration\ExtraneousLanguage;
+
+use SMW\ExtraneousLanguage\ExtraneousLanguage;
+use SMW\Tests\TestEnvironment;
+
+/**
+ * @group semantic-mediawiki
+ *
+ * @license GNU GPL v2+
+ * @since 2.5
+ *
+ * @author mwjames
+ */
+class LanguageContent extends \PHPUnit_Framework_TestCase {
+
+	protected function tearDown() {
+		ExtraneousLanguage::clear();
+		parent::tearDown();
+	}
+
+	/**
+	 * @dataProvider canonicalPropertyAliasesProvider
+	 */
+	public function testGetCanonicalPropertyAliases( $languageCode, $canonicalMatch, $aliasMatch, $expected ) {
+
+		$extraneousLanguage = ExtraneousLanguage::getInstance()->fetchByLanguageCode(
+			$languageCode
+		);
+
+		$canonicalPropertyAliases = $extraneousLanguage->getCanonicalPropertyAliases();
+
+		$this->assertEquals(
+			$expected,
+			$canonicalPropertyAliases[$canonicalMatch]
+		);
+	}
+
+	/**
+	 * @dataProvider canonicalPropertyAliasesProvider
+	 */
+	public function testGetPropertyAliases( $languageCode, $canonicalMatch, $aliasMatch, $expected ) {
+
+		$extraneousLanguage = ExtraneousLanguage::getInstance()->fetchByLanguageCode(
+			$languageCode
+		);
+
+		$propertyAliases = $extraneousLanguage->getPropertyAliases();
+
+		$this->assertEquals(
+			$expected,
+			$propertyAliases[$aliasMatch]
+		);
+	}
+
+	public function canonicalPropertyAliasesProvider() {
+
+		$provider[] = array(
+			'fr',
+			'Query size',
+			'Taille de la requête',
+			'_ASKSI'
+		);
+
+		return $provider;
+	}
+
+}
