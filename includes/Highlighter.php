bad14603ec8a9bac703cf8b4ae84f319bc4f1f4b
mwjames <jamesin.hongkong.1@gmail.com>
2017-03-26 03:30:24 +0000

    Highlighter to pre-process title content, refs 1905 (#2363)

--- includes/Highlighter.php
+++ includes/Highlighter.php
@@ -184,6 +184,11 @@ class Highlighter {
 
 		$language = is_string( $this->language ) ? $this->language : Message::USER_LANGUAGE;
 
+		// #1875
+		// title attribute contains stripped content to allow for a display in
+		// no-js environments, the tooltip will remove the element once it is
+		// loaded
+
 		return Html::rawElement(
 			'span',
 			array(
@@ -192,7 +197,7 @@ class Highlighter {
 				'data-content' => isset( $this->options['data-content'] ) ? $this->options['data-content'] : null,
 				'data-state'   => $this->options['state'],
 				'data-title'   => Message::get( $this->options['title'], Message::TEXT, $language ),
-				'title'        => strip_tags( htmlspecialchars_decode( str_replace( "[", "&#91;", $this->options['content'] ) ) )
+				'title'        => $this->createStrippedContentFrom( $this->options['content'], $language )
 			), Html::rawElement(
 					'span',
 					array(
@@ -276,4 +281,16 @@ class Highlighter {
 
 		return $settings;
 	}
+
+	private function createStrippedContentFrom( $content, $language ) {
+
+		// Pre-process the content when used as title to avoid breaking elements
+		// (URLs etc.)
+		if ( strpos( $content, '[' ) !== false || strpos( $content, '//' ) !== false ) {
+			$content = Message::get( array( 'smw-parse', $content ), Message::PARSE, $language );
+		}
+
+		return strip_tags( htmlspecialchars_decode( str_replace( "[", "&#91;", $content ) ) );
+	}
+
 }
