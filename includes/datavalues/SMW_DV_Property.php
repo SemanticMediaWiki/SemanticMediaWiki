a3499b66294997af90aa36952837811677b0e8c8
mwjames <jamesin.hongkong.1@gmail.com>
2017-05-21 10:29:47 +0000

    Capitalize predefined property only, refs 673, 2282 (#2475)

--- includes/datavalues/SMW_DV_Property.php
+++ includes/datavalues/SMW_DV_Property.php
@@ -175,21 +175,25 @@ class SMWPropertyValue extends SMWDataValue {
 			$this->getOption( self::OPT_QUERY_CONTEXT )
 		);
 
-		$propertyValueParser->requireUpperCase(
-			$this->getContextPage() !== null &&
-			$this->getContextPage()->getNamespace() === SMW_NS_PROPERTY
+		$reqCapitalizedFirstChar = $this->getContextPage() !== null && $this->getContextPage()->getNamespace() === SMW_NS_PROPERTY;
+
+		$propertyValueParser->reqCapitalizedFirstChar(
+			$reqCapitalizedFirstChar
 		);
 
-		list( $propertyName, $inverse ) = $propertyValueParser->parse( $value );
+		list( $propertyName, $capitalizedName, $inverse ) = $propertyValueParser->parse( $value );
 
 		foreach ( $propertyValueParser->getErrors() as $error ) {
 			return $this->addErrorMsg( $error, Message::PARSE );
 		}
 
-		$contentLanguage = $this->getOption( self::OPT_CONTENT_LANGUAGE );
-
 		try {
-			$this->m_dataitem = DIProperty::newFromUserLabel( $propertyName, $inverse, $contentLanguage );
+			$this->m_dataitem = $this->createDataItemFrom(
+				$reqCapitalizedFirstChar,
+				$propertyName,
+				$capitalizedName,
+				$inverse
+			);
 		} catch ( SMWDataItemException $e ) { // happens, e.g., when trying to sort queries by property "-"
 			$this->addErrorMsg( array( 'smw_noproperty', $value ) );
 			$this->m_dataitem = new DIProperty( 'ERROR', false ); // just to have something
@@ -520,6 +524,21 @@ class SMWPropertyValue extends SMWDataValue {
 	 */
 	public function getText() {
 		return $this->m_dataitem->getLabel();
+	}
+
+	private function createDataItemFrom( $reqCapitalizedFirstChar, $propertyName, $capitalizedName, $inverse ) {
+
+		$contentLanguage = $this->getOption( self::OPT_CONTENT_LANGUAGE );
+
+		// Probe on capitalizedFirstChar because we only want predefined
+		// properties (e.g. Has type vs. has type etc.) to adhere the rule while
+		// custom (user) defined properties can appear in any form
+		if ( $reqCapitalizedFirstChar ) {
+			$dataItem = DIProperty::newFromUserLabel( $capitalizedName, $inverse, $contentLanguage );
+			$propertyName = $dataItem->isUserDefined() ? $propertyName : $capitalizedName;
+		}
+
+		return DIProperty::newFromUserLabel( $propertyName, $inverse, $contentLanguage );
 	}
 
 }
