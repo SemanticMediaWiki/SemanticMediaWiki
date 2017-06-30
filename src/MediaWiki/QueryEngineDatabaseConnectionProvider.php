c1e3fd215dd9f6fa65182ef16e828e8cffa8140d
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-06-30 16:56:25 -0500

    add private $resetTransactionProfiler; consistent docs

--- src/MediaWiki/QueryEngineDatabaseConnectionProvider.php
+++ src/MediaWiki/QueryEngineDatabaseConnectionProvider.php
@@ -2,22 +2,39 @@
 
 namespace SMW\MediaWiki;
 
-
 use SMW\DBConnectionProvider;
 
 /**
  * QueryEngineDatabase use slave for read and write operation.
  * There is no write in QueryEngine but ->query() use writeConnection.
  * Query needs to create temporary tables and insert data to it.
- * @package SMW\MediaWiki
+ *
+ * @license GNU GPL v2+
+ * @since 2.5
+ *
+ * @author Sqreek
  */
 class QueryEngineDatabaseConnectionProvider implements DBConnectionProvider {
 
+	/**
+	 * @var Database
+	 */
 	private $connection = null;
 
 	/**
+	 * @var boolean
+	 */
+	private $resetTransactionProfiler = false;
+
+	/**
+	 * @see DBConnectionProvider::getConnection
+	 *
 	 * Returns the database connection.
 	 * Initialization of this connection is done if it was not already initialized.
+	 *
+	 * @since 2.5
+	 *
+	 * @return Database
 	 */
 	public function getConnection() {
 		if ($this->connection === null) {
@@ -29,6 +46,10 @@ class QueryEngineDatabaseConnectionProvider implements DBConnectionProvider {
 
 	/**
 	 * Releases the connection if doing so makes any sense resource wise.
+	 *
+	 * @see DBConnectionProvider::releaseConnection
+	 *
+	 * @since 2.5
 	 */
 	public function releaseConnection() {
 		$this->connection = null;
