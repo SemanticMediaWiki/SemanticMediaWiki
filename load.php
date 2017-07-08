03996eb13f2013902592c933b23cbebca6924da3
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2017-07-08 20:11:30 +0200

    2.5.3 release (#2537)
    * Create RELEASE-NOTES-2.5.3.md
    ** Added information about 2.5.3
    * Update README.md
    ** Added link to RELEASE-NOTES for 2.5.3
    * Update load.php
    ** Bump version to 2.5.3

--- load.php
+++ load.php
@@ -60,7 +60,7 @@ class SemanticMediaWiki {
 	 */
 	public static function initExtension() {
 
-		define( 'SMW_VERSION', '2.5.2' );
+		define( 'SMW_VERSION', '2.5.3' );
 
 		// Registration of the extension credits, see Special:Version.
 		$GLOBALS['wgExtensionCredits']['semantic'][] = array(
