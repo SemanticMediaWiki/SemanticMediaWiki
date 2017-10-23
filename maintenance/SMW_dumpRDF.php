78492b521d2e43a6bfa7f08c855387e31e643517
Karsten Hoffmeyer <karsten@hoffmeyer.info>
2017-10-23 17:47:01 +0200

    Remove deprecated entry points for maintenance scripts
    Refs: https://github.com/SemanticMediaWiki/SemanticMediaWiki/issues/2789

--- maintenance/SMW_dumpRDF.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * This script will be forwarded to dumpRDF.php.
- *
- * @deprecated 2.0 This maintenance script has been deprecated, please use
- * dumpRDF.php instead.
- */
-require_once ( 'dumpRDF.php' );
--- maintenance/SMW_refreshData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * This script will be forwarded to rebuildData.php.
- *
- * @deprecated 1.9.2 This maintenance script has been deprecated, please use
- * rebuildData.php instead.
- */
-require_once ( 'rebuildData.php' );
