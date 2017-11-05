8f1128e0452045f181c991a40f9aff36c5473515
mwjames <jamesin.hongkong.1@gmail.com>
2017-11-05 13:35:57 +0000

    Add CsvFileIterator (#2800)

---
+++ src/Exception/FileNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SMW\Exception;
+
+use RuntimeException;
+
+/**
+ * @license GNU GPL v2+
+ * @since 3.0
+ *
+ * @author mwjames
+ */
+class FileNotFoundException extends RuntimeException {
+
+}
