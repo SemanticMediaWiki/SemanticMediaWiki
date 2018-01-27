4a27284e4565a0a614b5633594c320106712884e
mwjames <jamesin.hongkong.1@gmail.com>
2018-01-27 16:09:15 +0900

    Add DataTypeRegistry::getFieldType

--- tests/phpunit/Unit/DataTypeRegistryTest.php
+++ tests/phpunit/Unit/DataTypeRegistryTest.php
@@ -293,6 +293,36 @@ class DataTypeRegistryTest extends \PHPUnit_Framework_TestCase {
 		);
 	}
 
+	public function testGetFieldType() {
+
+		$extraneousLanguage = $this->getMockBuilder( '\SMW\ExtraneousLanguage\ExtraneousLanguage' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$extraneousLanguage->expects( $this->once() )
+			->method( 'getDatatypeLabels' )
+			->will( $this->returnValue( [] ) );
+
+		$extraneousLanguage->expects( $this->once() )
+			->method( 'getDatatypeAliases' )
+			->will( $this->returnValue( [] ) );
+
+		$extraneousLanguage->expects( $this->once() )
+			->method( 'getCanonicalDatatypeLabels' )
+			->will( $this->returnValue( [] ) );
+
+		$instance = new DataTypeRegistry(
+			$extraneousLanguage
+		);
+
+		$instance->registerDataType( '_foo', 'FooValue', DataItem::TYPE_BLOB, false, true );
+
+		$this->assertEquals(
+			'_txt',
+			$instance->getFieldType( '_foo' )
+		);
+	}
+
 	protected function assertRegistryFindsIdForLabels( $inputLabel, array $equivalentLabels ) {
 
 		$id = '_wpg';
