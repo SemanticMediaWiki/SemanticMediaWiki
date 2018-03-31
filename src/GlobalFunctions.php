ff930dac48152cf606b37f08696ffeebc7b60cba
mwjames <jamesin.hongkong.1@gmail.com>
2018-03-31 10:38:40 +0000

    Add support for extension.json, refs 1367 (#1732)

--- src/GlobalFunctions.php
+++ src/GlobalFunctions.php
@@ -216,6 +216,9 @@ function smwfGetLinker() {
 function enableSemantics( $namespace = null, $complete = false ) {
 	global $smwgNamespace;
 
+	// #1732 + #2813
+	wfLoadExtension( 'SemanticMediaWiki' );
+
 	// Apparently this is required (1.28+) as the earliest possible execution
 	// point in order for settings that refer to the SMW_NS_PROPERTY namespace
 	// to be available in LocalSettings
