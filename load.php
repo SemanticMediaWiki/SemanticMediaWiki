55b550a34e2013b7e7c9012586f387f84bba72fa
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2018-08-09 09:33:11 +0200

    2.5.7 release (#3297)
    * Update RELEASE-NOTES.md
    ** Provide RELEASE-NOTES for the 2.5.7 release
    * Update README.md
    ** Provide link to latest RELEASE-NOTES
    * Update load.php
    ** Increment version number

--- load.php
+++ load.php
@@ -60,7 +60,7 @@ class SemanticMediaWiki {
 	 */
 	public static function initExtension() {
 
-		define( 'SMW_VERSION', '2.5.7-alpha' );
+		define( 'SMW_VERSION', '2.5.7' );
 
 		// Registration of the extension credits, see Special:Version.
 		$GLOBALS['wgExtensionCredits']['semantic'][] = array(
