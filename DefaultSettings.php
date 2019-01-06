af0425541513a5cc98aac72d37e63a81a9165e8c
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2019-01-06 18:08:07 +0100

    Extend `smwgURITypeSchemeList` array (#3566)

--- DefaultSettings.php
+++ DefaultSettings.php
@@ -1995,8 +1995,8 @@ return [
 	# @since 3.0
 	##
 	'smwgURITypeSchemeList' => [
-		'http', 'https', 'mailto', 'tel', 'ftp', 'news', 'file', 'urn', 'telnet',
-		'ldap', 'gopher'
+		'http', 'https', 'mailto', 'tel', 'ftp', 'sftp', 'news', 'file', 'urn',
+		'telnet', 'ldap', 'gopher', 'ssh', 'git', 'irc', 'ircs'
 	],
 	##
 
