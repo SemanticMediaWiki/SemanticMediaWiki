9152f948271393131de7515ffd73417579f6a4ed
mwjames <jamesin.hongkong.1@gmail.com>
2019-01-13 19:46:36 +0000

    SemanticDataLookup, use `DISTINCT` for non subject items, refs 2928, 3531 (#3617)

--- tests/phpunit/Unit/SQLStore/EntityStore/SemanticDataLookupTest.php
+++ tests/phpunit/Unit/SQLStore/EntityStore/SemanticDataLookupTest.php
@@ -4,6 +4,7 @@ namespace SMW\Tests\SQLStore\EntityStore;
 
 use SMW\DIProperty;
 use SMW\DIWikiPage;
+use SMWDIBlob as DIBlob;
 use SMW\RequestOptions;
 use SMW\SQLStore\EntityStore\SemanticDataLookup;
 use SMW\Tests\PHPUnitCompat;
@@ -301,6 +302,53 @@ class SemanticDataLookupTest extends \PHPUnit_Framework_TestCase {
 			"SELECT p.smw_title AS prop, fooField AS v0,  AS v2 FROM  " .
 			"INNER JOIN smw_object_ids AS p ON p_id=p.smw_id " .
 			"WHERE (s_id='42') AND (p.smw_iw!=':smw') AND (p.smw_iw!=':smw-delete')",
+			$this->query->build()
+		);
+	}
+
+	public function testFetchSemanticData_NonWikiPageTable_DISTINCT_SELECT() {
+
+		$row = new \stdClass;
+		$row->prop = 'FOO';
+		$row->v0 = '1001';
+
+		$this->dataItemHandler->expects( $this->any() )
+			->method( 'getFetchFields' )
+			->will( $this->returnValue( [ 'fooField' => 'fieldType' ] ) );
+
+		$propertyTable = $this->getMockBuilder( '\SMW\SQLStore\PropertyTableDefinition' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$propertyTable->expects( $this->atLeastOnce() )
+			->method( 'getDIType' )
+			->will( $this->returnValue( 'Foo' ) );
+
+		$propertyTable->expects( $this->atLeastOnce() )
+			->method( 'getName' )
+			->will( $this->returnValue( 'bar_table' ) );
+
+		$this->connection->expects( $this->any() )
+			->method( 'addQuotes' )
+			->will( $this->returnCallback( function( $value ) { return "'$value'"; } ) );
+
+		$this->connection->expects( $this->once() )
+			->method( 'query' )
+			->will( $this->returnValue( [ $row ] ) );
+
+		$dataItem = new DIBlob( __METHOD__ );
+
+		$requestOptions = new RequestOptions();
+		$requestOptions->setLimit( 4 );
+
+		$instance = new SemanticDataLookup(
+			$this->store
+		);
+
+		$instance->fetchSemanticData( 42, $dataItem, $propertyTable, $requestOptions );
+
+		$this->assertEquals(
+			"SELECT DISTINCT fooField AS v0 FROM bar_table WHERE (p_id='42') LIMIT 4",
 			$this->query->build()
 		);
 	}
