cfbd338b2a941f66f574106a6f7047df3c5cfe91
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2019-01-17 22:33:32 +0100

    Allows recurring events to start on a 30th or 31st of a month (#3622)
    * Allows recurring events to start on a 30th or 31st of a month
    
    Code change as suggested by @mwjames https://github.com/SemanticMediaWiki/SemanticMediaWiki/issues/3619#issuecomment-454833738
    
    * Provides test for end of month recurring events

--- includes/RecurringEvents.php
+++ includes/RecurringEvents.php
@@ -283,13 +283,15 @@ class RecurringEvents {
 					$display_month = ( $cur_month == 0 ) ? 12 : $cur_month;
 				}
 
-				// If the date is February 29, and this isn't
-				// a leap year, change it to February 28.
-				if ( $cur_month == 2 && $cur_day == 29 ) {
-					if ( !date( 'L', strtotime( "$cur_year-1-1" ) ) ) {
-						$cur_day = 28;
-					}
-				}
+				// If the date is greater than 28 for February, and it is not
+				// a leap year, change it to be a fixed 28 otherwise set it to
+				// 29 (for a leap year date)
+				if ( $cur_month == 2 && $cur_day > 28 ) {
+					$cur_day = !date( 'L', strtotime( "$cur_year-1-1" ) ) ? 28 : 29;
+				} elseif ( $cur_day > 30 ) {
+					// Check whether 31 is a valid day of a month
+					$cur_day = ( $display_month - 1 ) % 7 % 2 ? 30 : 31;
+ 				}
 
 				$date_str = "$cur_year-$display_month-$cur_day $cur_time";
 				$cur_date = DataValueFactory::getInstance()->newTypeIDValue( '_dat', $date_str );
