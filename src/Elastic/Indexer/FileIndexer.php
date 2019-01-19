87f64513d092be407040d1947e882ad56a96938e
mwjames <jamesin.hongkong.1@gmail.com>
2019-01-20 06:19:30 +0900

    [ES] FileIndexer isolate `readURL`

--- src/Elastic/Indexer/FileIndexer.php
+++ src/Elastic/Indexer/FileIndexer.php
@@ -45,6 +45,11 @@ class FileIndexer {
 	private $sha1Check = true;
 
 	/**
+	 * @var callable
+	 */
+	private $readCallback;
+
+	/**
 	 * @since 3.0
 	 *
 	 * @param Indexer $indexer
@@ -74,13 +79,55 @@ class FileIndexer {
 	 *
 	 * @param File|null $file
 	 */
-	public function planIngestJob( Title $title ) {
+	public function pushIngestJob( Title $title ) {
 
 		$fileIngestJob = new FileIngestJob(
 			$title
 		);
 
 		$fileIngestJob->lazyPush();
+	}
+
+	/**
+	 * @since 3.1
+	 *
+	 * @param callable $readCallback
+	 */
+	public function setReadCallback( callable $readCallback ) {
+		$this->readCallback = $readCallback;
+	}
+
+	/**
+	 * @since 3.1
+	 *
+	 * @param string $url
+	 *
+	 * @return string
+	 */
+	public function readURL( $url ) {
+
+		//PHP 7.1+
+		$readCallback = $this->readCallback;
+
+		if ( $this->readCallback !== null ) {
+			return $readCallback( $url );
+		}
+
+		$contents = '';
+
+		// Avoid a "failed to open stream: HTTP request failed! HTTP/1.1 404 Not Found"
+		$file_headers = @get_headers( $url );
+
+		if ( $file_headers !== false && $file_headers[0] !== 'HTTP/1.1 404 Not Found' && $file_headers[0] !== 'HTTP/1.0 404 Not Found' ) {
+			return file_get_contents( $url );
+		}
+
+		$this->logger->info(
+			[ 'File indexer', 'HTTP/1.1 404 Not Found', '{url}' ],
+			[ 'method' => __METHOD__, 'role' => 'production', 'origin' => $this->origin, 'url' => $url ]
+		);
+
+		return $contents;
 	}
 
 	/**
@@ -193,16 +240,7 @@ class FileIndexer {
 			return $this->logger->info( $msg, $context );
 		}
 
-		$contents = '';
-
-		// Avoid a "failed to open stream: HTTP request failed! HTTP/1.1 404 Not Found"
-		$file_headers = @get_headers( $url );
-
-		if ( $file_headers !== false && $file_headers[0] !== 'HTTP/1.1 404 Not Found' && $file_headers[0] !== 'HTTP/1.0 404 Not Found' ) {
-			$contents = file_get_contents( $url );
-		} else {
-			$this->logger->info( [ 'File indexer', "HTTP/1.1 404 Not Found for $url" ], $context );
-		}
+		$contents = $this->readURL( $url );
 
 		$params += [
 			'pipeline' => 'attachment',
