4f412d0670338d399395d9c3c278bfc95abb1e30
mwjames <jamesin.hongkong.1@gmail.com>
2019-03-09 19:29:36 +0900

    Use `property_list`, refs 3749

--- tests/phpunit/Unit/MediaWiki/Specials/Browse/GroupFormatterTest.php
+++ tests/phpunit/Unit/MediaWiki/Specials/Browse/GroupFormatterTest.php
@@ -91,7 +91,7 @@ class GroupFormatterTest extends \PHPUnit_Framework_TestCase {
 	public function testFindGroupMembershipFromSchema() {
 
 		$data = [
-			'Foo_schema' => [ 'properties' => [ 'Foo' ] ]
+			'Foo_schema' => [ 'property_list' => [ 'Foo' ] ]
 		];
 
 		$schemaDefinition = $this->getMockBuilder( '\SMW\Schema\SchemaDefinition' )
