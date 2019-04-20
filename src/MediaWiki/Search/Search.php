57da6052526ecc6953a0e585d8b3ac5e96e13795
mwjames <jamesin.hongkong.1@gmail.com>
2019-04-20 17:48:49 +0000

    [SMWSearch] Tidy (Search) (#3956)

--- src/MediaWiki/Search/Search.php
+++ src/MediaWiki/Search/Search.php
@@ -30,9 +30,15 @@ use SMW\Exception\ClassNotFoundException;
  */
 class Search extends SearchEngine {
 
-	private $fallbackSearch = null;
+	/**
+	 * @var SearchEngine
+	 */
+	private $fallbackSearch;
 
-	private $database = null;
+	/**
+	 * @var Database
+	 */
+	private $connection;
 
 	/**
 	 * @var array
@@ -70,6 +76,234 @@ class Search extends SearchEngine {
 			// MediaWiki default
 			'relevance'
 		];
+	}
+
+	/**
+	 * @see SearchEngine::searchTitle
+	 *
+	 * Perform a title-only search query and return a result set.
+	 *
+	 * This method will try to find wiki pages by interpreting the search term as an SMW query.
+	 *
+	 * If successful, the pages according to the query will be returned.
+	 * If not, it falls back to the default search engine.
+	 *
+	 * @param string $term Raw search term
+	 *
+	 * @return SearchResultSet|null
+	 */
+	public function searchTitle( $term ) {
+
+		if ( $this->getSearchQuery( $term ) !== null ) {
+			return null;
+		}
+
+		return $this->searchFallbackSearchEngine( $term, false );
+	}
+
+	/**
+	 * @see SearchEngine::searchText
+	 *
+	 * Perform a full text search query and return a result set.
+	 * If title searches are not supported or disabled, return null.
+	 *
+	 * @param string $term Raw search term
+	 *
+	 * @return SearchResultSet|\Status|null
+	 */
+	public function searchText( $term ) {
+
+		if ( $this->getSearchQuery( $term ) !== null ) {
+			return $this->newSearchResultSet( $term );
+		}
+
+		return $this->searchFallbackSearchEngine( $term, true );
+	}
+
+	/**
+	 * @see SearchEngine::supports
+	 *
+	 * @param string $feature
+	 *
+	 * @return bool
+	 */
+	public function supports( $feature ) {
+		return $this->getFallbackSearchEngine()->supports( $feature );
+	}
+
+	/**
+	 * @see SearchEngine::normalizeText
+	 *
+	 * May performs database-specific conversions on text to be used for
+	 * searching or updating search index.
+	 *
+	 * @param string $string String to process
+	 *
+	 * @return string
+	 */
+	public function normalizeText( $string ) {
+		return $this->getFallbackSearchEngine()->normalizeText( $string );
+	}
+
+	/**
+	 * @see SearchEngine::getTextFromContent
+	 */
+	public function getTextFromContent( Title $t, Content $c = null ) {
+		return $this->getFallbackSearchEngine()->getTextFromContent( $t, $c );
+	}
+
+	/**
+	 * @see SearchEngine::textAlreadyUpdatedForIndex
+	 */
+	public function textAlreadyUpdatedForIndex() {
+		return $this->getFallbackSearchEngine()->textAlreadyUpdatedForIndex();
+	}
+
+	/**
+	 * @see SearchEngine::update
+	 *
+	 * Create or update the search index record for the given page.
+	 * Title and text should be pre-processed.
+	 *
+	 * @param int    $id
+	 * @param string $title
+	 * @param string $text
+	 */
+	public function update( $id, $title, $text ) {
+		$this->getFallbackSearchEngine()->update( $id, $title, $text );
+	}
+
+	/**
+	 * @see SearchEngine::updateTitle
+	 *
+	 * Update a search index record's title only.
+	 * Title should be pre-processed.
+	 *
+	 * @param int    $id
+	 * @param string $title
+	 */
+	public function updateTitle( $id, $title ) {
+		$this->getFallbackSearchEngine()->updateTitle( $id, $title );
+	}
+
+	/**
+	 * @see SearchEngine::delete
+	 *
+	 * Delete an indexed page
+	 * Title should be pre-processed.
+	 *
+	 * @param int    $id    Page id that was deleted
+	 * @param string $title Title of page that was deleted
+	 */
+	public function delete( $id, $title ) {
+		$this->getFallbackSearchEngine()->delete( $id, $title );
+	}
+
+	/**
+	 * @see SearchEngine::setFeatureData
+	 */
+	public function setFeatureData( $feature, $data ) {
+		parent::setFeatureData( $feature, $data );
+		$this->getFallbackSearchEngine()->setFeatureData( $feature, $data );
+	}
+
+	/**
+	 * @see SearchEngine::getFeatureData
+	 *
+	 * @param String $feature
+	 *
+	 * @return array|null
+	 */
+	public function getFeatureData( $feature ) {
+
+		if ( array_key_exists( $feature, $this->features ) ) {
+			return $this->features[$feature];
+		}
+
+		return null;
+	}
+
+	/**
+	 * @see SearchEngine::replacePrefixes
+	 *
+	 * SMW queries do not have prefixes. Returns query as is.
+	 *
+	 * @param string $query
+	 *
+	 * @return string
+	 */
+	public function replacePrefixes( $query ) {
+		return $query;
+	}
+
+	/**
+	 * @see SearchEngine::transformSearchTerm
+	 *
+	 * No Transformation needed. Returns term as is.
+	 * @param $term
+	 * @return mixed
+	 */
+	public function transformSearchTerm( $term ) {
+		return $term;
+	}
+
+	/**
+	 * @see SearchEngine::setLimitOffset
+	 */
+	public function setLimitOffset( $limit, $offset = 0 ) {
+		parent::setLimitOffset( $limit, $offset );
+		$this->getFallbackSearchEngine()->setLimitOffset( $limit, $offset );
+	}
+
+	/**
+	 * @see SearchEngine::setNamespaces
+	 */
+	public function setNamespaces( $namespaces ) {
+		parent::setNamespaces( $namespaces );
+		$this->getFallbackSearchEngine()->setNamespaces( $namespaces );
+	}
+
+	/**
+	 * @see SearchEngine::setShowSuggestion
+	 */
+	public function setShowSuggestion( $showSuggestion ) {
+		parent::setShowSuggestion( $showSuggestion );
+		$this->getFallbackSearchEngine()->setShowSuggestion( $showSuggestion );
+	}
+
+	/**
+	 * @see SearchEngine::completionSearchBackend
+	 *
+	 * Perform a completion search.
+	 *
+	 * @param string $search
+	 *
+	 * @return SearchSuggestionSet
+	 */
+	protected function completionSearchBackend( $search ) {
+
+		$searchResultSet = null;
+
+		// Avoid MW's auto formatting of title entities
+		if ( $search !== '' ) {
+			$search{0} = strtolower( $search{0} );
+		}
+
+		$searchEngine = $this->getFallbackSearchEngine();
+
+		if ( !$this->hasPrefixAndMinLenForCompletionSearch( $search, 3 ) ) {
+			return $searchEngine->completionSearch( $search );
+		}
+
+		if ( $this->getSearchQuery( $search ) !== null ) {
+			$searchResultSet = $this->newSearchResultSet( $search, false, false );
+		}
+
+		if ( $searchResultSet instanceof SearchResultSet ) {
+			return $searchResultSet->newSearchSuggestionSet();
+		}
+
+		return $searchEngine->completionSearch( $search );
 	}
 
 	/**
@@ -123,10 +357,31 @@ class Search extends SearchEngine {
 	}
 
 	/**
+	 * @return int
+	 */
+	public function getLimit() {
+		return $this->limit;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getShowSuggestion() {
+		return $this->showSuggestion;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getOffset() {
+		return $this->offset;
+	}
+
+	/**
 	 * @param DatabaseBase $connection
 	 */
 	public function setDB( DatabaseBase $connection ) {
-		$this->database = $connection;
+		$this->connection = $connection;
 		$this->fallbackSearch = null;
 	}
 
@@ -135,126 +390,17 @@ class Search extends SearchEngine {
 	 */
 	public function getDB() {
 
-		if ( $this->database === null ) {
-			$this->database = ApplicationFactory::getInstance()->getLoadBalancer()->getConnection( defined( 'DB_REPLICA' ) ? DB_REPLICA : DB_SLAVE );
+		if ( $this->connection !== null ) {
+			return $this->connection;
 		}
 
-		return $this->database;
-	}
+		$loadBalancer = ApplicationFactory::getInstance()->getLoadBalancer();
 
-	/**
-	 * @param String $term
-	 *
-	 * @return SMWQuery | null
-	 */
-	private function getSearchQuery( $term ) {
-
-		if ( $this->queryBuilder === null ) {
-			$this->queryBuilder = new QueryBuilder();
-		}
-
-		$this->queryString = $this->queryBuilder->getQueryString(
-			ApplicationFactory::getInstance()->getStore(),
-			$term
+		$this->connection = $loadBalancer->getConnection(
+			defined( 'DB_REPLICA' ) ? DB_REPLICA : DB_SLAVE
 		);
 
-		$query = $this->queryBuilder->getQuery(
-			$this->queryString
-		);
-
-		$this->queryBuilder->addSort( $query );
-
-		$this->queryBuilder->addNamespaceCondition(
-			$query,
-			$this->searchableNamespaces()
-		);
-
-		return $query;
-	}
-
-	private function searchFallbackSearchEngine( $term, $fulltext ) {
-
-		$f = $this->getFallbackSearchEngine();
-		$f->prefix = $this->prefix;
-		$f->namespaces = $this->namespaces;
-
-		$term = $f->transformSearchTerm( $term );
-		$term = $f->replacePrefixes( $term );
-
-		return $fulltext ? $f->searchText( $term ) : $f->searchTitle( $term );
-	}
-
-	/**
-	 * Perform a title-only search query and return a result set.
-	 *
-	 * This method will try to find wiki pages by interpreting the search term as an SMW query.
-	 *
-	 * If successful, the pages according to the query will be returned.
-	 * If not, it falls back to the default search engine.
-	 *
-	 * @param string $term Raw search term
-	 *
-	 * @return SearchResultSet|null
-	 */
-	public function searchTitle( $term ) {
-
-		if ( $this->getSearchQuery( $term ) !== null ) {
-			return null;
-		}
-
-		return $this->searchFallbackSearchEngine( $term, false );
-	}
-
-	/**
-	 * Perform a full text search query and return a result set.
-	 * If title searches are not supported or disabled, return null.
-	 *
-	 * @param string $term Raw search term
-	 *
-	 * @return SearchResultSet|\Status|null
-	 */
-	public function searchText( $term ) {
-
-		if ( $this->getSearchQuery( $term ) !== null ) {
-			return $this->newSearchResultSet( $term );
-		}
-
-		return $this->searchFallbackSearchEngine( $term, true );
-	}
-
-	/**
-	 * @see SearchEngine::completionSearchBackend
-	 *
-	 * Perform a completion search.
-	 *
-	 * @param string $search
-	 *
-	 * @return SearchSuggestionSet
-	 */
-	protected function completionSearchBackend( $search ) {
-
-		$searchResultSet = null;
-
-		// Avoid MW's auto formatting of title entities
-		if ( $search !== '' ) {
-			$search{0} = strtolower( $search{0} );
-		}
-
-		$searchEngine = $this->getFallbackSearchEngine();
-
-		if ( !$this->hasPrefixAndMinLenForCompletionSearch( $search, 3 ) ) {
-			return $searchEngine->completionSearch( $search );
-		}
-
-		if ( $this->getSearchQuery( $search ) !== null ) {
-			$searchResultSet = $this->newSearchResultSet( $search, false, false );
-		}
-
-		if ( $searchResultSet instanceof SearchResultSet ) {
-			return $searchResultSet->newSearchSuggestionSet();
-		}
-
-		return $searchEngine->completionSearch( $search );
+		return $this->connection;
 	}
 
 	private function hasPrefixAndMinLenForCompletionSearch( $term, $minLen ) {
@@ -313,141 +459,45 @@ class Search extends SearchEngine {
 	}
 
 	/**
-	 * @param string $feature
+	 * @param String $term
 	 *
-	 * @return bool
+	 * @return SMWQuery | null
 	 */
-	public function supports( $feature ) {
-		return $this->getFallbackSearchEngine()->supports( $feature );
-	}
+	private function getSearchQuery( $term ) {
 
-	/**
-	 * May performs database-specific conversions on text to be used for
-	 * searching or updating search index.
-	 *
-	 * @param string $string String to process
-	 *
-	 * @return string
-	 */
-	public function normalizeText( $string ) {
-		return $this->getFallbackSearchEngine()->normalizeText( $string );
-	}
-
-	public function getTextFromContent( Title $t, Content $c = null ) {
-		return $this->getFallbackSearchEngine()->getTextFromContent( $t, $c );
-	}
-
-	public function textAlreadyUpdatedForIndex() {
-		return $this->getFallbackSearchEngine()->textAlreadyUpdatedForIndex();
-	}
-
-	/**
-	 * Create or update the search index record for the given page.
-	 * Title and text should be pre-processed.
-	 *
-	 * @param int    $id
-	 * @param string $title
-	 * @param string $text
-	 */
-	public function update( $id, $title, $text ) {
-		$this->getFallbackSearchEngine()->update( $id, $title, $text );
-	}
-
-	/**
-	 * Update a search index record's title only.
-	 * Title should be pre-processed.
-	 *
-	 * @param int    $id
-	 * @param string $title
-	 */
-	public function updateTitle( $id, $title ) {
-		$this->getFallbackSearchEngine()->updateTitle( $id, $title );
-	}
-
-	/**
-	 * Delete an indexed page
-	 * Title should be pre-processed.
-	 *
-	 * @param int    $id    Page id that was deleted
-	 * @param string $title Title of page that was deleted
-	 */
-	public function delete( $id, $title ) {
-		$this->getFallbackSearchEngine()->delete( $id, $title );
-	}
-
-	public function setFeatureData( $feature, $data ) {
-		parent::setFeatureData( $feature, $data );
-		$this->getFallbackSearchEngine()->setFeatureData( $feature, $data );
-	}
-
-	/**
-	 * @param String $feature
-	 *
-	 * @return array|null
-	 */
-	public function getFeatureData( $feature ) {
-
-		if ( array_key_exists( $feature, $this->features ) ) {
-			return $this->features[$feature];
+		if ( $this->queryBuilder === null ) {
+			$this->queryBuilder = new QueryBuilder();
 		}
 
-		return null;
-	}
+		$this->queryString = $this->queryBuilder->getQueryString(
+			ApplicationFactory::getInstance()->getStore(),
+			$term
+		);
 
-	/**
-	 * SMW queries do not have prefixes. Returns query as is.
-	 *
-	 * @param string $query
-	 *
-	 * @return string
-	 */
-	public function replacePrefixes( $query ) {
+		$query = $this->queryBuilder->getQuery(
+			$this->queryString
+		);
+
+		$this->queryBuilder->addSort( $query );
+
+		$this->queryBuilder->addNamespaceCondition(
+			$query,
+			$this->searchableNamespaces()
+		);
+
 		return $query;
 	}
 
-	/**
-	 * No Transformation needed. Returns term as is.
-	 * @param $term
-	 * @return mixed
-	 */
-	public function transformSearchTerm( $term ) {
-		return $term;
-	}
+	private function searchFallbackSearchEngine( $term, $fulltext ) {
 
-	/**
-	 * @return int
-	 */
-	public function getLimit() {
-		return $this->limit;
-	}
+		$f = $this->getFallbackSearchEngine();
+		$f->prefix = $this->prefix;
+		$f->namespaces = $this->namespaces;
 
-	/**
-	 * @return int
-	 */
-	public function getOffset() {
-		return $this->offset;
-	}
+		$term = $f->transformSearchTerm( $term );
+		$term = $f->replacePrefixes( $term );
 
-	/**
-	 * @return boolean
-	 */
-	public function getShowSuggestion() {
-		return $this->showSuggestion;
-	}
-
-	public function setLimitOffset( $limit, $offset = 0 ) {
-		parent::setLimitOffset( $limit, $offset );
-		$this->getFallbackSearchEngine()->setLimitOffset( $limit, $offset );
-	}
-
-	public function setNamespaces( $namespaces ) {
-		parent::setNamespaces( $namespaces );
-		$this->getFallbackSearchEngine()->setNamespaces( $namespaces );
-	}
-
-	public function setShowSuggestion( $showSuggestion ) {
-		parent::setShowSuggestion( $showSuggestion );
-		$this->getFallbackSearchEngine()->setShowSuggestion( $showSuggestion );
+		return $fulltext ? $f->searchText( $term ) : $f->searchTitle( $term );
 	}
 
 	/**
