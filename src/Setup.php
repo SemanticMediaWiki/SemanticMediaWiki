9fd975ca46f01d9bd3e48824dec27b63c4dc0510
mwjames <jamesin.hongkong.1@gmail.com>
2019-06-02 21:31:57 +0000

    Add `smw-footer` class
    [skip ci]

--- src/Setup.php
+++ src/Setup.php
@@ -316,7 +316,8 @@ final class Setup {
 		$vars['wgFooterIcons']['poweredby']['semanticmediawiki'] = [
 			'src' => $src,
 			'url' => 'https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki',
-			'alt' => 'Powered by Semantic MediaWiki'
+			'alt' => 'Powered by Semantic MediaWiki',
+			'class' => 'smw-footer'
 		];
 	}
 
