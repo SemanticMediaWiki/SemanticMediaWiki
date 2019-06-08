9fc19f10ffc2431b664320b7ad3b3a0fbc826cbd
mwjames <jamesin.hongkong.1@gmail.com>
2019-06-09 03:09:51 +0900

    Add RecurringEventsTest, refs 3622

--- tests/phpunit/includes/RecurringEventsTest.php
+++ tests/phpunit/includes/RecurringEventsTest.php
@@ -164,247 +164,289 @@ class RecurringEventsTest extends \PHPUnit_Framework_TestCase {
 	 * @return array
 	 */
 	public function getParametersDataProvider() {
-		return [
-			// {{#set_recurring_event:property=Has birthday
-			// |start=01 Feb 1970
-			// |has title= Birthday
-			// |unit=year
-			// |period=12
-			// |limit=3
-			// }}
+
+		// {{#set_recurring_event:property=Has birthday
+		// |start=01 Feb 1970
+		// |has title= Birthday
+		// |unit=year
+		// |period=12
+		// |limit=3
+		// }}
+		yield [
 			[
-				[
-					'property=Has birthday',
-					'start=01 Feb 1970',
-					'has title=Birthday',
-					'unit=month',
-					'period=12',
-					'limit=3'
-				],
-				[
-					'errors' => 0,
-					'dates' => [ '1 February 1970', '1 February 1971', '1 February 1972', '1 February 1973' ],
-					'property' => 'Has birthday',
-					'parameters' => [ 'has title' => [ 'Birthday' ] ]
-				]
+				'property=Has birthday',
+				'start=01 Feb 1970',
+				'has title=Birthday',
+				'unit=month',
+				'period=12',
+				'limit=3'
 			],
-
-			// {{#set_recurring_event:property=Has birthday
-			// |start=01 Feb 1970
-			// |end=01 Feb 1972
-			// |has title= Birthday
-			// |unit=year
-			// |period=12
-			// |limit=3
-			// }}
 			[
-				[
-					'property=Has birthday',
-					'start=01 Feb 1970',
-					'end=01 Feb 1972',
-					'has title=Birthday',
-					'unit=month',
-					'period=12',
-					'limit=3'
-				],
-				[
-					'errors' => 0,
-					'dates' => [ '1 February 1970', '1 February 1971', '1 February 1972' ],
-					'property' => 'Has birthday',
-					'parameters' => [ 'has title' => [ 'Birthday' ] ]
-				]
-			],
+				'errors' => 0,
+				'dates' => [ '1 February 1970', '1 February 1971', '1 February 1972', '1 February 1973' ],
+				'property' => 'Has birthday',
+				'parameters' => [ 'has title' => [ 'Birthday' ] ]
+			]
+		];
 
-			// {{#set_recurring_event:property=Has birthday
-			// |start=01 Feb 1970
-			// |end=01 Feb 1972
-			// |has title= Birthday
-			// |unit=year
-			// |week number=2
-			// |period=12
-			// |limit=3
-			// }}
+		// {{#set_recurring_event:property=Has birthday
+		// |start=01 Feb 1970
+		// |end=01 Feb 1972
+		// |has title= Birthday
+		// |unit=year
+		// |period=12
+		// |limit=3
+		// }}
+		yield [
 			[
-				[
-					'property=Has birthday',
-					'start=01 Feb 1970',
-					'end=01 Feb 1972',
-					'has title=Birthday',
-					'unit=month',
-					'week number=2',
-					'period=12',
-					'limit=3'
-				],
-				[
-					'errors' => 0,
-					'dates' => [ '1 February 1970', '14 February 1971' ],
-					'property' => 'Has birthday',
-					'parameters' => [ 'has title' => [ 'Birthday' ] ]
-				]
+				'property=Has birthday',
+				'start=01 Feb 1970',
+				'end=01 Feb 1972',
+				'has title=Birthday',
+				'unit=month',
+				'period=12',
+				'limit=3'
 			],
-
-			// {{#set_recurring_event:property=Has birthday
-			// |start=01 Feb 1972 02:00
-			// |has title=Test 12
-			// |unit=week
-			// |period=4
-			// |limit=3
-			// }}
 			[
-				[
-					'property=Has birthday',
-					'start=01 Feb 1972 02:00',
-					'has title=Test 2',
-					'unit=week',
-					'period=4',
-					'limit=3'
-				],
-				[
-					'errors' => 0,
-					'dates' => [ '1 February 1972 02:00:00', '29 February 1972 02:00:00', '28 March 1972 02:00:00', '25 April 1972 02:00:00' ],
-					'property' => 'Has birthday',
-					'parameters' => [ 'has title' => [ 'Test 2' ] ]
-				]
-			],
+				'errors' => 0,
+				'dates' => [ '1 February 1970', '1 February 1971', '1 February 1972' ],
+				'property' => 'Has birthday',
+				'parameters' => [ 'has title' => [ 'Birthday' ] ]
+			]
+		];
 
-			// {{#set_recurring_event:property=Has date
-			// |start=January 4, 2010
-			// |unit=week
-			// |period=1
-			// |limit=4
-			// |include=March 16, 2010;March 23, 2010
-			// |exclude=January 18, 2010;January 25, 2010
-			// }}
+		// {{#set_recurring_event:property=Has birthday
+		// |start=01 Feb 1970
+		// |end=01 Feb 1972
+		// |has title= Birthday
+		// |unit=year
+		// |week number=2
+		// |period=12
+		// |limit=3
+		// }}
+		yield [
 			[
-				[
-					'property=Has date',
-					'start=January 4, 2010',
-					'unit=week',
-					'period=1',
-					'limit=4',
-					'include=March 16, 2010;March 23, 2010',
-					'exclude=January 18, 2010;January 25, 2010'
-				],
-				[
-					'errors' => 0,
-					'dates' => [ '4 January 2010', '11 January 2010', '1 February 2010', 'March 16, 2010', 'March 23, 2010' ],
-					'property' => 'Has date',
-					'parameters' => []
-				]
+				'property=Has birthday',
+				'start=01 Feb 1970',
+				'end=01 Feb 1972',
+				'has title=Birthday',
+				'unit=month',
+				'week number=2',
+				'period=12',
+				'limit=3'
 			],
-
-			// {{#set_recurring_event:property=Has date
-			// |start=January 4, 2010
-			// |unit=week
-			// |period=1
-			// |limit=4
-			// |include=March 16, 2010;March 23, 2010|+sep=;
-			// |exclude=January 18, 2010;January 25, 2010|+sep=;
-			// }}
 			[
-				[
-					'property=Has date',
-					'start=January 4, 2010',
-					'unit=week',
-					'period=1',
-					'limit=4',
-					'include=March 16, 2010;March 23, 2010',
-					'+sep=;',
-					'exclude=January 18, 2010;January 25, 2010',
-					'+sep=;'
-				],
-				[
-					'errors' => 0,
-					'dates' => [ '4 January 2010', '11 January 2010', '1 February 2010', 'March 16, 2010', 'March 23, 2010' ],
-					'property' => 'Has date',
-					'parameters' => []
-				]
-			],
+				'errors' => 0,
+				'dates' => [ '1 February 1970', '14 February 1971' ],
+				'property' => 'Has birthday',
+				'parameters' => [ 'has title' => [ 'Birthday' ] ]
+			]
+		];
 
-			// Simulate start date has wrong type
-
-			// {{#set_recurring_event:property=Has date
-			// |start=???
-			// |unit=week
-			// |period=1
-			// |limit=4
-			// |include=March 16, 2010;March 23, 2010
-			// |exclude=January 18, 2010;January 25, 2010
-			// }}
+		// {{#set_recurring_event:property=Has birthday
+		// |start=01 Feb 1972 02:00
+		// |has title=Test 12
+		// |unit=week
+		// |period=4
+		// |limit=3
+		// }}
+		yield [
 			[
-				[
-					'property=Has date',
-					'start=???',
-					'unit=week',
-					'period=1',
-					'limit=4',
-					'include=March 16, 2010;March 23, 2010',
-					'exclude=January 18, 2010;January 25, 2010'
-				],
-				[
-					'errors' => 1,
-					'dates' => [],
-					'property' => 'Has date',
-					'parameters' => []
-				]
+				'property=Has birthday',
+				'start=01 Feb 1972 02:00',
+				'has title=Test 2',
+				'unit=week',
+				'period=4',
+				'limit=3'
 			],
-
-			// Simulate missing start date
-
-			// {{#set_recurring_event:property=Has date
-			// |start=
-			// |unit=week
-			// |period=1
-			// |limit=4
-			// |include=March 16, 2010;March 23, 2010
-			// |exclude=January 18, 2010;January 25, 2010
-			// }}
 			[
-				[
-					'property=Has date',
-					'start=',
-					'unit=week',
-					'period=1',
-					'limit=4',
-					'include=March 16, 2010;March 23, 2010',
-					'exclude=January 18, 2010;January 25, 2010'
-				],
-				[
-					'errors' => 1,
-					'dates' => [],
-					'property' => 'Has date',
-					'parameters' => []
-				]
-			],
+				'errors' => 0,
+				'dates' => [ '1 February 1972 02:00:00', '29 February 1972 02:00:00', '28 March 1972 02:00:00', '25 April 1972 02:00:00' ],
+				'property' => 'Has birthday',
+				'parameters' => [ 'has title' => [ 'Test 2' ] ]
+			]
+		];
 
-			// Simulate missing property
-
-			// {{#set_recurring_event:property=
-			// |start=January 4, 2010
-			// |unit=week
-			// |period=1
-			// |limit=4
-			// |include=March 16, 2010;March 23, 2010|+sep=;
-			// |exclude=January 18, 2010;January 25, 2010|+sep=;
-			// }}
+		// {{#set_recurring_event:property=Has date
+		// |start=January 4, 2010
+		// |unit=week
+		// |period=1
+		// |limit=4
+		// |include=March 16, 2010;March 23, 2010
+		// |exclude=January 18, 2010;January 25, 2010
+		// }}
+		yield [
 			[
-				[
-					'property=',
-					'start=January 4, 2010',
-					'unit=week', 'period=1',
-					'limit=4',
-					'include=March 16, 2010;March 23, 2010',
-					'+sep=;',
-					'exclude=January 18, 2010;January 25, 2010',
-					'+sep=;'
-				],
-				[
-					'errors' => 1,
-					'dates' => [],
-					'property' => '',
-					'parameters' => []
-				]
+				'property=Has date',
+				'start=January 4, 2010',
+				'unit=week',
+				'period=1',
+				'limit=4',
+				'include=March 16, 2010;March 23, 2010',
+				'exclude=January 18, 2010;January 25, 2010'
 			],
+			[
+				'errors' => 0,
+				'dates' => [ '4 January 2010', '11 January 2010', '1 February 2010', 'March 16, 2010', 'March 23, 2010' ],
+				'property' => 'Has date',
+				'parameters' => []
+			]
+		];
+
+		// {{#set_recurring_event:property=Has date
+		// |start=January 4, 2010
+		// |unit=week
+		// |period=1
+		// |limit=4
+		// |include=March 16, 2010;March 23, 2010|+sep=;
+		// |exclude=January 18, 2010;January 25, 2010|+sep=;
+		// }}
+		yield [
+			[
+				'property=Has date',
+				'start=January 4, 2010',
+				'unit=week',
+				'period=1',
+				'limit=4',
+				'include=March 16, 2010;March 23, 2010',
+				'+sep=;',
+				'exclude=January 18, 2010;January 25, 2010',
+				'+sep=;'
+			],
+			[
+				'errors' => 0,
+				'dates' => [ '4 January 2010', '11 January 2010', '1 February 2010', 'March 16, 2010', 'March 23, 2010' ],
+				'property' => 'Has date',
+				'parameters' => []
+			]
+		];
+
+		// Simulate start date has wrong type
+
+		// {{#set_recurring_event:property=Has date
+		// |start=???
+		// |unit=week
+		// |period=1
+		// |limit=4
+		// |include=March 16, 2010;March 23, 2010
+		// |exclude=January 18, 2010;January 25, 2010
+		// }}
+		yield [
+			[
+				'property=Has date',
+				'start=???',
+				'unit=week',
+				'period=1',
+				'limit=4',
+				'include=March 16, 2010;March 23, 2010',
+				'exclude=January 18, 2010;January 25, 2010'
+			],
+			[
+				'errors' => 1,
+				'dates' => [],
+				'property' => 'Has date',
+				'parameters' => []
+			]
+		];
+
+		// Simulate missing start date
+
+		// {{#set_recurring_event:property=Has date
+		// |start=
+		// |unit=week
+		// |period=1
+		// |limit=4
+		// |include=March 16, 2010;March 23, 2010
+		// |exclude=January 18, 2010;January 25, 2010
+		// }}
+		yield [
+			[
+				'property=Has date',
+				'start=',
+				'unit=week',
+				'period=1',
+				'limit=4',
+				'include=March 16, 2010;March 23, 2010',
+				'exclude=January 18, 2010;January 25, 2010'
+			],
+			[
+				'errors' => 1,
+				'dates' => [],
+				'property' => 'Has date',
+				'parameters' => []
+			]
+		];
+
+		// Simulate missing property
+
+		// {{#set_recurring_event:property=
+		// |start=January 4, 2010
+		// |unit=week
+		// |period=1
+		// |limit=4
+		// |include=March 16, 2010;March 23, 2010|+sep=;
+		// |exclude=January 18, 2010;January 25, 2010|+sep=;
+		// }}
+		yield [
+			[
+				'property=',
+				'start=January 4, 2010',
+				'unit=week', 'period=1',
+				'limit=4',
+				'include=March 16, 2010;March 23, 2010',
+				'+sep=;',
+				'exclude=January 18, 2010;January 25, 2010',
+				'+sep=;'
+			],
+			[
+				'errors' => 1,
+				'dates' => [],
+				'property' => '',
+				'parameters' => []
+			]
+		];
+
+		// #3622, #3622
+
+		yield [
+			[
+				'property=Has date',
+				'start=31 January 2003 9:30 am',
+				'end=1 January 2005',
+				'unit=month', 'period=1'
+			],
+			[
+				'errors' => 0,
+				'dates' => [
+					'31 January 2003 09:30:00',
+					'28 February 2003 09:30:00',
+					'31 March 2003 09:30:00',
+					'30 April 2003 09:30:00',
+					'31 May 2003 09:30:00',
+					'30 June 2003 09:30:00',
+					'31 July 2003 09:30:00',
+					'31 August 2003 09:30:00',
+					'30 September 2003 09:30:00',
+					'31 October 2003 09:30:00',
+					'30 November 2003 09:30:00',
+					'31 December 2003 09:30:00',
+					'31 January 2004 09:30:00',
+					'29 February 2004 09:30:00',
+					'31 March 2004 09:30:00',
+					'30 April 2004 09:30:00',
+					'31 May 2004 09:30:00',
+					'30 June 2004 09:30:00',
+					'31 July 2004 09:30:00',
+					'31 August 2004 09:30:00',
+					'30 September 2004 09:30:00',
+					'31 October 2004 09:30:00',
+					'30 November 2004 09:30:00',
+					'31 December 2004 09:30:00'
+				],
+				'property' => 'Has date',
+				'parameters' => []
+			]
 		];
 	}
+
 }
