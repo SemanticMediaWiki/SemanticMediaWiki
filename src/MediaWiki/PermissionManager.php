d856c60c8841e2f44b4fc275f18c75a73f565fc1
mwjames <jamesin.hongkong.1@gmail.com>
2019-06-30 12:50:47 +0900

    Tidy (PermissionManager)

--- src/MediaWiki/PermissionManager.php
+++ src/MediaWiki/PermissionManager.php
@@ -21,6 +21,11 @@ class PermissionManager {
 	private $protectionValidator;
 
 	/**
+	 * @var []
+	 */
+	private $errors = [];
+
+	/**
 	 * @since 2.5
 	 *
 	 * @param ProtectionValidator $protectionValidator
@@ -30,49 +35,68 @@ class PermissionManager {
 	}
 
 	/**
+	 * @since 3.1
+	 *
+	 * @return []
+	 */
+	public function getErrors() {
+		return $this->errors;
+	}
+
+	/**
 	 * @since 2.5
 	 *
-	 * @param Title &$title
+	 * @param Title $title
 	 * @param User $user
 	 * @param string $action
 	 * @param array &$errors
 	 *
 	 * @return boolean
 	 */
-	public function checkQuickPermission( Title &$title, User $user, $action, &$errors ) {
-		return $this->hasUserPermission( $title, $user, $action, $errors );
+	public function checkQuickPermission( Title $title, User $user, $action, &$errors ) {
+
+		$hasUserPermission = $this->hasUserPermission( $title, $user, $action );
+
+		foreach ( $this->errors as $error ) {
+			$errors[] = $error;
+		}
+
+		return $hasUserPermission;
 	}
 
 	/**
 	 * @since 2.4
 	 *
-	 * @param Title &$title
+	 * @param Title $title
 	 * @param User $user
 	 * @param string $action
-	 * @param array &$errors
 	 *
 	 * @return boolean
 	 */
-	public function hasUserPermission( Title &$title, User $user, $action, &$errors ) {
+	public function hasUserPermission( Title $title, User $user, $action ) {
+
+		$this->errors = [];
 
 		if ( $title->getNamespace() === SMW_NS_SCHEMA ) {
-			return $this->checkSchemaNamespacePermission( $title, $user, $action, $errors );
+			return $this->checkSchemaNamespacePermission( $title, $user, $action );
 		}
 
-		if ( $action !== 'edit' && $action !== 'delete' && $action !== 'move' && $action !== 'upload' ) {
+		$actions = [ 'edit', 'delete', 'move', 'upload' ];
+
+		if ( !in_array( $action, $actions ) ) {
 			return true;
 		}
 
 		if ( $title->getNamespace() === NS_MEDIAWIKI ) {
-			return $this->checkMwNamespacePatternEditPermission( $title, $user, $action, $errors );
+			return $this->checkMwNamespacePatternEditPermission( $title, $user, $action );
 		}
 
 		if ( $this->protectionValidator->getCreateProtectionRight() && $title->getNamespace() === SMW_NS_PROPERTY ) {
-			return $this->checkPropertyNamespaceCreatePermission( $title, $user, $action, $errors );
+			return $this->checkPropertyNamespaceCreatePermission( $title, $user, $action );
 		}
 
 		if ( $title->getNamespace() === NS_CATEGORY ) {
-			return $this->checkChangePropagationProtection( $title, $user, $action, $errors );
+			return $this->checkChangePropagationProtection( $title, $user, $action );
 		}
 
 		if ( !$title->exists() ) {
@@ -80,50 +104,54 @@ class PermissionManager {
 		}
 
 		if ( $title->getNamespace() === SMW_NS_PROPERTY ) {
-			return $this->checkPropertyNamespaceEditPermission( $title, $user, $action, $errors );
+			return $this->checkPropertyNamespaceEditPermission( $title, $user, $action );
 		}
 
 		if ( $this->protectionValidator->hasEditProtectionOnNamespace( $title ) ) {
-			return $this->checkEditPermissionOn( $title, $user, $action, $errors );
+			return $this->checkEditPermission( $title, $user, $action );
 		}
 
 		return true;
 	}
 
-	private function checkMwNamespacePatternEditPermission( Title &$title, User $user, $action, &$errors ) {
+	private function checkMwNamespacePatternEditPermission( Title $title, User $user, $action ) {
 
 		// @see https://www.semantic-mediawiki.org/wiki/Help:Special_property_Allows_pattern
 		if ( $title->getDBKey() !== AllowsPatternValue::REFERENCE_PAGE_ID || $user->isAllowed( 'smw-patternedit' ) ) {
 			return true;
 		}
 
-		$errors[] = [ 'smw-patternedit-protection', 'smw-patternedit' ];
+		$this->errors[] = [ 'smw-patternedit-protection', 'smw-patternedit' ];
 
 		return false;
 	}
 
-	private function checkSchemaNamespacePermission( Title &$title, User $user, $action, &$errors ) {
+	private function checkSchemaNamespacePermission( Title $title, User $user, $action ) {
 
 		if ( !$user->isAllowed( 'smw-schemaedit' ) ) {
-			$errors[] = [ 'smw-schema-namespace-edit-protection', 'smw-schemaedit' ];
+			$this->errors[] = [ 'smw-schema-namespace-edit-protection', 'smw-schemaedit' ];
 			return false;
 		}
 
 		// Disallow to change the content model
 		if ( $action === 'editcontentmodel' ) {
-			$errors[] = [ 'smw-schema-namespace-editcontentmodel-disallowed' ];
+			$this->errors[] = [ 'smw-schema-namespace-editcontentmodel-disallowed' ];
 			return false;
 		}
 
 		return true;
 	}
 
-	private function checkPropertyNamespaceCreatePermission( Title &$title, User $user, $action, &$errors ) {
+	private function checkPropertyNamespaceCreatePermission( Title $title, User $user, $action ) {
 
-		$createProtectionRight = $this->protectionValidator->getCreateProtectionRight();
+		$protectionRight = $this->protectionValidator->getCreateProtectionRight();
 
-		if ( $user->isAllowed( $createProtectionRight ) ) {
-			return $this->checkPropertyNamespaceEditPermission( $title, $user, $action, $errors );;
+		if ( $protectionRight === false ) {
+			$protectionRight = $this->protectionValidator->getEditProtectionRight();
+		}
+
+		if ( $user->isAllowed( $protectionRight ) ) {
+			return $this->checkPropertyNamespaceEditPermission( $title, $user, $action );
 		}
 
 		$msg = 'smw-create-protection';
@@ -132,36 +160,36 @@ class PermissionManager {
 			$msg = 'smw-create-protection-exists';
 		}
 
-		$errors[] = [ $msg, $title->getText(), $createProtectionRight ];
+		$this->errors[] = [ $msg, $title->getText(), $protectionRight ];
 
 		return false;
 	}
 
-	private function checkPropertyNamespaceEditPermission( Title &$title, User $user, $action, &$errors ) {
+	private function checkPropertyNamespaceEditPermission( Title $title, User $user, $action ) {
 
 		// This renders full protection until the ChangePropagationDispatchJob was run
 		if ( !$this->protectionValidator->hasChangePropagationProtection( $title ) ) {
-			return $this->checkEditPermissionOn( $title, $user, $action, $errors );
+			return $this->checkEditPermission( $title, $user, $action );
 		}
 
-		$errors[] = [ 'smw-change-propagation-protection' ];
+		$this->errors[] = [ 'smw-change-propagation-protection' ];
 
 		return false;
 	}
 
-	private function checkChangePropagationProtection( Title &$title, User $user, $action, &$errors ) {
+	private function checkChangePropagationProtection( Title $title, User $user, $action ) {
 
 		// This renders full protection until the ChangePropagationDispatchJob was run
 		if ( !$this->protectionValidator->hasChangePropagationProtection( $title ) ) {
 			return true;
 		}
 
-		$errors[] = [ 'smw-change-propagation-protection' ];
+		$this->errors[] = [ 'smw-change-propagation-protection' ];
 
 		return false;
 	}
 
-	private function checkEditPermissionOn( Title &$title, User $user, $action, &$errors ) {
+	private function checkEditPermission( Title $title, User $user, $action ) {
 
 		$editProtectionRight = $this->protectionValidator->getEditProtectionRight();
 
@@ -170,7 +198,7 @@ class PermissionManager {
 			return true;
 		}
 
-		$errors[] = [ 'smw-edit-protection', $editProtectionRight ];
+		$this->errors[] = [ 'smw-edit-protection', $editProtectionRight ];
 
 		return false;
 	}
