cefd79420e18aa9dd5bc7fc2d53f07efa8ef1013
mwjames <jamesin.hongkong.1@gmail.com>
2019-07-27 19:47:29 +0000

    Remove option keys from maintenance log, refs #4164
    [skip ci]

--- src/Maintenance/MaintenanceLogger.php
+++ src/Maintenance/MaintenanceLogger.php
@@ -61,6 +61,12 @@ class MaintenanceLogger {
 			unset( $message['Options']['with-maintenance-log'] );
 			unset( $message['Options']['memory-limit'] );
 			unset( $message['Options']['profiler'] );
+			unset( $message['Options']['conf'] );
+
+			// If it is null then removed it.
+			if ( !isset( $message['Options']['auto-recovery'] ) ) {
+				unset( $message['Options']['auto-recovery'] );
+			}
 		}
 
 		$this->log( json_encode( $message ), $target );
