1ea3e50caa123da0b507194c2822a2ad8ffabd64
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2019-07-27 21:40:19 +0200

    Check `isConnectable` in `ConnectionManager`, refs 4150 (#4153)

--- includes/Settings.php
+++ includes/Settings.php
@@ -43,6 +43,14 @@ class Settings extends Options {
 	 */
 	public static function newFromGlobals() {
 
+		// #4150
+		// If someone tried to use SMW without proper initialization then something
+		// like "Notice: Undefined index: smwgNamespaceIndex ..." would appear and
+		// to produce a proper error message avoid those by adding a default.
+		if ( !defined( 'SMW_VERSION' ) && !isset( $GLOBALS['smwgNamespaceIndex'] ) ) {
+			NamespaceManager::initCustomNamespace( $GLOBALS );
+		}
+
 		$configuration = [
 			'smwgIP' => $GLOBALS['smwgIP'],
 			'smwgExtraneousLanguageFileDir' => $GLOBALS['smwgExtraneousLanguageFileDir'],
