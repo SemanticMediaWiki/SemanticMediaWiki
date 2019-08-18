6de4c38833351e50c9866180a9ff859736bf032e
mwjames <jamesin.hongkong.1@gmail.com>
2019-08-18 00:03:49 +0000

    Allow to set a thumb image, refs 4212 (#4246)

--- tests/phpunit/Unit/Utils/Html/SummaryTableTest.php
+++ tests/phpunit/Unit/Utils/Html/SummaryTableTest.php
@@ -62,4 +62,47 @@ class SummaryTableTest extends \PHPUnit_Framework_TestCase {
 		);
 	}
 
+	public function testBuildHTML_ColumnThreshold() {
+
+		$instance = new SummaryTable(
+			[ 'Foo' => 'Bar', 'Foobar' => 'Bar' ]
+		);
+
+		$instance->setColumnThreshold( 1 );
+
+		$this->stringValidator->assertThatStringContains(
+			[
+				'<div class="smw-summarytable-columns"><div class="smw-summarytable-columns-2">',
+				'<div class="smw-summarytable"><div class="smw-table smwfacttable">',
+				'<div class="smw-table-row"><div class="smw-table-cell smwpropname">Foo</div>',
+				'<div class="smw-table-cell smwprops">Bar</div></div></div></div></div>',
+				'<div class="smw-summarytable-columns-2"><div class="smw-summarytable"><div class="smw-table smwfacttable">',
+				'<div class="smw-table-row"><div class="smw-table-cell smwpropname">Foobar</div>',
+				'<div class="smw-table-cell smwprops">Bar</div></div></div></div></div></div>'
+			],
+			$instance->buildHTML( ['columns' => 2 ] )
+		);
+	}
+
+	public function testBuildHTML_ColumnThreshold_NoImage() {
+
+		$instance = new SummaryTable(
+			[ 'Foo' => 'Bar', 'Foobar' => 'Bar' ]
+		);
+
+		$instance->noImage();
+		$instance->setColumnThreshold( 1 );
+
+		$this->stringValidator->assertThatStringContains(
+			[
+				'<div class="smw-summarytable-imagecolumn"><div class="smw-summarytable-facts">',
+				'<div class="smw-summarytable"><div class="smw-table smwfacttable">',
+				'<div class="smw-table-row"><div class="smw-table-cell smwpropname">Foo</div><div class="smw-table-cell smwprops">Bar</div></div>',
+				'<div class="smw-table-row"><div class="smw-table-cell smwpropname">Foobar</div><div class="smw-table-cell smwprops">Bar</div></div></div></div></div>',
+				'<div class="smw-summarytable-image"><div class="smw-summarytable-item-center"><div class="smw-summarytable-noimage"></div></div></div></div>'
+			],
+			$instance->buildHTML( ['columns' => 2 ] )
+		);
+	}
+
 }
