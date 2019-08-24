0cd4cd9e88f683388da91a05ee60e2659d17434e
mwjames <jamesin.hongkong.1@gmail.com>
2019-08-24 06:30:15 +0000

    Update PHPUnitResultPrinter.php
    [skip ci]

--- tests/phpunit/PHPUnitResultPrinter.php
+++ tests/phpunit/PHPUnitResultPrinter.php
@@ -21,26 +21,27 @@ class PHPUnitResultPrinter extends PHPUnit_TextUI_ResultPrinter {
 		$i = 0;
 
 		$this->write( "\n\n" );
-		$this->writeWithColor( 'fg-black, bg-yellow', "Slow test report" );
-		$this->write( "\n" );
+		$this->write( '--- Report (slow tests) ' . sprintf( "%'-56s", '' ) );
+		$this->write( "\n\n" );
 
 		$this->write( sprintf( "%-20s%s\n", 'Slow test(s) count:', count( $slowTestsReport['slowTests'] ) ) );
 		$this->write( sprintf( "%-20s%s\n", 'Threshold (in s):', $slowTestsReport['slowThreshold'] ) );
 
-		$this->write( "\nList of slow test(s) ..." );
-
 		foreach ( $slowTestsReport['slowTests'] as $label => $time ) {
 			$length = strlen( $label );
-			$startOff = 25;
+			$startOff = 32;
 			$endOff = 32;
 
-			if ( $length > 60 ) {
+			if ( $length > 65 ) {
 				$label = substr( $label, 0, $startOff ) . ' ... ' . substr( $label, $length - $endOff );
 			}
 
 			$i++;
-			$this->write( sprintf("\n%-73s%ss", "   ... $label", $time ) );
+			$this->write( sprintf("\n%-73s%ss", "- $label", $time ) );
 		}
+
+		$this->write( "\n\n" );
+		$this->write( sprintf( "%'-80s", '' ) );
 	}
 
 }
