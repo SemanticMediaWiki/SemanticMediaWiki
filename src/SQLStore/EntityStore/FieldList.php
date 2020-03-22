68c3fc7ce2f0581e6023f9f8a130c480335b32bf
mwjames <jamesin.hongkong.1@gmail.com>
2020-03-22 13:18:39 +0000

    Avoid using objects to create sha1 fingerprint, refs #4613
    [skip ci]

--- src/SQLStore/EntityStore/FieldList.php
+++ src/SQLStore/EntityStore/FieldList.php
@@ -5,6 +5,7 @@ namespace SMW\SQLStore\EntityStore;
 use SMW\DIProperty;
 use SMW\DIWikiPage;
 use RuntimeException;
+use SMW\Exception\PredefinedPropertyLabelMismatchException;
 
 /**
  * @private
@@ -54,26 +55,7 @@ class FieldList {
 		foreach ( $this->countMaps as $hash => $countMap ) {
 			foreach ( $countMap as $id => $map ) {
 				foreach ( $map as $key => $counts ) {
-
-					if ( $key === '_SKEY' ) {
-						continue;
-					}
-
-					if ( $type === self::CATEGORY_LIST && $key === '_INST' ) {
-						foreach ( $counts as $k => $count ) {
-							if ( !isset( $countList[$k] ) ) {
-								$countList[$k] = 0;
-							}
-
-							$countList[$k]++;
-						}
-					} elseif ( $type === self::PROPERTY_LIST && $key !== '_INST' ) {
-						if ( !isset( $countList[$key] ) ) {
-							$countList[$key] = 0;
-						}
-
-						$countList[$key] += $counts;
-					}
+					$this->matchKeyByCounts( $type, $key, $counts, $countList );
 				}
 			}
 		}
@@ -102,14 +84,50 @@ class FieldList {
 	private function makeList( $map, &$list ) {
 		foreach ( $map as $id => $values ) {
 			foreach ( $values as $key => $counts ) {
-				if ( $key === '_INST' ) {
-					foreach ( $counts as $k => $count ) {
-						$list[$k] = ( new DIWikiPage( $k, NS_CATEGORY ) )->getSha1();
-					}
-				} else {
-					$list[$key] = ( new DIProperty( $key ) )->getSha1();
-				}
+				$this->matchKeyByHash( $key, $counts, $list );
 			}
+		}
+	}
+
+	private function matchKeyByHash( $key, $counts, &$list ) {
+
+		// It is an internal property, so we never reference it as part of a
+		// lookup!
+		if ( $key === '_SKEY' ) {
+			return;
+		}
+
+		if ( $key === '_INST' ) {
+			foreach ( $counts as $k => $count ) {
+				// @see DIWikiPage::getSha1
+				$list[$k] = sha1( json_encode( [ $k, NS_CATEGORY, '', '' ] ) );
+			}
+		} else {
+			// @see DIProperty::getSha1
+			$list[$key] = sha1( json_encode( [ $key, SMW_NS_PROPERTY, '', '' ] ) );
+		}
+	}
+
+	private function matchKeyByCounts( $type, $key, $counts, &$list ) {
+
+		if ( $key === '_SKEY' ) {
+			return;
+		}
+
+		if ( $type === self::CATEGORY_LIST && $key === '_INST' ) {
+			foreach ( $counts as $k => $count ) {
+				if ( !isset( $list[$k] ) ) {
+					$list[$k] = 0;
+				}
+
+				$list[$k]++;
+			}
+		} elseif ( $type === self::PROPERTY_LIST && $key !== '_INST' ) {
+			if ( !isset( $list[$key] ) ) {
+				$list[$key] = 0;
+			}
+
+			$list[$key] += $counts;
 		}
 	}
 
