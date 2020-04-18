2ac1fb0f0dbb0dc285ad770a463e4c963ab1d259
mwjames <jamesin.hongkong.1@gmail.com>
2020-04-18 10:00:00 +0900

    Improve cli output (dumpRDF), refs 4403

---
+++ tests/phpunit/Integration/Maintenance/DumpRDFTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SMW\Tests\Integration\Maintenance;
+
+use SMW\Tests\DatabaseTestCase;
+use SMW\Tests\TestEnvironment;
+use SMW\Tests\PHPUnitCompat;
+
+/**
+ * @group semantic-mediawiki
+ * @group medium
+ *
+ * @license GNU GPL v2+
+ * @since 3.2
+ *
+ * @author mwjames
+ */
+class DumpRDFTest extends DatabaseTestCase {
+
+	use PHPUnitCompat;
+
+	protected $destroyDatabaseTablesAfterRun = true;
+	private $runnerFactory;
+	private $spyMessageReporter;
+
+	protected function setUp() : void {
+		parent::setUp();
+
+		$this->runnerFactory = TestEnvironment::getUtilityFactory()->newRunnerFactory();
+		$this->spyMessageReporter = $this->testEnvironment::getUtilityFactory()->newSpyMessageReporter();
+	}
+
+	protected function tearDown() : void {
+		parent::tearDown();
+	}
+
+	public function testRun() {
+
+		$maintenanceRunner = $this->runnerFactory->newMaintenanceRunner(
+			'SMW\Maintenance\DumpRDF'
+		);
+
+		$maintenanceRunner->setMessageReporter(
+			$this->spyMessageReporter
+		);
+
+		$maintenanceRunner->setQuiet();
+
+		$this->assertTrue(
+			$maintenanceRunner->run()
+		);
+
+		$this->assertContains(
+			'writting OWL/RDF information',
+			$this->spyMessageReporter->getMessagesAsString()
+		);
+	}
+
+}
