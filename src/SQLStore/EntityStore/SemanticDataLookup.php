0036cb8dc34b5e5e1fe856626b581e29fb88c333
Máté Szabó <mszabo@wikia-inc.com>
2021-05-31 22:18:46 +0200

    Use replica DBs for high-volume queries, refs #4989
    Introduce a new method readQuery() on SMW's Database helper class that uses a
    replica DB for executing the query, and use it in
    SemanticDataLookup::fetchFromTable, which is responsible for a large amount of
    queries. This allows for other call sites to be gradually migrated to the new
    method without breaking callers that might rely on query() using the primary DB.

--- src/SQLStore/EntityStore/SemanticDataLookup.php
+++ src/SQLStore/EntityStore/SemanticDataLookup.php
@@ -552,7 +552,7 @@ class SemanticDataLookup {
 			$caller .= " (for " . $requestOptions->getCaller() . ")";
 		}
 
-		$res = $connection->query(
+		$res = $connection->readQuery(
 			$query,
 			$caller
 		);
