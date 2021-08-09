1a36a58cd323731b197cf9979301169aca289c3c
Mark A. Hershberger <mah@nichework.com>
2021-08-09 15:00:10 -0400

    Move the only StoreTest at the top level  more targeted StoreTest

--- tests/phpunit/includes/storage/StoreTest.php
+++ tests/phpunit/includes/storage/StoreTest.php
@@ -198,4 +198,24 @@ class StoreTest extends DatabaseTestCase {
 		);
 	}
 
+	public function testGetRedirectTarget() {
+
+		$wikipage = new DIWikiPage( 'Foo', NS_MAIN );
+		$expected = new DIWikiPage( 'Bar', NS_MAIN );
+
+		$instance = $this->getMockBuilder( '\SMW\Store' )
+			->disableOriginalConstructor()
+			->setMethods( [ 'getPropertyValues' ] )
+			->getMockForAbstractClass();
+
+		$instance->expects( $this->once() )
+			->method( 'getPropertyValues' )
+			->will( $this->returnValue( [ $expected ] ) );
+
+		$this->assertEquals(
+			$expected,
+			$instance->getRedirectTarget( $wikipage )
+		);
+	}
+
 }
