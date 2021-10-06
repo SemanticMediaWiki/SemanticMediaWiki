851afa809858c0bb5546f16d9bbebd9824a071a4
Abijeet <abijeetpatro@gmail.com>
2021-10-06 20:34:34 +0530

    Localizer: Remove usage of wgUser
    Use IContextSource::getUser() instead

--- src/Localizer/Localizer.php
+++ src/Localizer/Localizer.php
@@ -3,7 +3,9 @@
 namespace SMW\Localizer;
 
 use DateTime;
+use IContextSource;
 use Language;
+use RequestContext;
 use SMW\Localizer\LocalLanguage\LocalLanguage;
 use SMW\DIWikiPage;
 use SMW\Site;
@@ -32,15 +34,23 @@ class Localizer {
 	 */
 	private $contentLanguage = null;
 
+	/** @var IContextSource */
+	private $context = null;
+
 	/**
 	 * @since 2.1
 	 *
 	 * @param Language $contentLanguage
 	 * @param NamespaceInfo $namespaceInfo
 	 */
-	public function __construct( Language $contentLanguage, NamespaceInfo $namespaceInfo ) {
+	public function __construct(
+		Language $contentLanguage,
+		NamespaceInfo $namespaceInfo,
+		IContextSource $context
+	) {
 		$this->contentLanguage = $contentLanguage;
 		$this->namespaceInfo = $namespaceInfo;
+		$this->context = $context;
 	}
 
 	/**
@@ -58,7 +68,8 @@ class Localizer {
 
 		self::$instance = new self(
 			$servicesFactory->singleton( 'ContentLanguage' ),
-			$servicesFactory->singleton( 'NamespaceInfo' )
+			$servicesFactory->singleton( 'NamespaceInfo' ),
+			RequestContext::getMain()
 		);
 
 		return self::$instance;
@@ -99,7 +110,7 @@ class Localizer {
 	public function hasLocalTimeOffsetPreference( $user = null ) {
 
 		if ( !$user instanceof User ) {
-			$user = $GLOBALS['wgUser'];
+			$user = $this->context->getUser();
 		}
 
 		return $user->getOption( 'smw-prefs-general-options-time-correction' );
@@ -116,7 +127,7 @@ class Localizer {
 	public function getLocalTime( DateTime $dateTime, $user = null ) {
 
 		if ( !$user instanceof User ) {
-			$user = $GLOBALS['wgUser'];
+			$user = $this->context->getUser();
 		}
 
 		LocalTime::setLocalTimeOffset(
