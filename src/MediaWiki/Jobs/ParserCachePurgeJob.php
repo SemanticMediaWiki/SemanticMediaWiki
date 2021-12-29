646c06e788b9efa15543f6b8274e16ec51c1c161
Ostrzyciel <56843159+Ostrzyciel@users.noreply.github.com>
2021-12-29 11:23:01 +0100

    Fix missing parenthesis on a method call

--- src/MediaWiki/Jobs/ParserCachePurgeJob.php
+++ src/MediaWiki/Jobs/ParserCachePurgeJob.php
@@ -46,7 +46,7 @@ class ParserCachePurgeJob extends Job {
 		if ( $this->hasParameter( 'user' ) ) {
 			$causeAgent = $this->getParameter( 'user' );
 		} else {
-			$causeAgent = RequestContext::getMain()->getUser()->getName;
+			$causeAgent = RequestContext::getMain()->getUser()->getName();
 		}
 
 		if ( $page === null ) {
