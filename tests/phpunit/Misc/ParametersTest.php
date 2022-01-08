cddd30824080b9856876956cb374d2900f15592d
Mark A. Hershberger <mah@nichework.com>
2022-01-08 17:43:30 -0500

    Move top-level tests to “Misc” dir
    Also attempt to isolate “slow” tests from the default run.

---
+++ tests/phpunit/Misc/ParametersTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SMW\Tests\Misc;
+
+use SMW\Parameters;
+use SMW\Tests\PHPUnitCompat;
+
+/**
+ * @covers \SMW\Parameters
+ * @group semantic-mediawiki
+ *
+ * @license GNU GPL v2+
+ * @since   3.0
+ *
+ * @author mwjames
+ */
+class ParametersTest extends \PHPUnit_Framework_TestCase {
+
+	use PHPUnitCompat;
+
+	public function testCanConstruct() {
+
+		$this->assertInstanceOf(
+			Parameters::class,
+			new Parameters()
+		);
+	}
+
+	public function testAddOption() {
+
+		$instance = new Parameters();
+
+		$this->assertFalse(
+			$instance->has( 'Foo' )
+		);
+
+		$instance->set( 'Foo', 42 );
+
+		$this->assertEquals(
+			42,
+			$instance->get( 'Foo' )
+		);
+	}
+
+	public function testUnregisteredKeyThrowsException() {
+
+		$instance = new Parameters();
+
+		$this->expectException( 'InvalidArgumentException' );
+		$instance->get( 'Foo' );
+	}
+
+}
