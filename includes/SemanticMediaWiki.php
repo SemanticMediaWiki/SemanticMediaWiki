47f606902b6d5f60bc016a95d38f5e2750045cf0
Morne Alberts <morne@malberts.net>
2022-12-14 22:50:55 +0200

    Update comments

--- includes/SemanticMediaWiki.php
+++ includes/SemanticMediaWiki.php
@@ -498,9 +498,9 @@ class SemanticMediaWiki {
 		define( 'SMW_ADM_PSTATS', 16 );
 		// Fulltext update
 		define( 'SMW_ADM_FULLT', 32 );
-		// Fulltext update
+		// Maintenance script docs
 		define( 'SMW_ADM_MAINTENANCE_SCRIPT_DOCS', 64 );
-		// Fulltext update
+		// Show Overview tab
 		define( 'SMW_ADM_SHOW_OVERVIEW', 128 );
 		// Table optimization alert
 		define( 'SMW_ADM_ALERT_LAST_OPTIMIZATION_RUN', 2048 );
