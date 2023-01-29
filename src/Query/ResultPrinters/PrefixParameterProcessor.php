0c8824ca6270a978faf19b61966ccc4971b25a3d
thomas-topway-it <topway.it@mail.com>
2023-01-29 13:49:37 +0400

    add type

--- src/Query/ResultPrinters/PrefixParameterProcessor.php
+++ src/Query/ResultPrinters/PrefixParameterProcessor.php
@@ -2,6 +2,7 @@
 
 namespace SMW\Query\ResultPrinters;
 
+use SMWQuery as Query;
 use SMW\Query\Language\NamespaceDescription;
 
 trait PrefixParameterProcessor {
@@ -9,22 +10,22 @@ trait PrefixParameterProcessor {
 	private bool $mixedResults;
 	private string $prefix;
 
-	public function setPrefix( $options ) {
-		$this->prefix = $options;
+	public function setPrefix( string $prefix ) {
+		$this->prefix = $prefix;
 	}
 
-	public function setQuery( $query ) {
+	public function setQuery( Query $query ) {
 		$this->mixedResults = $this->determineMixedResults( $query );
 	}
 
-	private function determineMixedResults( $query ) : bool {
+	private function determineMixedResults( Query $query ) : bool {
 		// this is a basic implementation, possibly to be expanded,
 		// to guess whether result entries are expected to have
 		// homogeneous or mixed prefixes
 		return !( $query->getDescription() instanceof NamespaceDescription );
 	}
 
-	public function useLongText( $isSubject ) : bool {
+	public function useLongText( bool $isSubject ) : bool {
 		$prefix = $this->prefix;
 
 		if ( $prefix === 'all'
