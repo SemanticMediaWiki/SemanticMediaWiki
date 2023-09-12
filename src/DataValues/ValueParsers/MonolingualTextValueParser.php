c7161f965135b1e0b4d5275847d2add13f852c0e
thomas-topway-it <topway.it@mail.com>
2023-09-13 01:25:12 +0400

    fix $mappedLanguageCode

--- src/DataValues/ValueParsers/MonolingualTextValueParser.php
+++ src/DataValues/ValueParsers/MonolingualTextValueParser.php
@@ -11,6 +11,7 @@ use SMW\Localizer;
  * @since 2.4
  *
  * @author mwjames
+ * @reviewer thomas-topway-it
  */
 class MonolingualTextValueParser implements ValueParser {
 
@@ -53,7 +54,13 @@ class MonolingualTextValueParser implements ValueParser {
 			}
 		}
 
-		return [ $text, Localizer::asBCP47FormattedLanguageCode( $languageCode ) ];
+		$languageCode = Localizer::asBCP47FormattedLanguageCode( $languageCode );
+		$nonstandardLanguageCodeMapping = \LanguageCode::getNonstandardLanguageCodeMapping();
+
+		$mappedLanguageCode = !in_array( $languageCode, $nonstandardLanguageCodeMapping ) ? $languageCode
+			: array_search( $languageCode, $nonstandardLanguageCodeMapping );
+
+		return [ $text, $mappedLanguageCode ];
 	}
 
 }
