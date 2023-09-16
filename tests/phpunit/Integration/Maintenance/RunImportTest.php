352f45b14cdf6777d4a4794326c5ac71daf68c71
thomas-topway-it <topway.it@mail.com>
2023-09-16 04:21:40 +0400

    temp revert

--- tests/phpunit/Integration/Maintenance/RunImportTest.php
+++ tests/phpunit/Integration/Maintenance/RunImportTest.php
@@ -29,11 +29,6 @@ class RunImportTest extends DatabaseTestCase {
 		$this->testEnvironment->addConfiguration( 'smwgImportReqVersion', 1 );
 		$this->testEnvironment->addConfiguration( 'smwgEnabledFulltextSearch', false );
 
-		$revisionGuard = $this->getMockBuilder( '\SMW\MediaWiki\RevisionGuard' )
-			->disableOriginalConstructor()
-			->getMock();
-
-		$this->testEnvironment->registerObject( 'RevisionGuard', $revisionGuard );
 
 		$this->runnerFactory  = $this->testEnvironment::getUtilityFactory()->newRunnerFactory();
 		$this->spyMessageReporter = $this->testEnvironment::getUtilityFactory()->newSpyMessageReporter();
