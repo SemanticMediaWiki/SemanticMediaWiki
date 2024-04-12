8894f81f3bd909ac831c05a1eea4e0c89ec4330f
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2024-04-12 13:30:40 +0900

    Remake test for SQLStore\EntityStore\PrefetchCache multiple time caching
    Based on code review.

--- tests/phpunit/SQLStore/EntityStore/PrefetchCacheTest.php
+++ tests/phpunit/SQLStore/EntityStore/PrefetchCacheTest.php
@@ -83,59 +83,43 @@ class PrefetchCacheTest extends \PHPUnit\Framework\TestCase {
 		);
 	}
 
-	public function testMultiCacheAndFetch() {
-		# two triples of (A, Pm, B), (X, Pm, Y)
-		$property = new DIProperty( 'Pm' );
-		$A = DIWikiPage::newFromText("A");
-		$B = DIWikiPage::newFromText("B");
-		$X = DIWikiPage::newFromText("X");
-		$Y = DIWikiPage::newFromText("Y");
-
-		$idTable = $this->getMockBuilder( '\SMW\SQLStore\EntityStore\EntityIdManager' )
+	public function testCacheMergeWithDifferentFingerprints() {
+		$property = new DIProperty('Pm');
+		$subject1 = DIWikiPage::newFromText("Subject1");
+		$subject2 = DIWikiPage::newFromText("Subject2");
+	
+		$idTable = $this->getMockBuilder('\SMW\SQLStore\EntityStore\EntityIdManager')
 			->disableOriginalConstructor()
 			->getMock();
-
-		$idTable->expects( $this->atLeastOnce() )
-			->method( 'getSMWPageID' )
-			->will( $this->returnCallback(
-				function($title, $namespace, $iw, $subobjectName, $canonical = true, $fetchHashes = false){
-					switch($title){
-						case "A": return 53;
-						case "X": return 54;
-					}
-				}
-			));
-
-		$this->store->expects( $this->atLeastOnce() )
-			->method( 'getObjectIds' )
-			->will( $this->returnValue( $idTable ) );
-
-		$this->prefetchItemLookup->expects( $this->atLeastOnce() )
-			->method( 'getPropertyValues' )
-			->will( $this->returnCallback(
-				function($subjects, $p, $opts) use ($A,$B,$X,$Y) {
-					switch($subjects){
-						case [$A]: return [ 53 => [$B]];
-						case [$X]: return [ 54 => [$Y]];
-					}
-				}
-			));
-
-		$instance = new PrefetchCache(
-			$this->store,
-			$this->prefetchItemLookup
-		);
-
-		$instance->prefetch( [ $A ], $property, $this->requestOptions );
-		$instance->prefetch( [ $X ], $property, $this->requestOptions );
+	
+		$idTable->method('getSMWPageID')
+			->willReturnOnConsecutiveCalls(101, 102);
+	
+		$this->store->method('getObjectIds')
+			->willReturn($idTable);
+	
+		$this->prefetchItemLookup->method('getPropertyValues')
+			->willReturnOnConsecutiveCalls(
+				[101 => [DIWikiPage::newFromText('Result1')]],
+				[102 => [DIWikiPage::newFromText('Result2')]]
+			);
+	
+		$instance = new PrefetchCache($this->store, $this->prefetchItemLookup);
+		
+		// First prefetch
+		$instance->prefetch([$subject1], $property, $this->requestOptions);
+	
+		// Second prefetch with a different fingerprint, that is generated in PrefetchCache
+		$instance->prefetch([$subject2], $property, $this->requestOptions);
+	
+		// Verify that both results are present in the cache
 		$this->assertEquals(
-			[ $B ],
-			$instance->getPropertyValues( $A, $property, $this->requestOptions )
+			[DIWikiPage::newFromText('Result1')],
+			$instance->getPropertyValues($subject1, $property, $this->requestOptions)
 		);
 		$this->assertEquals(
-			[ $Y ],
-			$instance->getPropertyValues( $X, $property, $this->requestOptions )
+			[DIWikiPage::newFromText('Result2')],
+			$instance->getPropertyValues($subject2, $property, $this->requestOptions)
 		);
 	}
-
 }
