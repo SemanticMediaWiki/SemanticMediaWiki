1a2c305a2ad5e3790de2e6b51e3ec9853e6e9ed2
Marko Ilic <gesinn-it-ilm@gesinn.it>
2024-07-15 17:57:38 +0200

    Docker compose ci (#5665)
    * init docker-compose-ci
    
    * chg (ci): updated Makefile
    
    - remove matrixes for MW 1.36, 1.37 and 1.39
    - add matrix for 1.40
    - update steps
    
    * chg (ci): remove unneccesary line
    
    * chg (ci): updated workflow
    
    * chg (ci): updated workflow
    
    - check with basic set up
    
    * fix CI syntax
    
    * set coverage to false
    
    * chg (ci): updated env
    
    * chg (ci): added db images
    
    * chg (ci): updated db images:
    
    - add mariadb:latest instead of mysql:8
    
    * chg (ci): added mysql db to MW 1.41 matrix
    
    * chg (ci): updated db images:
    
    - instead of latest, use mariadb:11.2 db image
    
    ---------
    
    Co-authored-by: gesinn-it-gea <gesinn-it-gea@gesinn.it>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -36,7 +36,8 @@ $autoloader->addClassMap( [
 ] );
 
 define( 'SMW_PHPUNIT_DIR', __DIR__ . '/phpunit' );
-define( 'SMW_PHPUNIT_TABLE_PREFIX', 'sunittest_' );
+#define( 'SMW_PHPUNIT_TABLE_PREFIX', 'sunittest_' );
+define( 'SMW_PHPUNIT_TABLE_PREFIX', '' );
 
 /**
  * Register a shutdown function the invoke a final clean-up
