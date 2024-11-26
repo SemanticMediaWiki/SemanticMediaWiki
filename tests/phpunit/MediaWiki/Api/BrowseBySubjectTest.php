bf61eab7f17dbc690cac5527977e003a700c0bf5
alistair3149 <alistair31494322@gmail.com>
2024-11-25 22:00:59 -0500

    Fix test

--- tests/phpunit/MediaWiki/Api/BrowseBySubjectTest.php
+++ tests/phpunit/MediaWiki/Api/BrowseBySubjectTest.php
@@ -197,7 +197,7 @@ class BrowseBySubjectTest extends \PHPUnit_Framework_TestCase {
 		$out = ob_get_clean();
 
 		$this->stringValidator->assertThatStringContains(
-			'"query":"<div class=\"smw-factbox.*\"><div class=\"smw-table smw-factbox-group\">',
+			'"query":"<div class=\"smw-factbox.*\"><div class=\"smw-factbox-header\">',
 			$out
 		);
 	}
