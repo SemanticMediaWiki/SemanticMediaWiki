1eb405d81e4a4d46a9ae4214404bdc55b9d9df7a
paladox <paladox@users.noreply.github.com>
2024-12-11 20:30:42 +0000

    Update MaintenanceFactory.php

--- src/Maintenance/MaintenanceFactory.php
+++ src/Maintenance/MaintenanceFactory.php
@@ -4,11 +4,12 @@ namespace SMW\Maintenance;
 
 use Onoi\MessageReporter\MessageReporter;
 use Onoi\MessageReporter\MessageReporterFactory;
+use SMW\Store;
 use SMW\Services\ServicesFactory as ApplicationFactory;
 use SMW\MediaWiki\ManualEntryLogger;
 use SMW\SQLStore\PropertyStatisticsStore;
-use SMW\Store;
 use SMW\Localizer\LocalMessageProvider;
+use SMW\Maintenance\RebuildPropertyStatistics;
 
 /**
  * @license GNU GPL v2+
