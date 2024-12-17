efb0b07e7ab602973e9baf7433835819d7d1603c
paladox <paladox@users.noreply.github.com>
2024-12-17 13:59:26 +0000

    Use || instead of 'or'

--- src/SQLStore/TableBuilder/TableBuilder.php
+++ src/SQLStore/TableBuilder/TableBuilder.php
@@ -263,7 +263,7 @@ abstract class TableBuilder implements TableBuilderInterface, MessageReporterAwa
 		foreach ( $haystack as $key => $value ) {
 			$current_key = $key;
 
-			if ( $needle === $value or ( is_array( $value ) && $this->recursive_array_search( $needle, $value ) !== false ) ) {
+			if ( $needle === $value || ( is_array( $value ) && $this->recursive_array_search( $needle, $value ) !== false ) ) {
 				return $current_key;
 			}
 		}
