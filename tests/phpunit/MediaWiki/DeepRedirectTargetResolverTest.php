a9cd62f89656d045c968863c50d154fdd738e1fc
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-12-20 17:38:19 +0100

    DeepRedirectTargetResolverTest
    - update tests, fix at() deprecations

--- tests/phpunit/MediaWiki/DeepRedirectTargetResolverTest.php
+++ tests/phpunit/MediaWiki/DeepRedirectTargetResolverTest.php
@@ -32,77 +32,84 @@ class DeepRedirectTargetResolverTest extends \PHPUnit\Framework\TestCase {
 	}
 
 	public function testResolveRedirectTarget() {
-		$title = MockTitle::buildMock( 'Uuuuuuuuuu' );
-
-		$wikiPage = $this->getMockBuilder( '\WikiPage' )
+		$title = MockTitle::buildMock('Uuuuuuuuuu');
+	
+		// Mock WikiPage
+		$wikiPage = $this->getMockBuilder('\WikiPage')
 			->disableOriginalConstructor()
 			->getMock();
-
-		$wikiPage->expects( $this->atLeastOnce() )
-			->method( 'getRedirectTarget' )
-			->will( $this->returnValue( MockTitle::buildMock( 'Ooooooo' ) ) );
-
-		$pageCreator = $this->getMockBuilder( '\SMW\MediaWiki\PageCreator' )
+	
+		$wikiPage->expects($this->any())
+			->method('getRedirectTarget')
+			->willReturn(MockTitle::buildMock('Ooooooo'));
+	
+		// Mock PageCreator
+		$pageCreator = $this->getMockBuilder('\SMW\MediaWiki\PageCreator')
 			->disableOriginalConstructor()
 			->getMock();
-
-		$pageCreator->expects( $this->any() )
-			->method( 'createPage' )
-			->will( $this->returnValue( $wikiPage ) );
-
-		$instance = $this->getMockBuilder( '\SMW\MediaWiki\DeepRedirectTargetResolver' )
-			->setConstructorArgs( [ $pageCreator ] )
-			->setMethods( [ 'isValidRedirectTarget', 'isRedirect' ] )
+	
+		$pageCreator->expects($this->any())
+			->method('createPage')
+			->willReturn($wikiPage);
+	
+		// Mock DeepRedirectTargetResolver
+		$instance = $this->getMockBuilder('\SMW\MediaWiki\DeepRedirectTargetResolver')
+			->setConstructorArgs([$pageCreator])
+			->setMethods(['isValidRedirectTarget', 'isRedirect'])
 			->getMock();
-
-		$instance->expects( $this->atLeastOnce() )
-			->method( 'isValidRedirectTarget' )
-			->will( $this->returnValue( true ) );
-
-		$instance->expects( $this->atLeastOnce() )
-			->method( 'isRedirect' )
-			->will( $this->returnValue( true ) );
-
-		$this->assertInstanceOf(
-			'\Title',
-			 $instance->findRedirectTargetFor( $title )
-		);
+	
+		$instance->expects($this->any())
+			->method('isValidRedirectTarget')
+			->willReturn(true);
+	
+		// Use willReturnOnConsecutiveCalls for isRedirect method
+		$instance->expects($this->exactly(2))
+			->method('isRedirect')
+			->willReturnOnConsecutiveCalls(true, false);
+	
+		// Perform assertion on the result of findRedirectTargetFor
+		$result = $instance->findRedirectTargetFor($title);
+		
+		$this->assertInstanceOf('\Title', $result);
 	}
-
+	
 	public function testResolveRedirectTargetThrowsException() {
-		$title = MockTitle::buildMock( 'Uuuuuuuuuu' );
-
-		$wikiPage = $this->getMockBuilder( '\WikiPage' )
+		$title = MockTitle::buildMock('Uuuuuuuuuu');
+	
+		$wikiPage = $this->getMockBuilder('\WikiPage')
 			->disableOriginalConstructor()
 			->getMock();
-
-		$wikiPage->expects( $this->never() )
-			->method( 'getRedirectTarget' );
-
-		$pageCreator = $this->getMockBuilder( '\SMW\MediaWiki\PageCreator' )
+	
+		$wikiPage->expects($this->never())
+			->method('getRedirectTarget');
+	
+		$pageCreator = $this->getMockBuilder('\SMW\MediaWiki\PageCreator')
 			->disableOriginalConstructor()
 			->getMock();
-
-		$pageCreator->expects( $this->any() )
-			->method( 'createPage' )
-			->will( $this->returnValue( $wikiPage ) );
-
-		$instance = $this->getMockBuilder( '\SMW\MediaWiki\DeepRedirectTargetResolver' )
-			->setConstructorArgs( [ $pageCreator ] )
-			->setMethods( [ 'isValidRedirectTarget', 'isRedirect' ] )
+	
+		$pageCreator->expects($this->any())
+			->method('createPage')
+			->will($this->returnValue($wikiPage));
+	
+		$instance = $this->getMockBuilder('\SMW\MediaWiki\DeepRedirectTargetResolver')
+			->setConstructorArgs([$pageCreator])
+			->setMethods(['isValidRedirectTarget', 'isRedirect'])
 			->getMock();
-
-		$instance->expects( $this->atLeastOnce() )
-			->method( 'isValidRedirectTarget' )
-			->will( $this->returnValue( false ) );
-
-		$instance->expects( $this->atLeastOnce() )
-			->method( 'isRedirect' )
-			->will( $this->returnValue( false ) );
-
-		$this->expectException( 'RuntimeException' );
-		$instance->findRedirectTargetFor( $title );
+	
+		$instance->expects($this->atLeastOnce())
+			->method('isValidRedirectTarget')
+			->will($this->returnValue(false));
+	
+		// Use withConsecutive() to mock method calls in sequence
+		$instance->expects($this->exactly(1))
+			->method('isRedirect')
+			->withConsecutive([])
+			->will($this->returnValue(false));
+	
+		$this->expectException('RuntimeException');
+		$instance->findRedirectTargetFor($title);
 	}
+	
 
 	public function testTryToResolveCircularRedirectThrowsException() {
 		$title = MockTitle::buildMock( 'Uuuuuuuuuu' );
