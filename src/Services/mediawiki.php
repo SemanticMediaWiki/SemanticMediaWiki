d7a3433f47bcc4792944e05276e1c3ad97d78c9b
paladox <paladox@users.noreply.github.com>
2025-01-01 15:03:21 +0000

    mediawiki: Fix support for MW 1.42 in 'WikiImporter'
    The constructor was changed with https://github.com/wikimedia/mediawiki/commit/64001f0ecd6ffdd8a639e4aed0289afc6bd1e539#diff-81ab53e2d79bfc77e91773c25ac399efeaa16486044dea310100d719b14b2cf9R26

--- src/Services/mediawiki.php
+++ src/Services/mediawiki.php
@@ -56,8 +56,28 @@ return [
 	'WikiImporter' => function ( $containerBuilder, \ImportSource $importSource ) {
 		$containerBuilder->registerExpectedReturnType( 'WikiImporter', '\WikiImporter' );
 		$services = MediaWikiServices::getInstance();
+
+		// MW 1.41 or lower
+		if ( version_compare( MW_VERSION, '1.42', '<' ) ) {
+			return new WikiImporter(
+				$importSource,
+				$containerBuilder->create( 'MainConfig' ),
+				$services->getHookContainer(),
+				$services->getContentLanguage(),
+				$services->getNamespaceInfo(),
+				$services->getTitleFactory(),
+				$services->getWikiPageFactory(),
+				$services->getWikiRevisionUploadImporter(),
+				$services->getPermissionManager(),
+				$services->getContentHandlerFactory(),
+				$services->getSlotRoleRegistry()
+			);
+		}
+
+		// MW 1.42+
 		return new WikiImporter(
 			$importSource,
+			RequestContext::getMain()->getAuthority(),
 			$containerBuilder->create( 'MainConfig' ),
 			$services->getHookContainer(),
 			$services->getContentLanguage(),
@@ -65,7 +85,6 @@ return [
 			$services->getTitleFactory(),
 			$services->getWikiPageFactory(),
 			$services->getWikiRevisionUploadImporter(),
-			$services->getPermissionManager(),
 			$services->getContentHandlerFactory(),
 			$services->getSlotRoleRegistry()
 		);
