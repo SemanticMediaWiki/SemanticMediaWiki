a31242f03fcfc6110d8eb48b29b55a6ba73d5653
paladox <paladox@users.noreply.github.com>
2025-01-03 11:46:45 +0000

    Update populateHashField.php

--- maintenance/populateHashField.php
+++ maintenance/populateHashField.php
@@ -15,11 +15,13 @@ use SMW\Utils\CliMsgFormatter;
 /**
  * Load the required class
  */
+// @codeCoverageIgnoreStart
 if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
 	require_once getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php';
 } else {
 	require_once __DIR__ . '/../../../maintenance/Maintenance.php';
 }
+// @codeCoverageIgnoreEnd
 
 /**
  * @license GNU GPL v2+
@@ -285,5 +287,7 @@ class populateHashField extends \Maintenance {
 
 }
 
+// @codeCoverageIgnoreStart
 $maintClass = populateHashField::class;
 require_once( RUN_MAINTENANCE_IF_MAIN );
+// @codeCoverageIgnoreEnd
