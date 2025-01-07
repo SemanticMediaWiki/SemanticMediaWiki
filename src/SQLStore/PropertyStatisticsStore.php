c185feae6960c0601a86b9412027f86b347b2551
paladox <paladox@users.noreply.github.com>
2025-01-07 11:25:59 +0000

    Update PropertyStatisticsStore.php

--- src/SQLStore/PropertyStatisticsStore.php
+++ src/SQLStore/PropertyStatisticsStore.php
@@ -111,6 +111,7 @@ class PropertyStatisticsStore {
 				__METHOD__
 			);
 		} catch ( DBQueryError | \Wikimedia\Rdbms\DBTransactionStateError $e ) {
+			$this->connection->rollback( __METHOD__ );
 			// #2345 Do nothing as it most likely an "Error: 1264 Out of range
 			// value for column" in strict mode
 			// As an unsigned int, we expected it to be 0
