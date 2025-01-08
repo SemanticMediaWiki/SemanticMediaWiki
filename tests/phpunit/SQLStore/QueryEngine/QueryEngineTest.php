e5815d92ad39f8ba05de13ca66b40246b2dec554
paladox <paladox@users.noreply.github.com>
2025-01-08 22:05:38 +0000

    Update QueryEngineTest.php

--- tests/phpunit/SQLStore/QueryEngine/QueryEngineTest.php
+++ tests/phpunit/SQLStore/QueryEngine/QueryEngineTest.php
@@ -5,6 +5,7 @@ namespace SMW\Tests\SQLStore\QueryEngine;
 use SMW\SQLStore\QueryEngine\QueryEngine;
 use SMW\Tests\PHPUnitCompat;
 use SMWQuery as Query;
+use Wikimedia\Rdbms\SelectQueryBuilder;
 
 /**
  * @covers \SMW\SQLStore\QueryEngine\QueryEngine
@@ -54,6 +55,61 @@ class QueryEngineTest extends \PHPUnit\Framework\TestCase {
 			->disableOriginalConstructor()
 			->getMock();
 
+		$queryBuilder = $this->getMockBuilder( SelectQueryBuilder::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'select' )
+			->willReturn( $queryBuilder );
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'from' )
+			->willReturn( $queryBuilder );
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'where' )
+			->willReturn( $queryBuilder );
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'distinct' )
+			->willReturn( $queryBuilder );
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'caller' )
+			->willReturn( $queryBuilder );
+
+		// Mock getSQL() for debug mode
+		$queryBuilder->expects( $this->any() )
+			->method( 'getSQL' )
+			->willReturn( 'SELECT * FROM test' );
+
+		$connection->expects( $this->any() )
+			->method( 'newSelectQueryBuilder' )
+			->willReturn( $queryBuilder );
+
+		$connection->expects( $this->any() )
+			->method( 'applySqlOptions' )
+			->willReturn( $queryBuilder );
+
+		// Mock isType() for EXPLAIN query formatting
+		$connection->expects( $this->any() )
+			->method( 'isType' )
+			->willReturn( false );
+
+		// Mock query() for EXPLAIN
+		$resultWrapper = $this->getMockBuilder( '\Wikimedia\Rdbms\IResultWrapper' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$resultWrapper->expects( $this->any() )
+			->method( 'free' )
+			->willReturn( true );
+
+		$connection->expects( $this->any() )
+			->method( 'query' )
+			->willReturn( $resultWrapper );
+
 		$this->store->expects( $this->any() )
 			->method( 'getConnection' )
 			->willReturn( $connection );
@@ -62,12 +118,10 @@ class QueryEngineTest extends \PHPUnit\Framework\TestCase {
 			->method( 'getErrors' )
 			->willReturn( [] );
 
-		// Set up conditionBuilder to return a valid rootid
 		$this->conditionBuilder->expects( $this->once() )
 			->method( 'buildCondition' )
 			->willReturn( 1 );
 
-		// Set up querySegmentList
 		$querySegment = $this->getMockBuilder( '\SMW\SQLStore\QueryEngine\QuerySegment' )
 			->disableOriginalConstructor()
 			->getMock();
