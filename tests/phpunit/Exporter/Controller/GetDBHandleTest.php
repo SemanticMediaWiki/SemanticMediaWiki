09b252709402cb7bb66651c42a1b35ce172d9bda
Greg Rundlett <greg.rundlett@gmail.com>
2025-01-12 01:08:37 +0000

    fix phpcs issues

--- tests/phpunit/Exporter/Controller/GetDBHandleTest.php
+++ tests/phpunit/Exporter/Controller/GetDBHandleTest.php
@@ -5,44 +5,43 @@ namespace SMW\Tests\Exporter\Controller;
 use MediaWiki\MediaWikiServices;
 use PHPUnit\Framework\TestCase;
 
-
 /**
- * @covers \SMW\Exporter\SMWExportController
- * @group semantic-mediawiki
- *
  * @license GPL-2.0-or-later
  * @since 5.0-alpha
  *
  * @author freephile
  */
 
-class SMW_ExportControllerTest extends TestCase {
+class GetDBHandleTest extends TestCase {
 
+	/**
+	 * @covers \SMW\Exporter\SMWExportController::getDBHandle
+	 */
 	public function testGetDBHandle() {
 		$mwVersion = MW_VERSION;
 
-		if (version_compare($mwVersion, '1.42', '>=')) {
-			$connectionProvider = $this->createMock(\Wikimedia\Rdbms\ConnectionProvider::class);
-			$replicaDatabase = $this->createMock(\Wikimedia\Rdbms\IDatabase::class);
-			$connectionProvider->method('getReplicaDatabase')->willReturn($replicaDatabase);
+		if ( version_compare( $mwVersion, '1.42', '>=' ) ) {
+			$connectionProvider = $this->createMock( \Wikimedia\Rdbms\ConnectionProvider::class );
+			$replicaDatabase = $this->createMock( \Wikimedia\Rdbms\IDatabase::class );
+			$connectionProvider->method( 'getReplicaDatabase' )->willReturn( $replicaDatabase );
 
-			$mediaWikiServices = $this->createMock(MediaWikiServices::class);
-			$mediaWikiServices->method('getConnectionProvider')->willReturn($connectionProvider);
+			$mediaWikiServices = $this->createMock( MediaWikiServices::class );
+			$mediaWikiServices->method( 'getConnectionProvider' )->willReturn( $connectionProvider );
 
-			MediaWikiServices::setInstanceForTesting($mediaWikiServices);
+			MediaWikiServices::setInstanceForTesting( $mediaWikiServices );
 
-			$this->assertSame($replicaDatabase, SMWExportController::getDBHandle());
+			$this->assertSame( $replicaDatabase, SMWExportController::getDBHandle() );
 		} else {
-			$dbLoadBalancer = $this->createMock(\Wikimedia\Rdbms\LoadBalancer::class);
-			$replicaDatabase = $this->createMock(\Wikimedia\Rdbms\IDatabase::class);
-			$dbLoadBalancer->method('getConnection')->with(DB_REPLICA)->willReturn($replicaDatabase);
+			$dbLoadBalancer = $this->createMock( \Wikimedia\Rdbms\LoadBalancer::class );
+			$replicaDatabase = $this->createMock( \Wikimedia\Rdbms\IDatabase::class );
+			$dbLoadBalancer->method( 'getConnection' )->with( DB_REPLICA )->willReturn( $replicaDatabase );
 
-			$mediaWikiServices = $this->createMock(MediaWikiServices::class);
-			$mediaWikiServices->method('getDBLoadBalancer')->willReturn($dbLoadBalancer);
+			$mediaWikiServices = $this->createMock( MediaWikiServices::class );
+			$mediaWikiServices->method( 'getDBLoadBalancer' )->willReturn( $dbLoadBalancer );
 
-			MediaWikiServices::setInstanceForTesting($mediaWikiServices);
+			MediaWikiServices::setInstanceForTesting( $mediaWikiServices );
 
-			$this->assertSame($replicaDatabase, SMWExportController::getDBHandle());
+			$this->assertSame( $replicaDatabase, SMWExportController::getDBHandle() );
 		}
 	}
 }
