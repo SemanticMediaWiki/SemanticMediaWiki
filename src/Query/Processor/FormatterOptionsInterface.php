decec8d8ea1daa2ffa89f92a0bb7612bfa3d883e
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2025-01-14 12:25:13 +0100

    FormatterOptionsInterface [skip ci]
    - remove unneccesary function
    - update comment

--- src/Query/Processor/FormatterOptionsInterface.php
+++ src/Query/Processor/FormatterOptionsInterface.php
@@ -4,17 +4,15 @@ namespace SMW\Query\Processor;
 
 /**
  * The interface is used as custom options formatter used in ask queries
- * Custom created formatter like SizeFormatterOption or LinkFormatterOption implements
- * this interface and use its functions for formating options
+ * Custom created formatter like SizeFormatter, LinkFormatter and TableHeaderFormatter
+ * implements this interface and use its functions for formating options
  *
  * @license GNU GPL v2+
- * @since 3.0
+ * @since 5.0.0
  *
  * @author milic
  */
 interface FormatterOptionsInterface {
-
-	public function addPrintRequest( $name, $param, $previousPrintout, $serialization );
 
 	public function addPrintRequestHandleParams( $name, $param, $previousPrintout, $serialization );
 }
