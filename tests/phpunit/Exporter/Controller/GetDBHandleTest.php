4cf206c140a9ee113b6170f6fbf03cdfff833401
Greg Rundlett <greg.rundlett@gmail.com>
2025-01-15 22:30:51 -0500

    Draft version of possible integration test case
    Not sure if this is even needed, but if an integration test is
    desirable for SMWExportController::getDBHandle() then it might
    look something like this
    for Issue #5958 and
    Pull #5988

--- tests/phpunit/Exporter/Controller/GetDBHandleTest.php
+++ tests/phpunit/Exporter/Controller/GetDBHandleTest.php
@@ -1,69 +1,46 @@
 <?php
 
-namespace SMW\Tests\Exporter\Controller;
+namespace SMW\Tests\Integration;
 
 use MediaWiki\MediaWikiServices;
+use SMW\Exporter\SMWExportController;
+use Wikimedia\Rdbms\IDatabase;
 use PHPUnit\Framework\TestCase;
 
 /**
+ * @group integration
  * @group semantic-mediawiki
  *
  * @license GPL-2.0-or-later
  * @since 5.0-alpha
- *
- * @author freephile
  */
-class GetDBHandleTest extends TestCase {
+class SMWExportControllerIntegrationTest extends TestCase {
 
-	/**
-	 * @covers \SMW\Exporter\SMWExportController::getDBHandle
-	 */
-	public function testGetDBHandle() {
-		$mwVersion = MW_VERSION;
+    /**
+     * @covers \SMW\Exporter\SMWExportController::getDBHandle
+     */
+    public function testGetDBHandle() {
+        $dbHandle = SMWExportController::getDBHandle();
+        $this->assertInstanceOf(IDatabase::class, $dbHandle);
+    }
 
-		if ( version_compare( $mwVersion, '1.42', '>=' ) ) {
-			$connectionProvider = $this->createMock( \Wikimedia\Rdbms\ConnectionProvider::class );
-			$replicaDatabase = $this->createMock( \Wikimedia\Rdbms\IDatabase::class );
-			$connectionProvider->method( 'getReplicaDatabase' )->willReturn( $replicaDatabase );
+    /**
+     * @covers \SMW\Exporter\SMWExportController::getDBHandle
+     */
+    public function testGetDBHandleWithDifferentVersions() {
+        $originalVersion = MW_VERSION;
 
-			$mediaWikiServices = $this->createMock( MediaWikiServices::class );
-			$mediaWikiServices->method( 'getConnectionProvider' )->willReturn( $connectionProvider );
+        // Test with version >= 1.42
+        define('MW_VERSION', '1.42');
+        $dbHandle = SMWExportController::getDBHandle();
+        $this->assertInstanceOf(IDatabase::class, $dbHandle);
 
-			MediaWikiServices::getInstance()->setService( 'ConnectionProvider', $connectionProvider );
+        // Test with version < 1.42
+        define('MW_VERSION', '1.41');
+        $dbHandle = SMWExportController::getDBHandle();
+        $this->assertInstanceOf(IDatabase::class, $dbHandle);
 
-			$this->assertSame( $replicaDatabase, SMWExportController::getDBHandle() );
-
-			// Test error condition
-			$connectionProvider = $this->createMock( \Wikimedia\Rdbms\ConnectionProvider::class );
-			$connectionProvider->method( 'getReplicaDatabase' )
-				->willThrowException( new \RuntimeException( 'Connection failed' ) );
-
-			$mediaWikiServices = $this->createMock( MediaWikiServices::class );
-			$mediaWikiServices->method( 'getConnectionProvider' )
-				->willReturn( $connectionProvider );
-
-			MediaWikiServices::getInstance()->setService( 'ConnectionProvider', $connectionProvider );
-
-			$this->expectException( \RuntimeException::class );
-			$this->expectExceptionMessage( 'Failed to obtain database handle: Connection failed' );
-			SMWExportController::getDBHandle();
-		} else {
-			$dbLoadBalancer = $this->createMock( \Wikimedia\Rdbms\LoadBalancer::class );
-			$replicaDatabase = $this->createMock( \Wikimedia\Rdbms\IDatabase::class );
-			$dbLoadBalancer->expects( $this->once() )
-				->method( 'getConnection' )
-				->with( DB_REPLICA )
-				->willReturn( $replicaDatabase );
-
-			$mediaWikiServices = $this->createMock( MediaWikiServices::class );
-			$mediaWikiServices->method( 'getDBLoadBalancer' )->willReturn( $dbLoadBalancer );
-
-			MediaWikiServices::getInstance()->setService( 'DBLoadBalancer', $dbLoadBalancer );
-
-			$this->assertSame( $replicaDatabase, SMWExportController::getDBHandle() );
-		}
-
-		// Cleanup
-		MediaWikiServices::getInstance()->resetServices();
-	}
+        // Restore original version
+        define('MW_VERSION', $originalVersion);
+    }
 }
