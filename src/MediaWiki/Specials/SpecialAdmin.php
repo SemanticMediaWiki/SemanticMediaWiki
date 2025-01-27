660e6bc34c4ce5ccb278fc4791999441df57a5d0
alistair3149 <alistair31494322@gmail.com>
2025-01-27 13:43:32 -0500

    Load mediawiki.codex.messagebox.styles module for message boxes
    Task: T375287
    Fixes: #6013

--- src/MediaWiki/Specials/SpecialAdmin.php
+++ src/MediaWiki/Specials/SpecialAdmin.php
@@ -62,8 +62,11 @@ class SpecialAdmin extends SpecialPage {
 		$output->setPageTitle( $this->msg_text( 'smw-title' ) );
 		$output->addHelpLink( $this->msg_text( 'smw-admin-helplink' ), true );
 
-		$output->addModuleStyles( 'ext.smw.styles' );
-		$output->addModuleStyles( 'ext.smw.special.styles' );
+		$output->addModuleStyles( [
+			'ext.smw.styles',
+			'ext.smw.special.styles',
+			'mediawiki.codex.messagebox.styles'	
+		] );
 		$output->addModules( 'ext.smw.admin' );
 
 		$applicationFactory = ApplicationFactory::getInstance();
