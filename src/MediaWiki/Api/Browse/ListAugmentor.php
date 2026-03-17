67dba3ac5ce15bcfa27c9058d588dc4894382ea4
vm-pranavan <techiepeeyon@gmail.com>
2026-03-17 18:38:24 +0530

    Hoist smwfGetLinker() retrieval outside of loop

--- src/MediaWiki/Api/Browse/ListAugmentor.php
+++ src/MediaWiki/Api/Browse/ListAugmentor.php
@@ -115,13 +115,14 @@ class ListAugmentor {
 	private function addPropertyDescription( &$res, array $languageCodes ) {
 		$list = $res['query'];
 		$propertySpecificationLookup = ApplicationFactory::getInstance()->getPropertySpecificationLookup();
+		$linker = \smwfGetLinker();
 
 		foreach ( $list as $key => $value ) {
 			$property = new DIProperty( $key );
 			$description = [];
 
 			foreach ( $languageCodes as $code ) {
-				$description[$code] = $propertySpecificationLookup->getPropertyDescriptionByLanguageCode( $property, $code, \smwfGetLinker() );
+				$description[$code] = $propertySpecificationLookup->getPropertyDescriptionByLanguageCode( $property, $code, $linker );
 			}
 
 			$list[$key] = $value + [
