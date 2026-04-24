e3b2e407ff8e9b585fe666c92655fa1cc44618ac
paladox <paladox@users.noreply.github.com>
2026-04-24 15:50:44 +0100

    Fix DisplayTitleFinder.php phan errors

--- src/DisplayTitleFinder.php
+++ src/DisplayTitleFinder.php
@@ -24,9 +24,6 @@ class DisplayTitleFinder {
 
 	/**
 	 * @since 3.1
-	 *
-	 * @param Store $store
-	 * @param EntityCache $entityCache
 	 */
 	public function __construct( Store $store, EntityCache $entityCache ) {
 		$this->store = $store;
@@ -51,11 +48,9 @@ class DisplayTitleFinder {
 
 	/**
 	 * @since 3.2
-	 *
-	 * @param SemanticData $semanticData
 	 */
 	public function prefetchFromSemanticData( SemanticData $semanticData ): void {
-		if ( $this->canUse === false ) {
+		if ( !$this->canUse ) {
 			return;
 		}
 
@@ -106,7 +101,7 @@ class DisplayTitleFinder {
 	 * @param array $dataItems
 	 */
 	public function prefetchFromList( $dataItems ): void {
-		if ( $this->canUse === false || !is_iterable( $dataItems ) ) {
+		if ( !$this->canUse || !is_iterable( $dataItems ) ) {
 			return;
 		}
 
@@ -141,7 +136,7 @@ class DisplayTitleFinder {
 
 		try {
 			$displayTitleLookup = $this->store->service( 'DisplayTitleLookup' );
-		} catch ( ServiceNotFoundException $e ) {
+		} catch ( ServiceNotFoundException ) {
 			return;
 		}
 
@@ -183,13 +178,9 @@ class DisplayTitleFinder {
 
 	/**
 	 * @since 3.1
-	 *
-	 * @param WikiPage $subject
-	 *
-	 * @return string
 	 */
 	public function findDisplayTitle( WikiPage $subject ): string {
-		if ( $this->canUse === false ) {
+		if ( !$this->canUse ) {
 			return '';
 		}
 
