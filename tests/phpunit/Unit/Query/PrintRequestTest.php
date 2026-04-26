208ce95cf31e1e29daab73f57cd2ecd86bf7a71d
paladox <paladox@users.noreply.github.com>
2026-04-26 12:35:50 +0100

    Change assertions for getLabel and getWikiText methods

--- tests/phpunit/Unit/Query/PrintRequestTest.php
+++ tests/phpunit/Unit/Query/PrintRequestTest.php
@@ -44,12 +44,12 @@ class PrintRequestTest extends TestCase {
 			$instance->getCanonicalLabel()
 		);
 
-		$this->assertNull(
-						$instance->getLabel()
+		$this->assertFalse(
+			$instance->getLabel()
 		);
 
 		$this->assertNull(
-						$instance->getWikiText()
+			$instance->getWikiText()
 		);
 
 		$instance->setLabel( 'Bar' );
