052a8143a0f17d8b80314bde461e0c4044796be9
alistair3149 <alistair31494322@gmail.com>
2026-05-02 20:39:48 -0400

    Add Muted*QueryBuilder subclasses with profiler-mute on execute

---
+++ src/MediaWiki/Connection/MutedReplaceQueryBuilder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SMW\MediaWiki\Connection;
+
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\ReplaceQueryBuilder;
+use Wikimedia\ScopedCallback;
+
+/**
+ * `ReplaceQueryBuilder` whose `execute()` is wrapped with the SMW
+ * `TransactionHandler::muteTransactionProfiler()` scope.
+ *
+ * @license GPL-2.0-or-later
+ * @since 7.0.0
+ */
+class MutedReplaceQueryBuilder extends ReplaceQueryBuilder {
+
+	/**
+	 * @since 7.0.0
+	 */
+	public function __construct(
+		IDatabase $db,
+		private readonly TransactionHandler $transactionHandler,
+	) {
+		parent::__construct( $db );
+	}
+
+	public function execute() {
+		$scope = $this->transactionHandler->muteTransactionProfiler();
+
+		try {
+			parent::execute();
+		} finally {
+			ScopedCallback::consume( $scope );
+		}
+	}
+}
