f270e6b8c3e91a799b44167b64f439f3956f049d
alistair3149 <alistair31494322@gmail.com>
2026-05-02 21:56:22 -0400

    Convert ProximityPropertyValueLookup to QueryBuilder

--- tests/phpunit/Unit/SQLStore/Lookup/ProximityPropertyValueLookupTest.php
+++ tests/phpunit/Unit/SQLStore/Lookup/ProximityPropertyValueLookupTest.php
@@ -5,13 +5,13 @@ namespace SMW\Tests\Unit\SQLStore\Lookup;
 use PHPUnit\Framework\TestCase;
 use SMW\DataItems\Property;
 use SMW\MediaWiki\Connection\Database;
-use SMW\MediaWiki\Connection\Query;
 use SMW\RequestOptions;
 use SMW\SQLStore\EntityStore\DataItemHandler;
 use SMW\SQLStore\Lookup\ProximityPropertyValueLookup;
 use SMW\SQLStore\SQLStore;
 use stdClass;
 use Wikimedia\Rdbms\FakeResultWrapper;
+use Wikimedia\Rdbms\SelectQueryBuilder;
 
 /**
  * @covers \SMW\SQLStore\Lookup\ProximityPropertyValueLookup
@@ -44,19 +44,20 @@ class ProximityPropertyValueLookupTest extends TestCase {
 			->disableOriginalConstructor()
 			->getMock();
 
-		$query = new Query( $connection );
-
 		$connection->expects( $this->any() )
 			->method( 'addQuotes' )
 			->willReturnArgument( 0 );
 
-		$connection->expects( $this->any() )
-			->method( 'newQuery' )
-			->willReturn( $query );
+		$whereConditions = [];
 
-		$connection->expects( $this->atLeastOnce() )
-			->method( 'query' )
-			->willReturn( new FakeResultWrapper( [ $row ] ) );
+		$connection->expects( $this->any() )
+			->method( 'newSelectQueryBuilder' )
+			->willReturnCallback( function () use ( $row, &$whereConditions ) {
+				return $this->createMockSelectQueryBuilder(
+					[ $row ],
+					$whereConditions
+				);
+			} );
 
 		$idTable = $this->getMockBuilder( '\stdClass' )
 			->disableOriginalConstructor()
@@ -99,24 +100,25 @@ class ProximityPropertyValueLookupTest extends TestCase {
 			$store
 		);
 
-		$parameters = [
-			'search' => 'Foo',
-			'property' => 'Bar'
-		];
-
 		$instance->lookup(
 			new Property( 'Bar' ),
 			'Foo',
 			new RequestOptions()
 		);
 
-		$this->assertStringContainsString(
-			'[{"OR":"smw_sortkey LIKE %Foo%"},{"OR":"smw_sortkey LIKE %Foo%"},{"OR":"smw_sortkey LIKE %FOO%"}]',
-			$query->__toString()
+		// The wpg path goes through fetchFromIDTable -> build_like which
+		// produces an OR-joined LIKE clause on smw_sortkey covering the search
+		// term, its ucfirst form, and uppercase form (and lowercase if input
+		// is mixed case).
+		$flat = $this->flattenWhereConditions( $whereConditions );
+
+		$this->assertContains(
+			'(smw_sortkey LIKE %Foo% OR smw_sortkey LIKE %Foo% OR smw_sortkey LIKE %FOO%)',
+			$flat
 		);
 	}
 
-	public function tesLookup_txt_property() {
+	public function testLookup_txt_property() {
 		$row = new stdClass;
 		$row->o_hash = 'Test';
 		$row->smw_id = 42;
@@ -125,19 +127,20 @@ class ProximityPropertyValueLookupTest extends TestCase {
 			->disableOriginalConstructor()
 			->getMock();
 
-		$query = new Query( $connection );
-
 		$connection->expects( $this->any() )
 			->method( 'addQuotes' )
 			->willReturnArgument( 0 );
 
-		$connection->expects( $this->any() )
-			->method( 'newQuery' )
-			->willReturn( $query );
+		$whereConditions = [];
 
-		$connection->expects( $this->atLeastOnce() )
-			->method( 'query' )
-			->willReturn( new FakeResultWrapper( [ $row ] ) );
+		$connection->expects( $this->any() )
+			->method( 'newSelectQueryBuilder' )
+			->willReturnCallback( function () use ( $row, &$whereConditions ) {
+				return $this->createMockSelectQueryBuilder(
+					[ $row ],
+					$whereConditions
+				);
+			} );
 
 		$idTable = $this->getMockBuilder( '\stdClass' )
 			->disableOriginalConstructor()
@@ -184,21 +187,86 @@ class ProximityPropertyValueLookupTest extends TestCase {
 			$store
 		);
 
-		$parameters = [
-			'search' => 'Foo',
-			'property' => 'Text'
-		];
-
 		$instance->lookup(
 			new Property( '_TEXT' ),
 			'Foo',
 			new RequestOptions()
 		);
 
-		$this->assertStringContainsString(
-			'[{"OR":"o_hash LIKE %Foo%"},{"OR":"o_hash LIKE %Foo%"},{"OR":"o_hash LIKE %FOO%"},{"AND":"p_id=42"}]',
-			$query->__toString()
+		$flat = $this->flattenWhereConditions( $whereConditions );
+
+		$this->assertContains(
+			'(o_hash LIKE %Foo% OR o_hash LIKE %Foo% OR o_hash LIKE %FOO%)',
+			$flat
 		);
+
+		$this->assertContains(
+			[ 'p_id' => 42 ],
+			$whereConditions
+		);
+	}
+
+	/**
+	 * Flatten a list of captured andWhere() arguments into a list of strings
+	 * for easy substring assertions.
+	 */
+	private function flattenWhereConditions( array $conditions ): array {
+		$flat = [];
+		foreach ( $conditions as $cond ) {
+			if ( is_string( $cond ) ) {
+				$flat[] = $cond;
+			} elseif ( is_array( $cond ) ) {
+				foreach ( $cond as $k => $v ) {
+					$flat[] = is_int( $k ) ? (string)$v : "$k=$v";
+				}
+			}
+		}
+		return $flat;
+	}
+
+	/**
+	 * Creates a mock SelectQueryBuilder where chained methods return $this,
+	 * fetchResultSet() returns the given rows wrapped in FakeResultWrapper,
+	 * and andWhere() arguments are captured into $whereConditions for
+	 * inspection by tests.
+	 */
+	private function createMockSelectQueryBuilder(
+		array $rows = [],
+		array &$whereConditions = []
+	) {
+		$queryBuilder = $this->getMockBuilder( SelectQueryBuilder::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$chainMethods = [ 'select', 'fields', 'field', 'from', 'table',
+			'join', 'leftJoin', 'where', 'groupBy', 'having', 'orderBy',
+			'caller', 'distinct', 'limit', 'offset', 'options', 'option' ];
+
+		foreach ( $chainMethods as $method ) {
+			$queryBuilder->expects( $this->any() )
+				->method( $method )
+				->willReturnSelf();
+		}
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'andWhere' )
+			->willReturnCallback( static function ( $conds ) use ( $queryBuilder, &$whereConditions ) {
+				$whereConditions[] = $conds;
+				return $queryBuilder;
+			} );
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'newSubquery' )
+			->willReturnCallback( fn () => $this->createMockSelectQueryBuilder(
+				$rows,
+				$whereConditions
+			) );
+
+		$queryBuilder->expects( $this->any() )
+			->method( 'fetchResultSet' )
+			->willReturn( new FakeResultWrapper( $rows ) );
+
+		return $queryBuilder;
 	}
 
 }
