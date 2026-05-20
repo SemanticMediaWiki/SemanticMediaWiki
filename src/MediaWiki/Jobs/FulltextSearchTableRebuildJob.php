aa70b6b2907a6203d92c7029b5a0de4222c1e187
alistair3149 <alistair31494322@gmail.com>
2026-05-20 00:48:43 -0400

    Inject Store into FulltextSearchTable{Update,Rebuild}Job
    Both jobs now receive the Store via the JobClasses ObjectFactory spec
    and stop pulling it from ApplicationFactory at runtime. Internal
    re-enqueue in FulltextSearchTableRebuildJob routes through MediaWiki's
    JobFactory so the spec is re-applied.

--- src/MediaWiki/Jobs/FulltextSearchTableRebuildJob.php
+++ src/MediaWiki/Jobs/FulltextSearchTableRebuildJob.php
@@ -2,11 +2,11 @@
 
 namespace SMW\MediaWiki\Jobs;
 
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
 use SMW\MediaWiki\Job;
-use SMW\Services\ServicesFactory as ApplicationFactory;
 use SMW\SQLStore\QueryEngine\FulltextSearchTableFactory;
-use SMW\SQLStore\SQLStore;
+use SMW\Store;
 
 /**
  * @license GPL-2.0-or-later
@@ -18,12 +18,14 @@ class FulltextSearchTableRebuildJob extends Job {
 
 	/**
 	 * @since 2.5
-	 *
-	 * @param Title $title
-	 * @param array $params job parameters
 	 */
-	public function __construct( Title $title, $params = [] ) {
+	public function __construct(
+		Title $title,
+		array $params,
+		Store $store
+	) {
 		parent::__construct( 'smw.fulltextSearchTableRebuild', $title, $params );
+		$this->setStore( $store );
 	}
 
 	/**
@@ -38,9 +40,8 @@ class FulltextSearchTableRebuildJob extends Job {
 
 		$fulltextSearchTableFactory = new FulltextSearchTableFactory();
 
-		// Only the SQLStore is supported
 		$searchTableRebuilder = $fulltextSearchTableFactory->newSearchTableRebuilder(
-			ApplicationFactory::getInstance()->getStore( SQLStore::class )
+			$this->store
 		);
 
 		if ( $this->hasParameter( 'table' ) ) {
@@ -60,12 +61,16 @@ class FulltextSearchTableRebuildJob extends Job {
 			return;
 		}
 
-		foreach ( $tableList as $tableName ) {
-			$fulltextSearchTableRebuildJob = new self( $this->getTitle(), [
-				'table' => $tableName
-			] );
+		$jobFactory = MediaWikiServices::getInstance()->getJobFactory();
 
-			$fulltextSearchTableRebuildJob->insert();
+		foreach ( $tableList as $tableName ) {
+			$job = $jobFactory->newJob(
+				'smw.fulltextSearchTableRebuild',
+				$this->getTitle(),
+				[ 'table' => $tableName ]
+			);
+
+			$job->insert();
 		}
 	}
 
