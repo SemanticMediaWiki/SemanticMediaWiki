86bd549ca00317a0d7fe3ed69d9b673eb1f40d63
alistair3149 <alistair31494322@gmail.com>
2026-06-01 13:45:56 -0400

    Cover skin-language fallback in ParserOutputPostCacheTransform test

--- tests/phpunit/Unit/MediaWiki/Hooks/ParserOutputPostCacheTransformTest.php
+++ tests/phpunit/Unit/MediaWiki/Hooks/ParserOutputPostCacheTransformTest.php
@@ -5,6 +5,7 @@ namespace SMW\Tests\Unit\MediaWiki\Hooks;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Parser\ParserOutput;
 use PHPUnit\Framework\TestCase;
+use Skin;
 use SMW\Localizer\DeferredLocalizedMessage;
 use SMW\MediaWiki\Hooks\ParserOutputPostCacheTransform;
 
@@ -33,5 +34,19 @@ class ParserOutputPostCacheTransformTest extends TestCase {
 			->onParserOutputPostCacheTransform( new ParserOutput(), $text, $options );
 
 		$this->assertSame( $before, $text );
+	}
+
+	public function testUsesLanguageFromSkinWhenUserLangAbsent() {
+		$de = MediaWikiServices::getInstance()->getLanguageFactory()->getLanguage( 'de' );
+		$skin = $this->createMock( Skin::class );
+		$skin->method( 'getLanguage' )->willReturn( $de );
+
+		$text = '<a>' . DeferredLocalizedMessage::newMarker( 'further-results' ) . '</a>';
+		$options = [ 'skin' => $skin ];
+
+		( new ParserOutputPostCacheTransform() )
+			->onParserOutputPostCacheTransform( new ParserOutput(), $text, $options );
+
+		$this->assertStringNotContainsString( 'smw-localized-message', $text );
 	}
 }
