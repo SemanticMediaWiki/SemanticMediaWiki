f872565d23ffea617eec9b4c2452698baf7f5746
alistair3149 <alistair31494322@gmail.com>
2026-08-10 16:48:13 -0400

    Escape card-state values in Special:FacetedSearch hidden inputs
    The hidden inputs that preserve the FacetedSearch card state (cstate)
    across requests concatenated the request-controlled key and value into
    the form without escaping, enabling reflected XSS via crafted cstate
    parameters. Build them with Html::hidden(), which escapes both.
    
    Residual of CVE-2025-10354, whose fix escaped only the adjacent q
    parameter and left the cstate loop untouched.

--- tests/phpunit/Unit/MediaWiki/Specials/FacetedSearch/HtmlBuilderTest.php
+++ tests/phpunit/Unit/MediaWiki/Specials/FacetedSearch/HtmlBuilderTest.php
@@ -3,7 +3,9 @@
 namespace SMW\Tests\Unit\MediaWiki\Specials\FacetedSearch;
 
 use MediaWiki\Html\TemplateParser;
+use MediaWiki\Title\Title;
 use PHPUnit\Framework\TestCase;
+use SMW\Localizer\MessageLocalizer;
 use SMW\MediaWiki\Specials\FacetedSearch\ExploreListBuilder;
 use SMW\MediaWiki\Specials\FacetedSearch\ExtraFieldBuilder;
 use SMW\MediaWiki\Specials\FacetedSearch\FacetBuilder;
@@ -11,6 +13,7 @@ use SMW\MediaWiki\Specials\FacetedSearch\HtmlBuilder;
 use SMW\MediaWiki\Specials\FacetedSearch\OptionsBuilder;
 use SMW\MediaWiki\Specials\FacetedSearch\Profile;
 use SMW\MediaWiki\Specials\FacetedSearch\ResultFetcher;
+use SMW\Utils\UrlArgs;
 
 /**
  * @covers \SMW\MediaWiki\Specials\FacetedSearch\HtmlBuilder
@@ -24,12 +27,13 @@ use SMW\MediaWiki\Specials\FacetedSearch\ResultFetcher;
 class HtmlBuilderTest extends TestCase {
 
 	private $profile;
-	private $templateParser;
+	private TemplateParser $templateParser;
 	private $optionsBuilder;
 	private $extraFieldBuilder;
 	private $facetBuilder;
 	private $resultFetcher;
 	private $exploreListBuilder;
+	private $messageLocalizer;
 
 	protected function setUp(): void {
 		parent::setUp();
@@ -38,9 +42,7 @@ class HtmlBuilderTest extends TestCase {
 			->disableOriginalConstructor()
 			->getMock();
 
-		$this->templateParser = $this->getMockBuilder( TemplateParser::class )
-			->disableOriginalConstructor()
-			->getMock();
+		$this->templateParser = new TemplateParser( __DIR__ . '/../../../../../../templates/FacetedSearch' );
 
 		$this->optionsBuilder = $this->getMockBuilder( OptionsBuilder::class )
 			->disableOriginalConstructor()
@@ -61,6 +63,12 @@ class HtmlBuilderTest extends TestCase {
 		$this->exploreListBuilder = $this->getMockBuilder( ExploreListBuilder::class )
 			->disableOriginalConstructor()
 			->getMock();
+
+		$this->messageLocalizer = $this->getMockBuilder( MessageLocalizer::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->messageLocalizer->method( 'msg' )->willReturn( '' );
 	}
 
 	public function testCanConstruct() {
@@ -68,6 +76,62 @@ class HtmlBuilderTest extends TestCase {
 			HtmlBuilder::class,
 			new HtmlBuilder( $this->profile, $this->templateParser, $this->optionsBuilder, $this->extraFieldBuilder, $this->facetBuilder, $this->resultFetcher, $this->exploreListBuilder )
 		);
+	}
+
+	public function testMaliciousCardStateValueIsEscapedInHiddenInput() {
+		$html = $this->buildHtmlForCardState( [ '0' => '"><script>alert(1)</script>' ] );
+
+		$this->assertStringNotContainsString( '<script>alert(1)</script>', $html );
+		$this->assertStringContainsString( '&lt;script&gt;alert(1)&lt;/script&gt;', $html );
+	}
+
+	public function testMaliciousCardStateKeyIsEscapedInHiddenInput() {
+		$html = $this->buildHtmlForCardState( [ '"><script>alert(2)</script>' => 'e' ] );
+
+		$this->assertStringNotContainsString( '<script>alert(2)</script>', $html );
+		$this->assertStringContainsString( '&lt;script&gt;alert(2)&lt;/script&gt;', $html );
+	}
+
+	public function testBenignCardStateIsPreservedInHiddenInput() {
+		$html = $this->buildHtmlForCardState( [ 'extra-fields' => 'c' ] );
+
+		$this->assertStringContainsString( 'name="cstate[extra-fields]"', $html );
+		$this->assertStringContainsString( 'value="c"', $html );
+	}
+
+	public function testNonScalarCardStateValueIsIgnored() {
+		$html = $this->buildHtmlForCardState( [ 'k' => [ 'b' => 'x' ] ] );
+
+		$this->assertStringNotContainsString( 'cstate[k]', $html );
+	}
+
+	/**
+	 * Renders the full search page for a given "cstate" (card state) request
+	 * value and returns the produced HTML.
+	 */
+	private function buildHtmlForCardState( array $cstate ): string {
+		$this->resultFetcher->method( 'getHtml' )->willReturn( '' );
+		$this->resultFetcher->method( 'getTotalCount' )->willReturn( 0 );
+		$this->resultFetcher->method( 'getOffset' )->willReturn( 0 );
+		$this->resultFetcher->method( 'getLimit' )->willReturn( 10 );
+		$this->resultFetcher->method( 'hasFurtherResults' )->willReturn( false );
+
+		$title = $this->createMock( Title::class );
+		$title->method( 'getLocalUrl' )->willReturn( '' );
+
+		$instance = new HtmlBuilder(
+			$this->profile,
+			$this->templateParser,
+			$this->optionsBuilder,
+			$this->extraFieldBuilder,
+			$this->facetBuilder,
+			$this->resultFetcher,
+			$this->exploreListBuilder
+		);
+
+		$instance->setMessageLocalizer( $this->messageLocalizer );
+
+		return $instance->buildHTML( $title, new UrlArgs( [ 'q' => 'Text', 'cstate' => $cstate ] ) );
 	}
 
 }
