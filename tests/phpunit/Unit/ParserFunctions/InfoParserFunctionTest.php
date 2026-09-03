acf5fbb03f51b2e88f457722864188582ace9066
alistair3149 <alistair31494322@gmail.com>
2026-09-03 12:31:03 -0400

    Cover the special-page branch of the tooltip resource commit
    commitRequestedResources() commits to $wgOut when the parser title is a
    special page, which is what keeps Special:ExpandTemplates working when no
    context title is given. That branch had no test, so nothing pinned it
    against the restructuring around it.
    
    Both new cases run on a non-rendering pass, pinning that the branch is
    reached whatever the output type. newParser() now takes the namespace, so
    every call site states which title the parser carries.

--- tests/phpunit/Unit/ParserFunctions/InfoParserFunctionTest.php
+++ tests/phpunit/Unit/ParserFunctions/InfoParserFunctionTest.php
@@ -2,15 +2,19 @@
 
 namespace SMW\Tests\Unit\ParserFunctions;
 
+use MediaWiki\Context\RequestContext;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Output\OutputPage;
 use MediaWiki\Parser\Parser;
 use MediaWiki\Parser\ParserOutput;
 use MediaWiki\Parser\StripState;
+use MediaWiki\Title\Title;
 use ParamProcessor\ProcessedParam;
 use ParamProcessor\ProcessingResult;
 use PHPUnit\Framework\TestCase;
 use SMW\MediaWiki\Outputs;
 use SMW\ParserFunctions\InfoParserFunction;
+use SMW\Tests\TestEnvironment;
 
 /**
  * @covers \SMW\ParserFunctions\InfoParserFunction
@@ -23,13 +27,18 @@ use SMW\ParserFunctions\InfoParserFunction;
  */
 class InfoParserFunctionTest extends TestCase {
 
+	private TestEnvironment $testEnvironment;
+
 	protected function setUp(): void {
 		parent::setUp();
+
+		$this->testEnvironment = new TestEnvironment();
 		Outputs::reset();
 	}
 
 	protected function tearDown(): void {
 		Outputs::reset();
+		$this->testEnvironment->tearDown();
 		parent::tearDown();
 	}
 
@@ -70,7 +79,7 @@ class InfoParserFunctionTest extends TestCase {
 	}
 
 	public function testHtmlPassCommitsTooltipResourcesToTheParser() {
-		$parser = $this->newParser( Parser::OT_HTML );
+		$parser = $this->newParser( Parser::OT_HTML, NS_MAIN );
 
 		$result = $this->handleInfo( $parser, 'Some tooltip text' );
 
@@ -87,7 +96,7 @@ class InfoParserFunctionTest extends TestCase {
 	}
 
 	public function testNonHtmlPassLeavesTooltipResourcesBufferedForTheRenderingParse() {
-		$parser = $this->newParser( Parser::OT_PREPROCESS );
+		$parser = $this->newParser( Parser::OT_PREPROCESS, NS_MAIN );
 
 		$result = $this->handleInfo( $parser, 'Some tooltip text' );
 
@@ -110,6 +119,19 @@ class InfoParserFunctionTest extends TestCase {
 			'ext.smw.tooltip',
 			$renderingParseOutput->getModules(),
 			'the requirements must stay buffered for the rendering parse that follows'
+		);
+	}
+
+	public function testSpecialPageCommitsTooltipResourcesToTheOutputPage() {
+		$parser = $this->newParser( Parser::OT_PREPROCESS, NS_SPECIAL );
+		$output = $this->newOutputPage();
+
+		$this->handleInfo( $parser, 'Some tooltip text' );
+
+		$this->assertContains(
+			'ext.smw.tooltip',
+			$output->getModules(),
+			'a special page carries the requirements on its OutputPage, whatever the pass'
 		);
 	}
 
@@ -146,13 +168,27 @@ class InfoParserFunctionTest extends TestCase {
 		return $processingResult;
 	}
 
-	private function newParser( int $outputType ): Parser {
+	private function newOutputPage(): OutputPage {
+		$context = new RequestContext();
+		$context->setTitle( $this->newTitle( NS_SPECIAL ) );
+
+		$output = $context->getOutput();
+		$this->testEnvironment->withConfiguration( [ 'wgOut' => $output ] );
+
+		return $output;
+	}
+
+	private function newTitle( int $namespace ): Title {
+		return MediaWikiServices::getInstance()->getTitleFactory()->newFromText( __CLASS__, $namespace );
+	}
+
+	private function newParser( int $outputType, int $namespace ): Parser {
 		$parser = $this->getMockBuilder( Parser::class )
 			->disableOriginalConstructor()
 			->getMock();
 
 		$parser->method( 'getTitle' )
-			->willReturn( MediaWikiServices::getInstance()->getTitleFactory()->newFromText( __CLASS__, NS_MAIN ) );
+			->willReturn( $this->newTitle( $namespace ) );
 
 		$parser->method( 'getOutput' )
 			->willReturn( new ParserOutput() );
--- tests/phpunit/Unit/ParserFunctions/PropertyLinkParserFunctionTest.php
+++ tests/phpunit/Unit/ParserFunctions/PropertyLinkParserFunctionTest.php
@@ -2,7 +2,9 @@
 
 namespace SMW\Tests\Unit\ParserFunctions;
 
+use MediaWiki\Context\RequestContext;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Output\OutputPage;
 use MediaWiki\Parser\Parser;
 use MediaWiki\Parser\ParserOutput;
 use MediaWiki\Title\Title;
@@ -87,12 +89,12 @@ class PropertyLinkParserFunctionTest extends TestCase {
 
 		$this->assertSame(
 			$this->propertyLink( '[[:Property:Foo|Foo]]' ),
-			$instance->parse( [ $this->newParser( Parser::OT_HTML ), 'Foo' ] )
+			$instance->parse( [ $this->newParser( Parser::OT_HTML, NS_MAIN ), 'Foo' ] )
 		);
 	}
 
 	public function testHtmlPassCommitsTooltipResourcesToTheParser() {
-		$parser = $this->newParser( Parser::OT_HTML );
+		$parser = $this->newParser( Parser::OT_HTML, NS_MAIN );
 
 		$result = $this->newInstance( $this->newParserData( NS_MAIN ) )->parse(
 			[ $parser, 'Modification date' ]
@@ -111,7 +113,7 @@ class PropertyLinkParserFunctionTest extends TestCase {
 	}
 
 	public function testNonHtmlPassLeavesTooltipResourcesBufferedForTheRenderingParse() {
-		$parser = $this->newParser( Parser::OT_PREPROCESS );
+		$parser = $this->newParser( Parser::OT_PREPROCESS, NS_MAIN );
 
 		$result = $this->newInstance( $this->newParserData( NS_MAIN ) )->parse(
 			[ $parser, 'Modification date' ]
@@ -136,6 +138,21 @@ class PropertyLinkParserFunctionTest extends TestCase {
 			'ext.smw.tooltip',
 			$renderingParseOutput->getModules(),
 			'the requirements must stay buffered for the rendering parse that follows'
+		);
+	}
+
+	public function testSpecialPageCommitsTooltipResourcesToTheOutputPage() {
+		$parser = $this->newParser( Parser::OT_PREPROCESS, NS_SPECIAL );
+		$output = $this->newOutputPage();
+
+		$this->newInstance( $this->newParserData( NS_MAIN ) )->parse(
+			[ $parser, 'Modification date' ]
+		);
+
+		$this->assertContains(
+			'ext.smw.tooltip',
+			$output->getModules(),
+			'a special page carries the requirements on its OutputPage, whatever the pass'
 		);
 	}
 
@@ -192,13 +209,23 @@ class PropertyLinkParserFunctionTest extends TestCase {
 		return MediaWikiServices::getInstance()->getTitleFactory()->newFromText( __CLASS__, $namespace );
 	}
 
-	private function newParser( int $outputType ): Parser {
+	private function newOutputPage(): OutputPage {
+		$context = new RequestContext();
+		$context->setTitle( $this->newTitle( NS_SPECIAL ) );
+
+		$output = $context->getOutput();
+		$this->testEnvironment->withConfiguration( [ 'wgOut' => $output ] );
+
+		return $output;
+	}
+
+	private function newParser( int $outputType, int $namespace ): Parser {
 		$parser = $this->getMockBuilder( Parser::class )
 			->disableOriginalConstructor()
 			->getMock();
 
 		$parser->method( 'getTitle' )
-			->willReturn( $this->newTitle( NS_MAIN ) );
+			->willReturn( $this->newTitle( $namespace ) );
 
 		$parser->method( 'getOutput' )
 			->willReturn( new ParserOutput() );
